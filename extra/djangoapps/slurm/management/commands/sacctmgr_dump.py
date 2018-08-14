df09f9b98473e82def604919933fc556d1e04d9b
Andrew E. Bruno <aebruno2@buffalo.edu>
2018-08-14 16:00:53 -0400

    Refactor sacct_dump command.
    1. saccmgr requires each cluster associations to be in their own file.
    
    2. Add support for "Slurm specifications" resource/subscription attributes
    
    3. Add support for "Slurm user specifications" attribute to allow adding specs
       for each user

--- extra/djangoapps/slurm/management/commands/sacctmgr_dump.py
+++ extra/djangoapps/slurm/management/commands/sacctmgr_dump.py
@@ -1,7 +1,9 @@
+import sys
+import os
+import logging
 from django.core.management.base import BaseCommand, CommandError
 from core.djangoapps.subscription.models import Subscription
-from core.djangoapps.resources.models import Resource
-import logging
+from core.djangoapps.resources.models import Resource,ResourceAttribute
 
 logger = logging.getLogger(__name__)
 
@@ -9,73 +11,26 @@ class Command(BaseCommand):
     help = 'Dump slurm assocations for sacctmgr in flat file format'
 
     def add_arguments(self, parser):
-        parser.add_argument("-o", "--output", help="Path to output file (defaults to STDOUT)")
+        parser.add_argument("-o", "--output", help="Path to output directory")
 
-    def write_resource(self, cluster, qos):
-        """Write out Cluster. Anything included on this line will be the defaults
-        for all associations on this cluster
+    def write_cluster(self, out, cluster):
+        """Write out Slurm assocations for the given cluster
         """
-        self.slurm_out.write("Cluster - '{}':Fairshare=1:QOS='{}'\n".format(
-            cluster,
-            ','.join(list(set(qos))),
-        ))
-
-    def write_account_user(self, account, username):
-        """Write out user assocation"""
-        self.slurm_out.write("User - '{}':DefaultAccount='{}'\n".format(
-            username,
-            account,
-        ))
-
-    def write_account_from_subscription(self, resource, sub):
-        """Write out account assocation"""
-        sub_qos = sub.subscriptionattribute_set.filter(subscription_attribute_type__name='Slurm QOS').first()
-        qos = []
-        if sub_qos is not None:
-            qos += sub_qos.value.split(',')
-
-        slurm_account = sub.subscriptionattribute_set.filter(subscription_attribute_type__name='Slurm Account Name').first()
-        if slurm_account is None:
-            logger.warn("Subscription {} to resource {} is missing 'Slurm Account Name' attribute. Skipping".format(sub, resource))
-            return
-
-        self.slurm_out.write("Account - '{}':Description='{}':Fairshare=100:QOS='{}'\n".format(
-            slurm_account.value,
-            sub.project.title.replace("'", '').strip(),
-            ','.join(list(set(qos))),
-        ))
-
-        self.slurm_out.write("Parent - '{}'\n".format(slurm_account.value))
-        for u in sub.subscriptionuser_set.filter(status__name='Active'):
-            self.write_account_user(slurm_account.value, u.user.username)
-
-    def handle(self, *args, **options):
-        verbosity = int(options['verbosity'])
-        root_logger = logging.getLogger('')
-        if verbosity > 1:
-            root_logger.setLevel(logging.DEBUG)
-
-        self.slurm_out = self.stdout
-        if options['output']:
-            logger.debug("Writing output to file: {}".format(options['output']))
-            self.slurm_out = open(options['output'], 'w')
-        else:
-            logger.debug("Writing to stdout")
-
-        # Fetch all resources that have a 'Slurm cluster name' attrbiute
+        # Fetch all resources for given cluster
         resources = Resource.objects.filter(
                 is_available=True,
-                resourceattribute__resource_attribute_type__name='Slurm cluster name'
+                resourceattribute__resource_attribute_type__name='Slurm cluster name',
+                resourceattribute__value=cluster,
             )
 
         for r in resources:
-            cluster = r.resourceattribute_set.filter(resource_attribute_type__name='Slurm cluster name').first()
-            res_qos = r.resourceattribute_set.filter(resource_attribute_type__name='Slurm QOS').first()
-            qos = []
-            if res_qos is not None:
-                qos += res_qos.value.split(',')
+            specs = r.resourceattribute_set.filter(resource_attribute_type__name='Slurm specifications').first()
+            out.write("Cluster - '{}':{}\n".format(
+                cluster,
+                specs.value if specs else '',
+            ))
+            out.write("Parent - 'root'\n")
 
-            self.write_resource(cluster.value, qos)
             subs = r.subscription_set.prefetch_related(
                     'project',
                     'subscriptionattribute_set',
@@ -85,4 +40,49 @@ class Command(BaseCommand):
                 )
 
             for s in subs:
-                self.write_account_from_subscription(r, s)
+                self.write_account_from_subscription(out, r, s)
+
+    def write_account_user(self, out, username, specs):
+        """Write out user assocation"""
+        out.write("User - '{}':{}\n".format(
+            username,
+            specs.value if specs else '',
+        ))
+
+    def write_account_from_subscription(self, out, resource, sub):
+        """Write out account assocation"""
+        specs = sub.subscriptionattribute_set.filter(subscription_attribute_type__name='Slurm specifications').first()
+        user_specs = sub.subscriptionattribute_set.filter(subscription_attribute_type__name='Slurm user specifications').first()
+        slurm_account = sub.subscriptionattribute_set.filter(subscription_attribute_type__name='Slurm Account Name').first()
+        if slurm_account is None:
+            logger.warn("Subscription {} to resource {} is missing 'Slurm Account Name' attribute. Skipping".format(sub, resource))
+            return
+
+        out.write("Account - '{}':Description='{}':{}\n".format(
+            slurm_account.value,
+            sub.project.title.replace("'", '').strip(),
+            specs.value if specs else '',
+        ))
+
+        out.write("Parent - '{}'\n".format(slurm_account.value))
+        for u in sub.subscriptionuser_set.filter(status__name='Active'):
+            self.write_account_user(out, u.user.username, user_specs)
+
+    def handle(self, *args, **options):
+        self.slurm_out = os.getcwd()
+        if options['output']:
+            self.slurm_out = options['output']
+
+        if not os.path.isdir(self.slurm_out):
+            os.mkdir(self.slurm_out, 0o0700)
+
+        logger.warn("Writing output to directory: %s", self.slurm_out)
+
+        # Fetch list of SLURM clusters
+        clusters = ResourceAttribute.objects.filter(
+                resource_attribute_type__name='Slurm cluster name'
+            )
+
+        for cluster in clusters.distinct():
+            with open(os.path.join(self.slurm_out, '{}.cfg'.format(cluster.value)), 'w') as fh:
+                self.write_cluster(fh, cluster.value)
