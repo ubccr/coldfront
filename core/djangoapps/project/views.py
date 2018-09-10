79cb2856ea54b0943dddf70ee874cb1beaa61811
mkzia <mkzia@buffalo.edu>
2018-09-10 10:46:55 -0400

    Add users as pending - add to subscriptions

--- core/djangoapps/project/views.py
+++ core/djangoapps/project/views.py
@@ -574,6 +574,7 @@ class ProjectAddUsersView(LoginRequiredMixin, UserPassesTestMixin, View):
         if formset.is_valid() and subscription_form.is_valid():
             project_user_active_status_choice = ProjectUserStatusChoice.objects.get(name='Active')
             subscription_user_active_status_choice = SubscriptionUserStatusChoice.objects.get(name='Active')
+            subscription_user_pending_add_status_choice = SubscriptionUserStatusChoice.objects.get(name='Pending - Add')
             subscription_form_data = subscription_form.cleaned_data['subscription']
             if '__select_all__' in subscription_form_data:
                 subscription_form_data.remove('__select_all__')
@@ -603,13 +604,13 @@ class ProjectAddUsersView(LoginRequiredMixin, UserPassesTestMixin, View):
                     for subscription in Subscription.objects.filter(pk__in=subscription_form_data):
                         if subscription.subscriptionuser_set.filter(user=user_obj).exists():
                             subscription_user_obj = subscription.subscriptionuser_set.get(user=user_obj)
-                            subscription_user_obj.status = subscription_user_active_status_choice
+                            subscription_user_obj.status = subscription_user_pending_add_status_choice
                             subscription_user_obj.save()
                         else:
                             subscription_user_obj = SubscriptionUser.objects.create(
                                 subscription=subscription,
                                 user=user_obj,
-                                status=subscription_user_active_status_choice)
+                                status=subscription_user_pending_add_status_choice)
                         subscription_activate_user.send(sender=self.__class__,
                                                         subscription_user_pk=subscription_user_obj.pk)
 
