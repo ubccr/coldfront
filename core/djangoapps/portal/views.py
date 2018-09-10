92d8d8e5c3d9927f4dc3532e1a95b4ed2ea6e62b
mkzia <mkzia@buffalo.edu>
2018-09-10 10:33:35 -0400

    Fix typo

--- core/djangoapps/portal/views.py
+++ core/djangoapps/portal/views.py
@@ -30,11 +30,12 @@ def home(request):
             Q(status__name__in=['Active', 'Approved', 'Denied', 'Expired', 'New', 'Pending', ]) &
             Q(project__status__name__in=['Active', 'New']) &
             Q(project__projectuser__user=request.user) &
-            Q(project__projectuser__status__name=['Active', 'Pending - Add']) &
+            Q(project__projectuser__status__name__in=['Active', 'Pending - Add']) &
             Q(subscriptionuser__user=request.user) &
             Q(subscriptionuser__status__name__in=['Active', 'Pending - Add'])
         ).distinct().order_by('-created')[:5]
         context['project_list'] = project_list
+        print(subscription_list)
         context['subscription_list'] = subscription_list
     else:
         template_name = 'portal/nonauthorized_home.html'
