015d39e3a93d876a3b1d89508c3370fdd7a973bc
Andrew E. Bruno <aebruno2@buffalo.edu>
2018-09-14 12:23:04 -0400

    Add robots.txt Fixes #73

--- config/urls.py
+++ config/urls.py
@@ -3,13 +3,15 @@ Coldfront URL Configuration
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 
 import core.djangoapps.portal.views as portal_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hijack/', include('hijack.urls', namespace='hijack')),
+    re_path('^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name="robots"),
     path('', portal_views.home, name='home'),
     path('center-summary', portal_views.center_summary, name='center-summary'),
     path('subscription-summary', portal_views.subscription_summary, name='subscription-summary'),
