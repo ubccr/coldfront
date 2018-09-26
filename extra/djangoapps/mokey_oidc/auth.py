1fd0509665ff1f5b45cb0453ef702f7d1280c94d
Andrew E. Bruno <aebruno2@buffalo.edu>
2018-09-26 15:44:41 -0400

    Refactor mokey OpenID Connect extra app.
    - Use mozilla-django-oidc
    - Implement custom create/update user
    - Add readme
    - Clean up settings.py

---
+++ extra/djangoapps/mokey_oidc/auth.py
@@ -0,0 +1,50 @@
+from mozilla_django_oidc.auth import OIDCAuthenticationBackend
+from django.contrib.auth.models import Group
+
+class OIDCMokeyAuthenticationBackend(OIDCAuthenticationBackend):
+
+    def create_user(self, claims):
+        username = claims.get('uid')
+        if not username:
+            return None
+
+        user = self.UserModel.objects.create_user(username, email)
+
+        user.first_name = claims.get('first_name', '')
+        user.last_name = claims.get('last_name', '')
+        user.email = claims.get('email')
+
+        groups = claims.get('groups', '')
+        user.groups.clear()
+        for group_name in groups.split(';'):
+            group, created = Group.objects.get_or_create(name=group_name)
+            user.groups.add(group)
+
+        user.save()
+
+        return user
+
+    def update_user(self, user, claims):
+        user.first_name = claims.get('first_name', '')
+        user.last_name = claims.get('last_name', '')
+        user.email = claims.get('email')
+
+        groups = claims.get('groups', '')
+        user.groups.clear()
+        for group_name in groups.split(';'):
+            group, created = Group.objects.get_or_create(name=group_name)
+            user.groups.add(group)
+
+        user.save()
+
+        return user
+
+    def filter_users_by_claims(self, claims):
+        uid = claims.get('uid')
+        if not uid:
+            return self.UserModel.objects.none()
+
+        try:
+            return self.UserModel.objects.filter(username=uid)
+        except self.UserModel.DoesNotExist:
+            return self.UserModel.objects.none()
--- extra/djangoapps/mokey_oidc/pipelines.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import Group
-
-
-def mirror_groups(backend, user, response, *args, **kwargs):
-    groups = response.get('groups')
-    user.groups.clear()
-    for group_name in groups.split(';'):
-        group, created = Group.objects.get_or_create(name=group_name)
-        user.groups.add(group)
