21e4b567bfa91d8944b6201a4a2612bdd6962364
Andrew E. Bruno <aebruno2@buffalo.edu>
2018-09-27 11:52:25 -0400

    Add syncing of pi, allow/deny groups, and better logging

--- extra/djangoapps/mokey_oidc/auth.py
+++ extra/djangoapps/mokey_oidc/auth.py
@@ -1,5 +1,14 @@
+import logging
+
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 from django.contrib.auth.models import Group
+from common.djangolibs.utils import import_from_settings
+
+logger = logging.getLogger(__name__)
+
+PI_GROUP = import_from_settings('MOKEY_OIDC_PI_GROUP', 'pi')
+ALLOWED_GROUPS = import_from_settings('MOKEY_OIDC_ALLOWED_GROUPS', [])
+DENY_GROUPS = import_from_settings('MOKEY_OIDC_DENY_GROUPS', [])
 
 class OIDCMokeyAuthenticationBackend(OIDCAuthenticationBackend):
 
@@ -7,7 +16,11 @@ class OIDCMokeyAuthenticationBackend(OIDCAuthenticationBackend):
         email = claims.get('email')
         username = claims.get('uid')
         if not username:
+            logger.error("Failed to create user. username not found in mokey oidc id_token claims: %s", claims)
             return None
+
+        if not email:
+            logger.warn("Creating user with no email. Could not find email for user %s in mokey oidc id_token claims: %s", username, claims)
 
         user = self.UserModel.objects.create_user(username, email)
 
@@ -15,30 +28,38 @@ class OIDCMokeyAuthenticationBackend(OIDCAuthenticationBackend):
         user.last_name = claims.get('last', '')
 
         groups = claims.get('groups', '')
-        user.groups.clear()
-        for group_name in groups.split(';'):
-            group, created = Group.objects.get_or_create(name=group_name)
-            user.groups.add(group)
+        self._sync_groups(user, groups.split(';'))
 
         user.save()
 
         return user
 
     def update_user(self, user, claims):
-        print(claims)
         user.first_name = claims.get('first', '')
         user.last_name = claims.get('last', '')
-        user.email = claims.get('email')
+        email = claims.get('email')
+        if email and len(email) > 0:
+            user.email = email
+        else:
+            logger.warn("Failed to update email. Could not find email for user %s in mokey oidc id_token claims: %s", username, claims)
 
         groups = claims.get('groups', '')
-        user.groups.clear()
-        for group_name in groups.split(';'):
-            group, created = Group.objects.get_or_create(name=group_name)
-            user.groups.add(group)
+        self._sync_groups(user, groups.split(';'))
 
         user.save()
 
         return user
+
+    def _sync_groups(self, user, groups):
+        is_pi = False
+        user.groups.clear()
+        for group_name in groups:
+            group, created = Group.objects.get_or_create(name=group_name)
+            user.groups.add(group)
+            if group_name == PI_GROUP:
+                is_pi = True
+
+        user.userprofile.is_pi = is_pi
 
     def filter_users_by_claims(self, claims):
         uid = claims.get('uid')
@@ -49,3 +70,24 @@ class OIDCMokeyAuthenticationBackend(OIDCAuthenticationBackend):
             return self.UserModel.objects.filter(username=uid)
         except self.UserModel.DoesNotExist:
             return self.UserModel.objects.none()
+
+    def verify_claims(self, claims):
+        verified = super(OIDCMokeyAuthenticationBackend, self).verify_claims(claims)
+
+        if len(ALLOWED_GROUPS) == 0 and len(DENY_GROUPS) == 0:
+            return verified and True
+
+        groups = claims.get('groups', '')
+        group_list = groups.split(';')
+        
+        if len(ALLOWED_GROUPS) > 0:
+            for g in ALLOWED_GROUPS:
+                if g not in group_list:
+                    return False
+
+        if len(DENY_GROUPS) > 0:
+            for g in DENY_GROUPS:
+                if g in group_list:
+                    return False
+    
+        return verified and True
