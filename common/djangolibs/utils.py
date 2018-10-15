39e954ccef5c13ce049d16e6252d6b0b047d6d87
mkzia <mkzia@buffalo.edu>
2018-10-15 15:43:49 -0400

    Add emails for subscriptions expiring and move update statuses function to tasks in subscription

--- common/djangolibs/utils.py
+++ common/djangolibs/utils.py
@@ -27,15 +27,6 @@ def import_from_settings(attr, *args):
         raise ImproperlyConfigured('Setting {0} not found'.format(attr))
 
 
-def update_statuses():
-
-    number_expired = Subscription.objects.filter(
-        status__name='Active', active_until__lt=datetime.datetime.now().date()).update(
-        status=SubscriptionStatusChoice.objects.get(name='Expired'))
-
-    logger.info('Subscriptions set to expired: {}'.format(number_expired))
-
-
 def get_domain_url(request):
     return request.build_absolute_uri().replace(request.get_full_path(), '')
 
@@ -58,3 +49,5 @@ def su_login_callback(user):
 
     logger.warn('User {} requested to login as another user but does not have permissions', user)
     return False
+
+
