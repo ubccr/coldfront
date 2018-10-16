e40eaf88d7e0761ac4a28f9b5c34291870877f15
Andrew E. Bruno <aebruno2@buffalo.edu>
2018-10-16 13:05:39 -0400

    Change title of admin

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,9 @@ from django.views.generic import TemplateView
 
 import core.djangoapps.portal.views as portal_views
 
+admin.site.site_header = 'ColdFront Administration'
+admin.site.site_title = 'ColdFront Administration'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name="robots"),
