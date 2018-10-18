9b0b1295b8afab9f03baaa9b8232c9b138e7c1de
mkzia <mkzia@buffalo.edu>
2018-10-18 11:21:19 -0400

    Allow projects with pending project review to request and renew subscriptions

--- core/djangoapps/subscription/views.py
+++ core/djangoapps/subscription/views.py
@@ -280,9 +280,6 @@ class SubscriptionCreateView(LoginRequiredMixin, UserPassesTestMixin, FormView):
             messages.error(request, 'You cannot request a new subscription because you have to review your project first.')
             return HttpResponseRedirect(reverse('project-detail', kwargs={'pk': project_obj.pk}))
 
-        if project_obj.last_project_review and project_obj.last_project_review.status.name == 'Pending':
-            messages.error(request, 'You cannot request a new subscription because your project review is in pending state.')
-            return HttpResponseRedirect(reverse('project-detail', kwargs={'pk': project_obj.pk}))
 
         if project_obj.status.name not in ['Active', 'New', ]:
             messages.error(request, 'You cannot request a new subscription to an archived project.')
@@ -754,10 +751,6 @@ class SubscriptionRenewView(LoginRequiredMixin, UserPassesTestMixin, TemplateVie
 
         if subscription_obj.project.needs_review:
             messages.error(request, 'You cannot renew your subscription because you have to review your project first.')
-            return HttpResponseRedirect(reverse('project-detail', kwargs={'pk': subscription_obj.project.pk}))
-
-        if subscription_obj.project.last_project_review and subscription_obj.project.last_project_review.status.name == 'Pending':
-            messages.error(request, 'You cannot renew your subscription because your project review is in pending state.')
             return HttpResponseRedirect(reverse('project-detail', kwargs={'pk': subscription_obj.project.pk}))
 
         if subscription_obj.expires_in > 60:
