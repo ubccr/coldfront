813deb8ad6b0499bd2bad6f97232d8aea8dc7fc5
Mohammad K. Zia <mkzia@ventures.ccr.buffalo.edu>
2018-12-21 16:57:13 -0500

    Resolves #134

--- coldfront/core/publication/views.py
+++ coldfront/core/publication/views.py
@@ -11,12 +11,13 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views.generic import DetailView, ListView, TemplateView, View
-from doi2bib import crossref
 
 from coldfront.core.project.models import Project
 from coldfront.core.publication.forms import (PublicationDeleteForm,
-                                               PublicationSearchForm)
+                                              PublicationResultForm,
+                                              PublicationSearchForm)
 from coldfront.core.publication.models import Publication, PublicationSource
+from doi2bib import crossref
 
 
 class PublicationSearchView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
@@ -27,7 +28,8 @@ class PublicationSearchView(LoginRequiredMixin, UserPassesTestMixin, TemplateVie
         if self.request.user.is_superuser:
             return True
 
-        project_obj = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
+        project_obj = get_object_or_404(
+            Project, pk=self.kwargs.get('project_pk'))
 
         if project_obj.pi == self.request.user:
             return True
@@ -36,9 +38,11 @@ class PublicationSearchView(LoginRequiredMixin, UserPassesTestMixin, TemplateVie
             return True
 
     def dispatch(self, request, *args, **kwargs):
-        project_obj = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
+        project_obj = get_object_or_404(
+            Project, pk=self.kwargs.get('project_pk'))
         if project_obj.status.name not in ['Active', 'New', ]:
-            messages.error(request, 'You cannot add publications to an archived project.')
+            messages.error(
+                request, 'You cannot add publications to an archived project.')
             return HttpResponseRedirect(reverse('project-detail', kwargs={'pk': project_obj.pk}))
         else:
             return super().dispatch(request, *args, **kwargs)
@@ -46,7 +50,8 @@ class PublicationSearchView(LoginRequiredMixin, UserPassesTestMixin, TemplateVie
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['publication_search_form'] = PublicationSearchForm()
-        context['project'] = Project.objects.get(pk=self.kwargs.get('project_pk'))
+        context['project'] = Project.objects.get(
+            pk=self.kwargs.get('project_pk'))
         return context
 
 
@@ -58,7 +63,8 @@ class PublicationSearchResultView(LoginRequiredMixin, UserPassesTestMixin, Templ
         if self.request.user.is_superuser:
             return True
 
-        project_obj = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
+        project_obj = get_object_or_404(
+            Project, pk=self.kwargs.get('project_pk'))
 
         if project_obj.pi == self.request.user:
             return True
@@ -67,18 +73,16 @@ class PublicationSearchResultView(LoginRequiredMixin, UserPassesTestMixin, Templ
             return True
 
     def dispatch(self, request, *args, **kwargs):
-        project_obj = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
+        project_obj = get_object_or_404(
+            Project, pk=self.kwargs.get('project_pk'))
         if project_obj.status.name not in ['Active', 'New', ]:
-            messages.error(request, 'You cannot add publications to an archived project.')
+            messages.error(
+                request, 'You cannot add publications to an archived project.')
             return HttpResponseRedirect(reverse('project-detail', kwargs={'project_pk': project_obj.pk}))
         else:
             return super().dispatch(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        unique_id = request.POST.get('unique_id')
-        project_pk = self.kwargs.get('project_pk')
-
-        project_obj = get_object_or_404(Project, pk=project_pk)
+    def _search_id(self, unique_id):
         matching_source_obj = None
         for source in PublicationSource.objects.all():
             if source.name == 'doi':
@@ -94,7 +98,8 @@ class PublicationSearchResultView(LoginRequiredMixin, UserPassesTestMixin, Templ
 
             elif source.name == 'adsabs':
                 try:
-                    url = 'http://adsabs.harvard.edu/cgi-bin/nph-bib_query?bibcode={}&data_type=BIBTEX'.format(unique_id)
+                    url = 'http://adsabs.harvard.edu/cgi-bin/nph-bib_query?bibcode={}&data_type=BIBTEX'.format(
+                        unique_id)
                     r = requests.get(url, timeout=5)
                     bp = BibTexParser(interpolate_strings=False)
                     bib_database = bp.parse(r.text)
@@ -105,21 +110,44 @@ class PublicationSearchResultView(LoginRequiredMixin, UserPassesTestMixin, Templ
                     continue
 
         if not matching_source_obj:
-            return render(request, self.template_name, {})
+            return False
 
         year = as_text(bib_json['year'])
-        author = as_text(bib_json['author']).replace('{\\textquotesingle}', "'").replace('{\\textendash}', '-').replace('{\\textemdash}', '-').replace('{\\textasciigrave}', ' ').replace('{\\textdaggerdbl}', ' ').replace('{\\textdagger}', ' ')
-        title = as_text(bib_json['title']).replace('{\\textquotesingle}', "'").replace('{\\textendash}', '-').replace('{\\textemdash}', '-').replace('{\\textasciigrave}', ' ').replace('{\\textdaggerdbl}', ' ').replace('{\\textdagger}', ' ')
+        author = as_text(bib_json['author']).replace('{\\textquotesingle}', "'").replace('{\\textendash}', '-').replace(
+            '{\\textemdash}', '-').replace('{\\textasciigrave}', ' ').replace('{\\textdaggerdbl}', ' ').replace('{\\textdagger}', ' ')
+        title = as_text(bib_json['title']).replace('{\\textquotesingle}', "'").replace('{\\textendash}', '-').replace(
+            '{\\textemdash}', '-').replace('{\\textasciigrave}', ' ').replace('{\\textdaggerdbl}', ' ').replace('{\\textdagger}', ' ')
 
         author = re.sub("{|}", "", author)
         title = re.sub("{|}", "", title)
+        pub_dict = {}
+        pub_dict['author'] = author
+        pub_dict['year'] = year
+        pub_dict['title'] = title
+        pub_dict['unique_id'] = unique_id
+        pub_dict['source_pk'] = matching_source_obj.pk
+
+        return pub_dict
+
+    def post(self, request, *args, **kwargs):
+        search_ids = list(set(request.POST.get('search_id').split()))
+        project_pk = self.kwargs.get('project_pk')
+
+        project_obj = get_object_or_404(Project, pk=project_pk)
+        pubs = []
+        for ele in search_ids:
+            pub_dict = self._search_id(ele)
+            if pub_dict:
+                pubs.append(pub_dict)
+
+        formset = formset_factory(PublicationResultForm, max_num=len(pubs))
+        formset = formset(initial=pubs, prefix='pubform')
+
         context = {}
-        context['author'] = author
-        context['year'] = year
-        context['title'] = title
-        context['unique_id'] = unique_id
-        context['source'] = matching_source_obj
         context['project_pk'] = project_obj.pk
+        context['formset'] = formset
+        context['search_ids'] = search_ids
+        context['pubs'] = pubs
 
         return render(request, self.template_name, context)
 
@@ -131,7 +159,8 @@ class PublicationAddView(LoginRequiredMixin, UserPassesTestMixin, View):
         if self.request.user.is_superuser:
             return True
 
-        project_obj = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
+        project_obj = get_object_or_404(
+            Project, pk=self.kwargs.get('project_pk'))
 
         if project_obj.pi == self.request.user:
             return True
@@ -140,37 +169,52 @@ class PublicationAddView(LoginRequiredMixin, UserPassesTestMixin, View):
             return True
 
     def dispatch(self, request, *args, **kwargs):
-        project_obj = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
+        project_obj = get_object_or_404(
+            Project, pk=self.kwargs.get('project_pk'))
         if project_obj.status.name not in ['Active', 'New', ]:
-            messages.error(request, 'You cannot add publications to an archived project.')
+            messages.error(
+                request, 'You cannot add publications to an archived project.')
             return HttpResponseRedirect(reverse('project-detail', kwargs={'pk': project_obj.pk}))
         else:
             return super().dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        title = request.POST.get('title')
-        author = request.POST.get('author')
-        year = request.POST.get('year')
-        source_pk = request.POST.get('source')
-        unique_id = request.POST.get('unique_id')
+        pubs = eval(request.POST.get('pubs'))
         project_pk = self.kwargs.get('project_pk')
 
         project_obj = get_object_or_404(Project, pk=project_pk)
-        source_obj = get_object_or_404(PublicationSource, pk=source_pk)
+        formset = formset_factory(PublicationResultForm, max_num=len(pubs))
+        formset = formset(request.POST, initial=pubs, prefix='pubform')
 
-        try:
-            publication_obj = Publication.objects.create(
-                project=project_obj,
-                title=title,
-                author=author,
-                year=year,
-                unique_id=unique_id,
-                source=source_obj
-            )
-            messages.success(request, 'Added publication to project.')
-            return HttpResponseRedirect(reverse('project-detail', kwargs={'pk': project_pk}))
-        except IntegrityError as e:
-            messages.warning(request, 'Publication "{}" already in project.'.format(title))
+        publications_added = 0
+        publications_skipped = []
+        if formset.is_valid():
+            for form in formset:
+                form_data = form.cleaned_data
+                source_obj = PublicationSource.objects.get(
+                    pk=form_data.get('source_pk'))
+                publication_obj, created = Publication.objects.get_or_create(
+                    project=project_obj,
+                    title=form_data.get('title'),
+                    author=form_data.get('author'),
+                    year=form_data.get('year'),
+                    unique_id=form_data.get('unique_id'),
+                    source=source_obj
+                )
+                if created:
+                    publications_added += 1
+                else:
+                    publications_skipped.append(form_data.get('unique_id'))
+
+            msg = ''
+            if publications_added:
+                msg += 'Added {} publication{} to project.'.format(
+                    publications_added, 's' if publications_added > 1 else '')
+            if publications_skipped:
+                msg += 'Skipped adding: {}'.format(
+                    ', '.join(publications_skipped))
+
+            messages.success(request, msg)
             return HttpResponseRedirect(reverse('project-detail', kwargs={'pk': project_pk}))
 
 
@@ -182,7 +226,8 @@ class PublicationDeletePublicationsView(LoginRequiredMixin, UserPassesTestMixin,
         if self.request.user.is_superuser:
             return True
 
-        project_obj = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
+        project_obj = get_object_or_404(
+            Project, pk=self.kwargs.get('project_pk'))
 
         if project_obj.pi == self.request.user:
             return True
@@ -190,7 +235,8 @@ class PublicationDeletePublicationsView(LoginRequiredMixin, UserPassesTestMixin,
         if project_obj.projectuser_set.filter(user=self.request.user, role__name='Manager', status__name='Active').exists():
             return True
 
-        messages.error(self.request, 'You do not have permission to delete publications from this project.')
+        messages.error(
+            self.request, 'You do not have permission to delete publications from this project.')
 
     def get_publications_to_delete(self, project_obj):
 
@@ -203,27 +249,33 @@ class PublicationDeletePublicationsView(LoginRequiredMixin, UserPassesTestMixin,
         return publications_do_delete
 
     def get(self, request, *args, **kwargs):
-        project_obj = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
+        project_obj = get_object_or_404(
+            Project, pk=self.kwargs.get('project_pk'))
 
         publications_do_delete = self.get_publications_to_delete(project_obj)
         context = {}
 
         if publications_do_delete:
-            formset = formset_factory(PublicationDeleteForm, max_num=len(publications_do_delete))
-            formset = formset(initial=publications_do_delete, prefix='publicationform')
+            formset = formset_factory(
+                PublicationDeleteForm, max_num=len(publications_do_delete))
+            formset = formset(initial=publications_do_delete,
+                              prefix='publicationform')
             context['formset'] = formset
 
         context['project'] = project_obj
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
-        project_obj = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
+        project_obj = get_object_or_404(
+            Project, pk=self.kwargs.get('project_pk'))
 
         publications_do_delete = self.get_publications_to_delete(project_obj)
         context = {}
 
-        formset = formset_factory(PublicationDeleteForm, max_num=len(publications_do_delete))
-        formset = formset(request.POST, initial=publications_do_delete, prefix='publicationform')
+        formset = formset_factory(
+            PublicationDeleteForm, max_num=len(publications_do_delete))
+        formset = formset(
+            request.POST, initial=publications_do_delete, prefix='publicationform')
 
         publications_deleted_count = 0
 
@@ -240,7 +292,8 @@ class PublicationDeletePublicationsView(LoginRequiredMixin, UserPassesTestMixin,
                     publication_obj.delete()
                     publications_deleted_count += 1
 
-            messages.success(request, 'Deleted {} publications from project.'.format(publications_deleted_count))
+            messages.success(request, 'Deleted {} publications from project.'.format(
+                publications_deleted_count))
             return HttpResponseRedirect(reverse('project-detail', kwargs={'pk': project_obj.pk}))
 
     def get_success_url(self):
