3a49b0c36e4f469e485521132a530afd657c2c05
Mohammad K. Zia <mkzia@ventures.ccr.buffalo.edu>
2019-01-04 09:42:21 -0500

    Add date validation

--- coldfront/core/subscription/models.py
+++ coldfront/core/subscription/models.py
@@ -1,6 +1,6 @@
 import datetime
-import logging
 import importlib
+import logging
 
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -8,20 +8,20 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.html import mark_safe
 from django.utils.module_loading import import_string
-from model_utils.models import TimeStampedModel
-from simple_history.models import HistoricalRecords
 
 from coldfront.core.project.models import Project
 from coldfront.core.resources.models import Resource
 from coldfront.core.utils.common import import_from_settings
-
+from model_utils.models import TimeStampedModel
+from simple_history.models import HistoricalRecords
 
 logger = logging.getLogger(__name__)
 
 
-SUBSCRIPTION_FUNCS_ON_EXPIRE = import_from_settings('SUBSCRIPTION_FUNCS_ON_EXPIRE', [])
-SLURM_ACCOUNT_ATTRIBUTE_NAME = import_from_settings('SLURM_ACCOUNT_ATTRIBUTE_NAME', 'slurm_account_name')
-
+SUBSCRIPTION_FUNCS_ON_EXPIRE = import_from_settings(
+    'SUBSCRIPTION_FUNCS_ON_EXPIRE', [])
+SLURM_ACCOUNT_ATTRIBUTE_NAME = import_from_settings(
+    'SLURM_ACCOUNT_ATTRIBUTE_NAME', 'slurm_account_name')
 
 
 class SubscriptionStatusChoice(TimeStampedModel):
@@ -38,7 +38,8 @@ class Subscription(TimeStampedModel):
     """ Subscription to a system Resource. """
     project = models.ForeignKey(Project, on_delete=models.CASCADE,)
     resources = models.ManyToManyField(Resource)
-    status = models.ForeignKey(SubscriptionStatusChoice, on_delete=models.CASCADE, verbose_name='Status')
+    status = models.ForeignKey(
+        SubscriptionStatusChoice, on_delete=models.CASCADE, verbose_name='Status')
     quantity = models.IntegerField(default=1)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
@@ -51,7 +52,8 @@ class Subscription(TimeStampedModel):
 
         permissions = (
             ('can_view_all_subscriptions', 'Can view all subscriptions'),
-            ('can_review_subscription_requests', 'Can review subscription requests'),
+            ('can_review_subscription_requests',
+             'Can review subscription requests'),
         )
 
     def clean(self):
@@ -75,7 +77,8 @@ class Subscription(TimeStampedModel):
                 raise ValidationError('You have to set the end date.')
 
             if self.start_date > self.end_date:
-                raise ValidationError('Start date cannot be greater than the end date.')
+                raise ValidationError(
+                    'Start date cannot be greater than the end date.')
 
     def save(self, *args, **kwargs):
         if self.pk:
@@ -97,7 +100,8 @@ class Subscription(TimeStampedModel):
         for attribute in self.subscriptionattribute_set.all():
 
             if attribute.subscription_attribute_type.name in [SLURM_ACCOUNT_ATTRIBUTE_NAME, ]:
-                html_string += '%s: %s <br>' % (attribute.subscription_attribute_type.name, attribute.value)
+                html_string += '%s: %s <br>' % (
+                    attribute.subscription_attribute_type.name, attribute.value)
 
             if hasattr(attribute, 'subscriptionattributeusage'):
                 try:
@@ -116,8 +120,6 @@ class Subscription(TimeStampedModel):
                 )
                 html_string += string
 
-
-
         return mark_safe(html_string)
 
     @property
@@ -129,13 +131,15 @@ class Subscription(TimeStampedModel):
         return self.resources.filter(is_subscribable=True).first()
 
     def get_attribute(self, name):
-        attr = self.subscriptionattribute_set.filter(subscription_attribute_type__name=name).first()
+        attr = self.subscriptionattribute_set.filter(
+            subscription_attribute_type__name=name).first()
         if attr:
             return attr.value
         return None
 
     def set_usage(self, name, value):
-        attr = self.subscriptionattribute_set.filter(subscription_attribute_type__name=name).first()
+        attr = self.subscriptionattribute_set.filter(
+            subscription_attribute_type__name=name).first()
         if not attr:
             return
 
@@ -143,7 +147,8 @@ class Subscription(TimeStampedModel):
             return
 
         if not SubscriptionAttributeUsage.objects.filter(subscription_attribute=attr).exists():
-            usage = SubscriptionAttributeUsage.objects.create(subscription_attribute=attr)
+            usage = SubscriptionAttributeUsage.objects.create(
+                subscription_attribute=attr)
         else:
             usage = attr.subscriptionattributeusage
 
@@ -151,7 +156,8 @@ class Subscription(TimeStampedModel):
         usage.save()
 
     def get_attribute_list(self, name):
-        attr = self.subscriptionattribute_set.filter(subscription_attribute_type__name=name).all()
+        attr = self.subscriptionattribute_set.filter(
+            subscription_attribute_type__name=name).all()
         return [a.value for a in attr]
 
     def __str__(self):
@@ -211,7 +217,8 @@ class SubscriptionAttributeType(TimeStampedModel):
 
 class SubscriptionAttribute(TimeStampedModel):
     """ SubscriptionAttribute. """
-    subscription_attribute_type = models.ForeignKey(SubscriptionAttributeType, on_delete=models.CASCADE)
+    subscription_attribute_type = models.ForeignKey(
+        SubscriptionAttributeType, on_delete=models.CASCADE)
     subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
     value = models.CharField(max_length=128)
     history = HistoricalRecords()
@@ -220,20 +227,31 @@ class SubscriptionAttribute(TimeStampedModel):
 
         super().save(*args, **kwargs)
         if self.subscription_attribute_type.has_usage and not SubscriptionAttributeUsage.objects.filter(subscription_attribute=self).exists():
-            SubscriptionAttributeUsage.objects.create(subscription_attribute=self)
+            SubscriptionAttributeUsage.objects.create(
+                subscription_attribute=self)
 
     def clean(self):
         if self.subscription_attribute_type.is_unique and self.subscription.subscriptionattribute_set.filter(subscription_attribute_type=self.subscription_attribute_type).exists():
             raise ValidationError("'{}' attribute already exists for this subscription.".format(
                 self.subscription_attribute_type))
 
-        expected_value_type = self.subscription_attribute_type.name.strip()
+        expected_value_type = self.subscription_attribute_type.attribute_type.name.strip()
+
         if expected_value_type == "Int" and not isinstance(self.value, int):
-            raise ValidationError('Invalid Value "%s". Value must be an integer.' % (self.value))
+            raise ValidationError(
+                'Invalid Value "%s". Value must be an integer.' % (self.value))
         elif expected_value_type == "Float" and not isinstance(self.value, float):
-            raise ValidationError('Invalid Value "%s". Value must be a float.' % (self.value))
+            raise ValidationError(
+                'Invalid Value "%s". Value must be a float.' % (self.value))
         elif expected_value_type == "Yes/No" and self.value not in ["Yes", "No"]:
-            raise ValidationError('Invalid Value "%s". Allowed inputs are "Yes" or "No".' % (self.value))
+            raise ValidationError(
+                'Invalid Value "%s". Allowed inputs are "Yes" or "No".' % (self.value))
+        elif expected_value_type == "Date":
+            try:
+                datetime.datetime.strptime(self.value.strip(), "%Y-%m-%d")
+            except ValueError:
+                raise ValidationError(
+                    'Invalid Value "%s". Date must be in format YYYY-MM-DD' % (self.value))
 
     def __str__(self):
         return '%s' % (self.subscription_attribute_type.name)
@@ -241,7 +259,8 @@ class SubscriptionAttribute(TimeStampedModel):
 
 class SubscriptionAttributeUsage(TimeStampedModel):
     """ SubscriptionAttributeUsage. """
-    subscription_attribute = models.OneToOneField(SubscriptionAttribute, on_delete=models.CASCADE, primary_key=True)
+    subscription_attribute = models.OneToOneField(
+        SubscriptionAttribute, on_delete=models.CASCADE, primary_key=True)
     value = models.FloatField(default=0)
     history = HistoricalRecords()
 
