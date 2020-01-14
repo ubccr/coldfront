b54ed670677c5f5a22d8327a29965183debf3dac
Shawn Rice <zooley@purdue.edu>
2020-01-14 09:27:18 -0500

    [fix] Fixing form field type The `note` field should be a `textarea`. When set as a standard input with a character limit of 64, `form.is_valid()` returns false if the note is longer than 64 characters (which should be allowable).

--- coldfront/core/allocation/forms.py
+++ coldfront/core/allocation/forms.py
@@ -159,7 +159,7 @@ class AllocationReviewUserForm(forms.Form):
 
 class AllocationInvoiceNoteDeleteForm(forms.Form):
     pk = forms.IntegerField(required=False, disabled=True)
-    note = forms.CharField(max_length=64, disabled=True)
+    note = forms.CharField(widget=forms.Textarea, disabled=True)
     author = forms.CharField(
         max_length=512, required=False, disabled=True)
     selected = forms.BooleanField(initial=False, required=False)
