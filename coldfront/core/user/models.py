e9ca134909edddec8b1479bfe1b6fef695fbeae2
Matthew Li <matthew.elbert.li@gmail.com>
2020-10-01 16:09:55 -0700

    Add model fields needed for the API to UserProfile and Project

--- coldfront/core/user/models.py
+++ coldfront/core/user/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
-
+from django.core.validators import MinLengthValidator
+from django.core.validators import RegexValidator
 from django.db import models
 from rest_framework.authtoken.models import Token
 
@@ -7,6 +8,19 @@ from rest_framework.authtoken.models import Token
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_pi = models.BooleanField(default=False)
+
+    middle_name = models.CharField(max_length=30, blank=True)
+    cluster_uid = models.CharField(
+        unique=True,
+        max_length=10,
+        validators=[
+            MinLengthValidator(3),
+            RegexValidator(
+                regex=r'^[0-9]+$', message='Cluster UID must be numeric.')
+        ],
+        blank=True,
+        null=True
+    )
 
 
 class ExpiringToken(Token):
