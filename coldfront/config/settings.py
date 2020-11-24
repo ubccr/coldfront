62a6b7259f7790174ba3996a47ef01fddec3efb4
Matthew Li <matthew.elbert.li@gmail.com>
2020-11-24 13:00:30 -0800

    Enforce password validation in settings

--- coldfront/config/settings.py
+++ coldfront/config/settings.py
@@ -92,6 +92,24 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 #------------------------------------------------------------------------------
+# Authentication validators
+#------------------------------------------------------------------------------
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+#------------------------------------------------------------------------------
 # Django site settings
 #------------------------------------------------------------------------------
 ROOT_URLCONF = 'coldfront.config.urls'
