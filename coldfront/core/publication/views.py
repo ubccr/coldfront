ba0d0cbcec0660c40b7b203d17bdbf698732bbca
Andrew E. Bruno <aebruno2@buffalo.edu>
2021-03-04 09:55:10 -0500

    Fix year validation bug.
    This fixes #258. Add better form validation for manual publication
    entry. Change to using uuid4 instead of sha256 hash for unique ids. The
    issue is when manually entering a publication, we do not ask for a DOI
    as it may not have one. As a doi uniquely identifies a publication, we
    simply use a randomly generated uuid4 as the unique id.

--- coldfront/core/publication/views.py
+++ coldfront/core/publication/views.py
@@ -1,7 +1,5 @@
-import hashlib
-import json
 import re
-
+import uuid
 import requests
 import os
 import io
@@ -16,6 +14,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views.generic import DetailView, ListView, TemplateView, View
+from django.views.generic.edit import FormView
 from django.views.static import serve
 
 from coldfront.core.project.models import Project
@@ -247,8 +246,8 @@ class PublicationAddView(LoginRequiredMixin, UserPassesTestMixin, View):
 
         return HttpResponseRedirect(reverse('project-detail', kwargs={'pk': project_pk}))
 
-
-class PublicationAddManuallyView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+class PublicationAddManuallyView(LoginRequiredMixin, UserPassesTestMixin, FormView):
+    form_class = PublicationAddForm
     template_name = 'publication/publication_add_publication_manually.html'
 
     def test_func(self):
@@ -256,8 +255,7 @@ class PublicationAddManuallyView(LoginRequiredMixin, UserPassesTestMixin, Templa
         if self.request.user.is_superuser:
             return True
 
-        project_obj = get_object_or_404(
-            Project, pk=self.kwargs.get('project_pk'))
+        project_obj = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
 
         if project_obj.pi == self.request.user:
             return True
@@ -265,62 +263,44 @@ class PublicationAddManuallyView(LoginRequiredMixin, UserPassesTestMixin, Templa
         if project_obj.projectuser_set.filter(user=self.request.user, role__name='Manager', status__name='Active').exists():
             return True
 
+        messages.error(self.request, 'You do not have permission to add a new publication to this project.')
+
     def dispatch(self, request, *args, **kwargs):
-        project_obj = get_object_or_404(
-            Project, pk=self.kwargs.get('project_pk'))
+        project_obj = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
         if project_obj.status.name not in ['Active', 'New', ]:
-            messages.error(
-                request, 'You cannot add publications to an archived project.')
+            messages.error(request, 'You cannot add publications to an archived project.')
             return HttpResponseRedirect(reverse('project-detail', kwargs={'pk': project_obj.pk}))
         else:
             return super().dispatch(request, *args, **kwargs)
 
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['source'] = MANUAL_SOURCE
+        return initial
+
+    def form_valid(self, form):
+        form_data = form.cleaned_data
+        project_obj = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
+        pub_obj = Publication.objects.create(
+            project=project_obj,
+            title=form_data.get('title'),
+            author=form_data.get('author'),
+            year=form_data.get('year'),
+            journal=form_data.get('journal'),
+            unique_id=uuid.uuid4(),
+            source=PublicationSource.objects.get(name=MANUAL_SOURCE),
+        )
+
+        return super().form_valid(form)
+
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['publication_add_form'] = PublicationAddForm(
-            initial={'source': MANUAL_SOURCE})
-        context['project'] = Project.objects.get(
-            pk=self.kwargs.get('project_pk'))
+        context['project'] = Project.objects.get(pk=self.kwargs.get('project_pk'))
         return context
 
-    def post(self, request, *args, **kwargs):
-        project_pk = self.kwargs.get('project_pk')
-        project_obj = get_object_or_404(Project, pk=project_pk)
-
-        # a single, manual pub is being sent
-        client_pub = {
-            key: request.POST[key] for key in (
-                'title',
-                'author',
-                'year',
-                'journal',
-            )
-        }
-        # although not perfect, use a low-collision hash for unique id in the database
-        stable_hash = hashlib.sha256(
-            json.dumps(client_pub, sort_keys=True).encode()
-        ).hexdigest()
-        client_pub['unique_id'] = stable_hash
-        client_pub['source'] = PublicationSource.objects.get(name=MANUAL_SOURCE)
-
-        publication_obj, created = Publication.objects.get_or_create(
-            project=project_obj,
-            **client_pub,
-        )
-        if created:
-            msg = 'Added publication to project.'
-        else:
-            # since we don't have a sensible unique_id here, we need to have
-            # something sensible to display to the user
-            user_friendly_publication_info = '[{} ({}) by {}]'.format(
-                client_pub['title'],
-                client_pub['year'],
-                client_pub['author'],
-            )
-            msg = 'Skipped adding: {}'.format(user_friendly_publication_info)
-
-        messages.success(request, msg)
-        return HttpResponseRedirect(reverse('project-detail', kwargs={'pk': project_pk}))
+    def get_success_url(self):
+        messages.success(self.request, 'Added a publication.')
+        return reverse('project-detail', kwargs={'pk': self.kwargs.get('project_pk')})
 
 
 class PublicationDeletePublicationsView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
