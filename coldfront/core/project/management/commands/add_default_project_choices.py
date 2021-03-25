40e3efe8db8f2c0701b6ae2f7a7eff01f68b4bbf
Matthew Li <matthew.elbert.li@gmail.com>
2021-03-24 21:57:36 -0700

    Fix issues with management commands for creating defaults

--- coldfront/core/project/management/commands/add_default_project_choices.py
+++ coldfront/core/project/management/commands/add_default_project_choices.py
@@ -13,23 +13,23 @@ class Command(BaseCommand):
     help = 'Add default project related choices'
 
     def handle(self, *args, **options):
-        ProjectStatusChoice.objects.all().delete()
+        # ProjectStatusChoice.objects.all().delete()
         for choice in ['New', 'Active', 'Archived', ]:
             ProjectStatusChoice.objects.get_or_create(name=choice)
 
-        ProjectReviewStatusChoice.objects.all().delete()
+        # ProjectReviewStatusChoice.objects.all().delete()
         for choice in ['Completed', 'Pending', ]:
             ProjectReviewStatusChoice.objects.get_or_create(name=choice)
 
-        ProjectUserRoleChoice.objects.all().delete()
+        # ProjectUserRoleChoice.objects.all().delete()
         for choice in ['User', 'Manager', 'Principal Investigator', ]:
             ProjectUserRoleChoice.objects.get_or_create(name=choice)
 
-        ProjectUserStatusChoice.objects.all().delete()
+        # ProjectUserStatusChoice.objects.all().delete()
         for choice in ['Active', 'Pending - Add', 'Pending - Remove', 'Denied', 'Removed', ]:
             ProjectUserStatusChoice.objects.get_or_create(name=choice)
 
-        ProjectAllocationRequestStatusChoice.objects.all().delete()
+        # ProjectAllocationRequestStatusChoice.objects.all().delete()
         for choice in ['Approved', 'Pending', 'Denied', ]:
             ProjectAllocationRequestStatusChoice.objects.get_or_create(
                 name=choice)
