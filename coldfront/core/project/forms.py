0ea694b49d261b531a1fde4c98e4f4db5b55922d
Matthew Li <matthew.elbert.li@gmail.com>
2021-03-30 21:39:55 -0700

    Add form for Vector setup; update length requirements for Savio projects

--- coldfront/core/project/forms.py
+++ coldfront/core/project/forms.py
@@ -591,10 +591,10 @@ class SavioProjectReviewSetupForm(forms.Form):
             'Update the name of the project, in case it needed to be '
             'changed. It must begin with the correct prefix.'),
         label='Final Name',
-        max_length=12,
+        max_length=len('fc_') + 12,
         required=True,
         validators=[
-            MinLengthValidator(7),
+            MinLengthValidator(len('fc_') + 4),
             RegexValidator(
                 r'^[0-9a-z_]+$',
                 message=(
@@ -687,3 +687,64 @@ class VectorProjectDetailsForm(forms.Form):
             raise forms.ValidationError(
                 f'A project with name {name} already exists.')
         return name
+
+
+class VectorProjectReviewSetupForm(forms.Form):
+
+    status = forms.ChoiceField(
+        choices=(
+            ('', 'Select one.'),
+            ('Pending', 'Pending'),
+            ('Complete', 'Complete'),
+        ),
+        help_text='If you are unsure, leave the status as "Pending".',
+        label='Status',
+        required=True)
+    final_name = forms.CharField(
+        help_text=(
+            'Update the name of the project, in case it needed to be '
+            'changed. It must begin with the correct prefix.'),
+        label='Final Name',
+        max_length=len('vector_') + 12,
+        required=True,
+        validators=[
+            MinLengthValidator(len('vector_') + 4),
+            RegexValidator(
+                r'^[0-9a-z_]+$',
+                message=(
+                    'Name must contain only lowercase letters, numbers, and '
+                    'underscores.'))
+        ])
+    justification = forms.CharField(
+        help_text=(
+            'Provide reasoning for your decision. This field is only required '
+            'when the name changes.'),
+        label='Justification',
+        validators=[MinLengthValidator(10)],
+        required=False,
+        widget=forms.Textarea(attrs={'rows': 3}))
+
+    def __init__(self, *args, **kwargs):
+        self.requested_name = kwargs.pop('requested_name')
+        super().__init__(*args, **kwargs)
+        self.fields['final_name'].initial = self.requested_name
+
+    def clean(self):
+        cleaned_data = super().clean()
+        final_name = cleaned_data.get('final_name', '').lower()
+        # Require justification for name changes.
+        if final_name != self.requested_name:
+            justification = cleaned_data.get('justification', '')
+            if not justification.strip():
+                raise forms.ValidationError(
+                    'Please provide a justification for the name change.')
+        return cleaned_data
+
+    def clean_final_name(self):
+        cleaned_data = super().clean()
+        final_name = cleaned_data.get('final_name', '').lower()
+        expected_prefix = 'vector_'
+        if not final_name.startswith(expected_prefix):
+            raise forms.ValidationError(
+                f'Final project name must begin with "{expected_prefix}".')
+        return final_name
