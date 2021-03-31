6365fed55b2efc7c39e0f8982af15f263cd23ddf
Matthew Li <matthew.elbert.li@gmail.com>
2021-03-30 20:37:12 -0700

    Add and rearrange settings; add method for computing prorated allocation to assign

--- coldfront/core/allocation/utils.py
+++ coldfront/core/allocation/utils.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from decimal import Decimal
+
+from django.conf import settings
 from django.db.models import Q
 
 from coldfront.core.allocation.models import (AllocationAttributeType,
@@ -96,3 +100,38 @@ def request_project_cluster_access(allocation_obj, user_obj):
         raise ValueError('Cluster access status is already Active.')
     cluster_access_status.value = 'Pending - Add'
     cluster_access_status.save()
+
+
+def prorated_allocation_amount(amount, dt):
+    """Given a number of service units and a datetime, return the
+    prorated number of service units that would be allocated in the
+    current allocation year, based on the datetime's month.
+
+    Parameters:
+        - amount (Decimal): a number of service units (e.g.,
+                            settings.FCA_DEFAULT_ALLOCATION).
+        - dt (datetime): a datetime object whose month is used in the
+                         calculation, based on its position relative to
+                         the start month of the allocation year.
+
+    Returns:
+        - Decimal
+
+    Raises:
+        - TypeError, if either argument has an invalid type
+        - ValueError, if the provided amount is outside of the allowed
+        range for allocations
+    """
+    if not isinstance(amount, Decimal):
+        raise TypeError(f'Invalid Decimal {amount}.')
+    if not isinstance(dt, datetime):
+        raise TypeError(f'Invalid datetime {dt}.')
+    if not (settings.ALLOCATION_MIN < amount < settings.ALLOCATION_MAX):
+        raise ValueError(f'Invalid amount {amount}.')
+    month = dt.month
+    amount_per_month = amount / 12
+    start_month = settings.ALLOCATION_YEAR_START_MONTH
+    if month >= start_month:
+        return amount - amount_per_month * (month - start_month)
+    else:
+        return amount_per_month * (start_month - month)
