ca2a8d610815ae20dcc32a542d1947ef12b452c8
Matthew Li <matthew.elbert.li@gmail.com>
2021-04-02 12:28:00 -0700

    Add util for retrieving the compute resource for a given project

--- coldfront/core/project/utils.py
+++ coldfront/core/project/utils.py
@@ -40,12 +40,21 @@ def add_project_user_status_choices(apps, schema_editor):
         ProjectUserStatusChoice.objects.get_or_create(name=choice)
 
 
-def get_project_compute_allocation(project_obj):
-    """Return the given Project's Allocation to a Compute Resource."""
-    if project_obj.name.startswith('vector_'):
+def get_project_compute_resource_name(project_obj):
+    """Return the name of the Compute Resource that corresponds to the
+    given Project."""
+    if project_obj.name == 'abc':
+        resource_name = 'ABC Compute'
+    elif project_obj.name.startswith('vector_'):
         resource_name = 'Vector Compute'
     else:
         resource_name = 'Savio Compute'
+    return resource_name
+
+
+def get_project_compute_allocation(project_obj):
+    """Return the given Project's Allocation to a Compute Resource."""
+    resource_name = get_project_compute_resource_name(project_obj)
     return project_obj.allocation_set.get(resources__name=resource_name)
 
 
