7b0d60c0a946d93c1b8a7983dd0586d622e54049
Shiwei Huang <shiwei.julia.huang@gmail.com>
2021-04-07 19:31:28 -0400

    updated import_allocationuser.py

--- coldfront/core/utils/management/commands/import_users_from_files.py
+++ coldfront/core/utils/management/commands/import_users_from_files.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.contrib.auth.models import Group, User
 from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.utils import OperationalError
 
 from csv import reader
 
@@ -23,15 +24,28 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print('Adding users now ...')
-        lab_list = ['zhuang_lab.csv', 'moorcroft_lab.csv', 'kuang_lab.csv', 'kovac_lab.csv',
-        'holman_lab.csv', 'giribet_lab.csv', 'edwards_lab.csv', 'denolle_lab.csv',
-        'wofsy_lab.csv', 'rc_admin.csv']
+        file_path = os.path.join(base_dir, 'local_data/labs')
+        print("line27, file path is", file_path)
+        # arr = os.listdir()
+        arr = os.listdir(file_path)
+
+        print("type of arr is", type(arr))
+        print("line 30",arr)
+        lab_list = []
+        for f in arr:
+            f_name = f.split('.')
+            if (f_name[len(f_name)-1] == 'csv'):
+                my_file = f_name[len(f_name)-2]+('.csv')
+                lab_list.append(my_file)
+         
+        print("line 41: lab_list", lab_list)
         for lab in lab_list:
+            print("line 39 lab is:", lab)
             file_name = lab
             lab_list = list(lab.split('.'))
             lab_name = lab_list[0]
-            file_path = os.path.join(base_dir, 'local_data', file_name)
-
+            file_path = os.path.join(base_dir, 'local_data/labs', file_name)
+            print("line 43 file path is:", file_path)
             # if (file_name != "rc_admin"):
             # open file in read mode
             with open (file_path, 'r') as read_obj:
@@ -40,8 +54,10 @@ class Command(BaseCommand):
                 for row in csv_reader:
                     try:
                         username = row[0]
+                        print("line52, my username is", username)
                         user = User.objects.get(username=username)
-                        #(username, "already exist, don't add to database")
+                        # print("line 54 user is", user)
+                        # print("line47",username, "already exist, don't add to database")
                         # if the user exists, I only need to append this existing user's group
                         if not user.groups.filter(name = lab_name).exists():
                             print("line 45",lab_name)
@@ -51,19 +67,20 @@ class Command(BaseCommand):
                         continue
                     # the type of row is 
                     except ObjectDoesNotExist:
-                    
+                        
+                        print("jumped to line 68")
                         username = row[0]
                         full_name = row[1] 
                         full_name_list = full_name.split()
                         first_name = full_name_list[0]
                     
-                      
                         if (len(full_name_list) > 1):
                             last_name = full_name_list[1]
                         
                         else:
                             last_name = "N/A"
-                            
+                        print("line76 my username is", username)
+                        print("line77 my fullname is", full_name)
                             
                         email = row[2] 
                         is_active = True
@@ -89,6 +106,50 @@ class Command(BaseCommand):
                             is_superuser=is_superuser,
                         )
                         # add user to group
+                        if group_objs:
+                            user_obj.groups.add(*group_objs) # add the group object to the user
+                        user_obj.save()
+                    except OperationalError:
+                        print("line110")
+                        print("jumped to line 64")
+                        username = row[0]
+                        full_name = row[1] 
+                        full_name_list = full_name.split()
+                        first_name = full_name_list[0]
+                    
+                      
+                        if (len(full_name_list) > 1):
+                            last_name = full_name_list[1]
+                        
+                        else:
+                            last_name = "N/A"
+                        print("line124 my username is", username)
+                        print("line125 my fullname is", full_name)
+                            
+                        email = row[2] 
+                        is_active = True
+                        is_staff = False
+                        is_superuser = False
+                        groups = lab_name 
+
+                        # creates my user object to load data from csv to GUI
+                        # create user object
+                        group_objs = []
+                        for group in groups.split(','):
+                            group_obj, _ = Group.objects.get_or_create(name=group.strip()) # gets u the group object based on the group name
+                            group_objs.append(group_obj)
+
+                        
+                        user_obj = User.objects.create(
+                            username=username,
+                            first_name=first_name,
+                            last_name=last_name,
+                            email=email,
+                            is_active=is_active,
+                            is_staff=is_staff,
+                            is_superuser=is_superuser,
+                        )
+                        # # add user to group
                         if group_objs:
                             user_obj.groups.add(*group_objs) # add the group object to the user
                         user_obj.save()
