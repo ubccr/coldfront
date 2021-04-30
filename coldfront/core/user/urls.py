fb4d59d04d9615345c9816de5b1fcc904dd746bc
Matthew Li <matthew.elbert.li@gmail.com>
2021-04-29 19:31:38 -0700

    Use custom form for resetting password to use EmailAddress so that any verified address can be used

--- coldfront/core/user/urls.py
+++ coldfront/core/user/urls.py
@@ -7,6 +7,7 @@ from django.contrib.auth.views import PasswordResetView
 from django.urls import path, reverse_lazy
 
 import coldfront.core.user.views as user_views
+from coldfront.core.user.forms import VerifiedEmailAddressPasswordResetForm
 from coldfront.core.user.forms import UserLoginForm
 
 EXTRA_APPS = settings.EXTRA_APPS
@@ -42,6 +43,7 @@ urlpatterns = [
     path('password-change/', user_views.CustomPasswordChangeView.as_view(), name='password-change'),
     path('password-reset/',
          PasswordResetView.as_view(
+             form_class=VerifiedEmailAddressPasswordResetForm,
              template_name='user/passwords/password_reset_form.html',
              email_template_name='user/passwords/password_reset_email.html',
              subject_template_name='user/passwords/password_reset_subject.txt',
