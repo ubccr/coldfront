8e16bd514a46ecc141882c1de9cd0041dca36da4
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2021-07-02 10:39:21 -0400

    Updated ifxbilling to get object code substitution Setup FACILITY class in settings Moved IFX_APP to plugin config

--- coldfront/config/plugins/ifx.py
+++ coldfront/config/plugins/ifx.py
@@ -2,7 +2,7 @@
 Config for ifxbilling
 Installs ifxuser, ifxbilling, and author.  Sets AUTH_USER_MODEL
 '''
-
+import os
 from coldfront.config.base import INSTALLED_APPS, MIDDLEWARE
 
 # INSTALLED_APPS = ['ifxuser'] + INSTALLED_APPS + ['author', 'ifxbilling']
@@ -12,3 +12,12 @@ MIDDLEWARE += [
 ]
 
 NANITE2USER_CLASS = 'coldfront.plugins.ifx.nanites.Nanite2ColdfrontUser'
+
+IFX_APP = {
+    'name': 'coldfront',
+    'token': os.environ.get('COLDFRONT_IFX_APP_TOKEN', 'aslkdfj'),
+}
+
+class FACILITY():
+    NAME = 'Research Computing Storage'
+    OBJECT_CODE = '6803'
