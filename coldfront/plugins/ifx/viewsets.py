895942a50db8486bfdb430b58baf01df0ff7a5ac
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2021-07-07 09:52:12 -0400

    Added authentication to billing viewset, rest framework token authentication Moved more stuff to plugin settings Updated ifxbilling to support improved billing and transaction serializers

---
+++ coldfront/plugins/ifx/viewsets.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+'''
+Viewsets
+'''
+from ifxbilling.serializers import BillingRecordViewSet
+from rest_framework.authentication import TokenAuthentication
+from coldfront.plugins.ifx.permissions import FiineBillingRecordViewSetPermissions
+
+class ColdfrontBillingRecordViewSet(BillingRecordViewSet):
+    '''
+    Set FiineOrAdmin permisisons
+    '''
+    permission_classes = [FiineBillingRecordViewSetPermissions]
+    authentication_classes = [TokenAuthentication]
