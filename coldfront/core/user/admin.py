b62a23d79562bf722c567138d2160c896780d8e5
JoJo Feinstein <joseph_feinstein@brown.edu>
2021-09-20 10:37:35 -0400

    fixed line length

--- coldfront/core/user/admin.py
+++ coldfront/core/user/admin.py
@@ -34,23 +34,28 @@ class EmailAddressAdmin(admin.ModelAdmin):
     # custom logic for delete button in emailaddress admin form
     def delete_model(self, request, obj):
         if obj.is_primary:
-            raise ValidationError('Cannot delete primary email. Unset primary status in list display before deleting.')
+            raise ValidationError('Cannot delete primary email. Unset primary '
+                                  'status in list display before deleting.')
         else:
             super().delete_model(request, obj)
 
-    # custom action to give an email primary status without causing conflicting states
+    # custom action to give an email primary status without
+    # causing conflicting states
     @admin.action(description='Make selected primary email')
     def make_primary(self, request, queryset):
-        # (a) unset the current primary, (b) set the selected one as primary, and (c) update user.email
+        # (a) unset the current primary, (b) set the selected one
+        # as primary, and (c) update user.email
         for emailaddress in queryset:
-            for primary_email in EmailAddress.objects.filter(user=emailaddress.user).filter(is_primary=True):
+            for primary_email in EmailAddress.objects.filter\
+                        (user=emailaddress.user, is_primary=True):
                 primary_email.is_primary = False
                 primary_email.save()
 
             emailaddress.is_primary = True
             emailaddress.save()
 
-    # ensure that the "delete selected" on list display only deletes non primary emails
+    # ensure that the "delete selected" on list display
+    # only deletes non primary emails
     def delete_queryset(self, request, queryset):
         queryset.filter(is_primary=False).delete()
 
