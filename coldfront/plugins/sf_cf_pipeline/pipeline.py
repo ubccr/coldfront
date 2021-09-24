0e5a287381254080bc1f642c0582adea76bc118e
Claire Peters <clairepeters@g.harvard.edu>
2021-09-24 11:41:58 -0700

    add AllocationUser update methods

--- coldfront/plugins/sf_cf_pipeline/pipeline.py
+++ coldfront/plugins/sf_cf_pipeline/pipeline.py
@@ -3,8 +3,17 @@ import requests
 import os
 import time
 import json
-from pynput import keyboard
+import timeit
+from pathlib import Path
+# from tqdm import tqdm
+# from pynput import keyboard
 from datetime import datetime
+import re
+from ifxuser.models import IfxUser
+from coldfront.core.project.models import Project
+from coldfront.core.allocation.models import (Allocation, AllocationUser)
+
+
 import logging
 
 
@@ -97,16 +106,35 @@ class Server():
         query = Query(self.headers, self.api_url, query, group_by, volpath, sec=sec)
         return query
 
-    def get_vol_users(self, volume):
-        logger.debug("get_vol_users")
+    def get_vol_user_membership(self, volume):
+        logger.debug("get_vol_user_membership")
         usermap_url = self.api_url+"mapping/user_membership?volume_name="+volume
         userlist = return_get_json(usermap_url, self.headers)
+        logger.debug(f"userlist:\n{userlist}")
         return userlist
 
+    def get_vol_user_name_ids(self, volume):
+        logger.debug("get_vol_user_name_ids")
+        usermap_url = self.api_url+"mapping/user?volume_name="+volume
+        users = return_get_json(usermap_url, self.headers)
+        userdict = {u['uid']:u['name'] for u in users}
+        logger.debug(f"userdict:\n{userdict}")
+        return userdict
 
-    # def add_user_ids(self, usagejson):
-    #     for userdict in usagejson:
-    #         username = userdict['username']
+    def get_vol_group_membership(self, volume):
+        logger.debug("get_vol_groups")
+        group_url = self.api_url+"mapping/group_membership?volume_name="+volume
+        grouplist = return_get_json(group_url, self.headers)
+        logger.debug(f"grouplist:\n{grouplist}")
+        return grouplist
+
+
+class User():
+
+
+    def translate_user_id(self, userid, usertable):
+        for user in usertable:
+            username = userdict['username']
 
 
 class Query():
@@ -114,7 +142,7 @@ class Query():
         self.api_url = api_url
         self.headers = headers
         self.query_id = self.post_async_query(query, group_by, volpath)
-        self.result = self.return_results_once_prepared(sec=sec)
+        self.result = self.return_results_once_prepared(maxsec=sec)
 
 
     def post_async_query(self, query, group_by, volpath):
@@ -144,9 +172,10 @@ class Query():
 
 
     # 4. return query results
-    def return_results_once_prepared(self, sec=3):
+    def return_results_once_prepared(self, maxsec=3):
         logger.debug("return_results_once_prepared")
         # add a means of triggering query deletion
+        sec = 1
         while True:
             # delete_query = False
             # with keyboard.Listener(on_press=self.on_press) as listener:
@@ -154,13 +183,16 @@ class Query():
             query_check_url = self.api_url+"async/query/"+self.query_id
             response = return_get_json(query_check_url, self.headers)
             if response["is_done"] == True:
-                print("query complete!")
+                # print("query complete!")
                 result = self.return_query_result()
                 return result
             else:
-                print("waiting for query...")
-                print(response)
+
+                # print("waiting for query...")
+                # print(response)
                 time.sleep(sec)
+                if sec < maxsec:
+                    sec += 1
                 # listener.join()
 
 
@@ -180,15 +212,55 @@ class Query():
         return response
 
 
-class User():
+class UsageStat():
     def __init__(self, userdict):
-        user.username = userdict['name']
-        user.id = userdict['uid']
-        user.volume = userdict['volume']
-        user.groups = userdict['gids']
+        self.count = userdict['count']
+        self.groupname = userdict['groupname']
+        self.size_sum = userdict['size_sum']
+        self.size_sum_hum = userdict['size_sum_hum']
+        self.username = userdict['username']
+        self.volume = userdict['volume']
 
     def get_usage(self):
         pass
+
+# @singleton
+class ColdFrontDB():
+    def __init__(self):
+        self.db = os.environ.get("coldfrontdb")
+
+    def locate_uid(self, username):
+        user = IfxUser.objects.get(username=username)
+        return user.id
+
+    def locate_pid(self, labname):
+        project = Project.objects.get(title=labname)
+        return project.id
+
+    def locate_aaid(self, pid):
+        allocation = Allocation.objects.get(project_id=pid)
+        return allocation.id
+
+
+    def update_usage(self, userdict):
+        # get allocationuser entry id
+        user_id = self.locate_uid(userdict['username'])
+        project_id = self.locate_pid(userdict['groupname'])
+        allocation_id = self.locate_aaid(project_id)
+
+        allocationuser = AllocationUser.objects.get(allocation_id=allocation_id, user_id=user_id)
+        allocationuser.usage_bytes = userdict['size_sum']
+        usage, unit = split_num_string(userdict['size_sum_hum'])
+        allocationuser.usage = usage
+        allocationuser.unit = unit
+        # "modified" field is automatically updated, old record is
+        # automatically added to history
+        allocationuser.save()
+
+def split_num_string(x):
+    n = re.search('\d*\.?\d+',x).group()
+    s = x.replace(n, '')
+    return n, s
 
 
 def generate_volpath_strings(volpathdict):
@@ -206,49 +278,60 @@ def return_get_json(url, headers):
     return response.json()
 
 
+def collect_starfish_json(server, servername, volume, volumepath):
+    labs = server.get_vol_group_membership(volume)
+    usertable = server.get_vol_user_name_ids(volume)
+    usage_query_by_lab = []
+    queryline = "type=d"
+    dir_query = server.create_query(queryline, "groupname", f"{volume}:{volumepath}", sec=2)
+    logger.info(f"dir_query.result:\s{dir_query.result}")
+    present_labs = [d['groupname'] for d in dir_query.result]
+    full_labs = [lab for lab in labs if lab['name'] in present_labs]
+    logger.info(f"present_labs:\s{present_labs}")
+    # t = tqdm(full_labs)
+    for lab in full_labs:
+        # t.set_postfix_str("{}".format(lab))
+        # A Test
+        print(str(lab['name']))
+        queryline = "type=f groupname={}".format(str(lab['name']))
+        usage_query = server.create_query(queryline, "username, groupname",
+        f"{volume}:{volumepath}", sec=2)
+        result = usage_query.result
+        print(result)
+        if not result:
+            pass
+        elif type(result) is dict and "error" in result:
+            pass
+        else:
+            usage_query_by_lab.append(result)
+            # A test end
+    filecontents = {"server":servername, "volume":volume, "volumepath":volumepath, "contents":usage_query_by_lab}
+    return filecontents
+
+
 if __name__ == "__main__":
-    server = Server("holysfdb02")
-    usage_query = server.create_query("type=d",
-                            "username, groupname",
-                            "holylfs02:LABS", sec=5)
-    with open('usage_by_subdir_json.json', 'w') as fp:
-        json.dump(usage_query.result, fp, sort_keys=True, indent=4)
+    servername = "holysfdb01"
+    volume = "holylfs04"
+    volumepath = "HDD/C/LABS"
+    server = Server(servername)
 
-
-
-
-
-    ### Attempt 1 ###
-    # lab_query = server.create_query("type=d", "groupname", "holylfs02:LABS")
-    # logger.debug(f"lab_query.result:\n{lab_query.result}")
-    ## get names of groups to use in paths for user query ##
-    # dir_query = server.create_query("type=d", "groupname", "holylfs02:LABS")
-
-    ### Attempt 2 ###
-    # subpaths = server.get_subpaths("holylfs02/LABS")
-    # usage_by_subdir_json = {}
-    ### Attempt 2A ###
-    # for sp in subpaths:
-    #     logger.debug(f"subpath: {sp}")
-    #     path = "holylfs02:LABS/"+sp
-    #     user_query = server.create_query("type=f", "username", path)
-    #     usage_by_subdir_json[sp] = user_query.result
-    ### Attempt 2B ###
-    # print(subpaths)
-    # allpaths = ["holylfs02:LABS%2F"+sp+"%2F" for sp in subpaths]
-    # allpathstring = "/".join(allpaths)
-    # user_query = server.create_query("type=f", "username, groupname", allpathstring, sec=5)
-    # usage_by_subdir_json["query"] = user_query.result
-    #
-    # usage_by_subdir_json["datetime"] = str(datetime.utcnow())
-    # with open('usage_by_subdir_json.json', 'w') as fp:
-    #     json.dump(usage_by_subdir_json, fp, sort_keys=True, indent=4)
-
-
-    # for user in user_query_result:
-    #     username = user['username']
-    #     user['fullname'] = get_name(user['username'], lab['groupname'])
-
-
-    # query_with_user = server.add_user_ids(query_result)
-    # users = server.get_vol_users("holylfs02")
+    filepath = f'usage_by_subdir_json_{servername}_{volume}.json'
+    if Path(filepath).exists():
+        pass
+    else:
+        filecontents = collect_starfish_json(server, servername, volume,volumepath)
+        with open(filepath, 'w') as fp:
+            json.dump(filecontents, fp, sort_keys=True, indent=4)
+    coldfrontdb = ColdFrontDB()
+    with open(filepath, 'r') as myfile:
+        data = myfile.read()
+    usage_stats = json.loads(data)
+    usage_stats['contents'] = [i for l in usage_stats['contents'] for i in l]
+    for statdict in usage_stats['contents']:
+        statdict['volume'] = usage_stats['volume']
+        if statdict['groupname'] != "bicepdata_group" and statdict['username'] != "root":
+            logger.debug(statdict)
+            try:
+                coldfrontdb.update_usage(statdict)
+            except Exception as e:
+                logger.debug("EXCEPTION FOR LAST ENTRY: {}".format(e))
