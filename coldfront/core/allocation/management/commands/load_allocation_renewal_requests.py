35b2d129e983a5a096a19836489cd224fda28136
Matthew Li <matthew.elbert.li@gmail.com>
2021-10-23 12:51:15 -0700

    Also load in the original requester when loading renewal requests if it exists in the database

--- coldfront/core/allocation/management/commands/load_allocation_renewal_requests.py
+++ coldfront/core/allocation/management/commands/load_allocation_renewal_requests.py
@@ -6,6 +6,7 @@ from coldfront.core.project.models import ProjectUserRoleChoice
 from coldfront.core.project.models import ProjectUserStatusChoice
 from coldfront.core.project.utils_.renewal_utils import get_current_allocation_period
 from coldfront.core.project.utils_.renewal_utils import has_non_denied_renewal_request
+from coldfront.core.user.models import EmailAddress
 from coldfront.core.utils.common import utc_now_offset_aware
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
@@ -58,9 +59,9 @@ class Command(BaseCommand):
     @staticmethod
     def parse_input_file(file_path, allocation_period):
         """Given a path to a JSON input file and an AllocationPeriod,
-        partition the entries into three lists: (1) valid (User,
-        Project) pairs, (2) Users that have already renewed, and (3)
-        invalid input pairs."""
+        partition the entries into three lists: (1) valid (PI User,
+        Project, Requester User) tuples, (2) Users that have already
+        renewed, and (3) invalid input tuples."""
         with open(file_path, 'r') as f:
             pairs = json.load(f)
         pi_role = ProjectUserRoleChoice.objects.get(
@@ -68,12 +69,12 @@ class Command(BaseCommand):
         active_project_user_status = ProjectUserStatusChoice.objects.get(
             name='Active')
         valid, already_renewed, invalid = [], [], []
-        for pi_name, project_name in pairs:
+        for pi_name, project_name, requester_email, _ in pairs:
             try:
                 project = Project.objects.prefetch_related(
                     'projectuser_set').get(name=project_name)
             except Project.DoesNotExist:
-                invalid.append((pi_name, project_name))
+                invalid.append((pi_name, project_name, requester_email))
                 continue
             pi_name_parts = pi_name.split()
             first_name, last_name = pi_name_parts[0], pi_name_parts[-1]
@@ -81,19 +82,27 @@ class Command(BaseCommand):
                 pi_user = User.objects.get(
                     first_name=first_name, last_name=last_name)
             except (User.DoesNotExist, User.MultipleObjectsReturned):
-                invalid.append((pi_name, project_name))
+                invalid.append((pi_name, project_name, requester_email))
                 continue
             try:
                 project.projectuser_set.get(
                     role=pi_role, status=active_project_user_status,
                     user=pi_user)
             except ProjectUser.DoesNotExist:
-                invalid.append((pi_name, project_name))
+                invalid.append((pi_name, project_name, requester_email))
                 continue
             if has_non_denied_renewal_request(pi_user, allocation_period):
                 already_renewed.append(pi_user)
                 continue
-            valid.append((pi_user, project))
+            # Retrieve the requester by email address. If none exists, use the
+            # PI as the requester.
+            try:
+                email_address = EmailAddress.objects.get(email=requester_email)
+            except EmailAddress.DoesNotExist:
+                requester_user = pi_user
+            else:
+                requester_user = email_address.user
+            valid.append((pi_user, project, requester_user))
         return valid, already_renewed, invalid
 
     def process_already_renewed_pis(self, already_renewed, allocation_period):
@@ -109,34 +118,41 @@ class Command(BaseCommand):
             self.stdout.write(self.style.WARNING(message))
 
     def process_invalid_pairs(self, invalid_pairs):
-        """Given a list of (pi_name, project_name) pairs, where the PI
-        and/or the Project are not valid, write to stderr."""
-        for pi_name, project_name in invalid_pairs:
-            message = f'Invalid pair: ({pi_name}, {project_name}). Skipping.'
+        """Given a list of (pi_name, project_name, requester_user)
+        tuples, where the PI and/or the Project are not valid, write to
+        stderr."""
+        for pi_name, project_name, requester_email in invalid_pairs:
+            message = (
+                f'Invalid tuple: ({pi_name}, {project_name}, '
+                f'{requester_email}). Skipping.')
             self.stderr.write(self.style.ERROR(message))
 
     def process_valid_pairs(self, valid, allocation_period, dry_run):
-        """Given a list of valid (User, Project) pairs, create an
-        AllocationRenewalRequest under the given AllocationPeriod with
-        status 'Complete', and write to stdout.
+        """Given a list of valid (PI User, Project, Requester User)
+        pairs, create an AllocationRenewalRequest under the given
+        AllocationPeriod with status 'Complete', and write to stdout.
 
-        If dry_run is True, write the pair to stdout without creating
+        If dry_run is True, write the triple to stdout without creating
         the request."""
         complete_renewal_status = \
             AllocationRenewalRequestStatusChoice.objects.get(name='Complete')
-        for pi_user, project in valid:
+        for pi_user, project, requester_user in valid:
+            requester_str = (
+                f'{requester_user.first_name} {requester_user.last_name} '
+                f'({requester_user.email})')
             pi_str = (
                 f'{pi_user.first_name} {pi_user.last_name} ({pi_user.email})')
             if dry_run:
                 message = (
                     f'Would create pre-completed AllocationRenewalRequest for '
-                    f'PI {pi_str}, Project {project.name}, and '
-                    f'AllocationPeriod {allocation_period.name}.')
+                    f'PI {pi_str}, Project {project.name}, requester '
+                    f'{requester_str}, and AllocationPeriod '
+                    f'{allocation_period.name}.')
                 self.stdout.write(self.style.WARNING(message))
                 continue
             try:
                 request = AllocationRenewalRequest.objects.create(
-                    requester=pi_user,
+                    requester=requester_user,
                     pi=pi_user,
                     allocation_period=allocation_period,
                     status=complete_renewal_status,
@@ -149,13 +165,15 @@ class Command(BaseCommand):
             except Exception as e:
                 message = (
                     f'Failed to create AllocationRenewalRequest for PI '
-                    f'{pi_str} and Project {project.name}.')
+                    f'{pi_str}, Project {project.name}, and requester '
+                    f'{requester_str}.')
                 self.stderr.write(self.style.ERROR(message))
                 self.logger.exception(e)
             else:
                 message = (
                     f'Created pre-completed AllocationRenewalRequest '
                     f'{request.pk} for PI {pi_str}, Project {project.name}, '
-                    f'and AllocationPeriod {allocation_period.name}.')
+                    f'requester {requester_str} and AllocationPeriod '
+                    f'{allocation_period.name}.')
                 self.logger.info(message)
                 self.stdout.write(self.style.SUCCESS(message))
