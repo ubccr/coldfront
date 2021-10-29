64b0409541d66ca8867ef4c4448fa9b430575f34
Matthew Li <matthew.elbert.li@gmail.com>
2021-10-28 18:37:38 -0700

    Test that inactive projects cannot be pooled with in form

---
+++ coldfront/core/project/tests/test_forms/test_savio_project_pooled_project_selection_form.py
@@ -0,0 +1,31 @@
+from coldfront.core.project.forms import SavioProjectPooledProjectSelectionForm
+from coldfront.core.project.models import Project
+from coldfront.core.project.models import ProjectStatusChoice
+from coldfront.core.utils.tests.test_base import TestBase
+
+
+class TestSavioProjectPooledProjectSelectionForm(TestBase):
+    """A class for testing SavioProjectPooledProjectSelectionForm."""
+
+    def setUp(self):
+        """Set up test data."""
+        super().setUp()
+
+    def test_inactive_projects_not_included_in_project_field(self):
+        """Test that Projects with the 'Inactive' status are not
+        included in the choices for the 'project' field."""
+        active_name = 'fc_active_project'
+        active_status = ProjectStatusChoice.objects.get(name='Active')
+        active_project = Project.objects.create(
+            name=active_name, title=active_name, status=active_status)
+        inactive_name = 'fc_inactive_project'
+        inactive_status = ProjectStatusChoice.objects.get(name='Inactive')
+        Project.objects.create(
+            name=inactive_name, title=inactive_name, status=inactive_status)
+
+        form = SavioProjectPooledProjectSelectionForm(allocation_type='fc_')
+        project_field = form.fields['project']
+        self.assertEqual(len(project_field.queryset), 1)
+        self.assertEqual(project_field.queryset[0], active_project)
+
+    # TODO
