e8dac3c7999c73fe23dcfaf5b60130608cf7530d
JoJo Feinstein <joseph_feinstein@brown.edu>
2021-11-01 19:15:43 -0400

    added 3 new forms for project removal requests

--- coldfront/core/project/forms.py
+++ coldfront/core/project/forms.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django import forms
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
 
@@ -90,6 +91,7 @@ class ProjectRemoveUserForm(forms.Form):
     last_name = forms.CharField(max_length=150, required=False, disabled=True)
     email = forms.EmailField(max_length=100, required=False, disabled=True)
     role = forms.CharField(max_length=30, disabled=True)
+    status = forms.CharField(max_length=50, required=False, disabled=True)
     selected = forms.BooleanField(initial=False, required=False)
 
 
@@ -1078,3 +1080,37 @@ class VectorProjectReviewSetupForm(forms.Form):
             raise forms.ValidationError(
                 f'A project with name {final_name} already exists.')
         return final_name
+
+
+class ProjectRemovalRequestSearchForm(forms.Form):
+    project_name = forms.CharField(label='Project Title',
+                                   max_length=100, required=False)
+    username = forms.CharField(
+        label='User Username', max_length=100, required=False)
+    requester = forms.CharField(
+        label='Requester Username', max_length=100, required=False)
+    email = forms.CharField(label='User Email', max_length=100, required=False)
+    show_all_requests = forms.BooleanField(initial=True, required=False)
+
+
+class ProjectRemovalRequestUpdateStatusForm(forms.Form):
+
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Processing', 'Processing'),
+    ]
+
+    status = forms.ChoiceField(
+        label='Status', choices=STATUS_CHOICES, required=True,
+        widget=forms.Select())
+
+
+class ProjectRemovalRequestCompletionForm(forms.Form):
+    STATUS_CHOICES = [
+        ('Processing', 'Processing'),
+        ('Complete', 'Complete')
+    ]
+
+    status = forms.ChoiceField(
+        label='Status', choices=STATUS_CHOICES, required=True,
+        widget=forms.Select())
