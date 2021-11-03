0b278b799e6d3dd52551c300354f308450aaecc7
Matthew Kusz <mkusz@iu.edu>
2021-11-03 12:54:55 -0400

    Add title and department to UserProfile

--- coldfront/core/user/models.py
+++ coldfront/core/user/models.py
@@ -5,3 +5,5 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_pi = models.BooleanField(default=False)
+    department = models.CharField(max_length=100, default='')
+    title = models.CharField(max_length=30, default='')
--- coldfront/core/user/signals.py
+++ coldfront/core/user/signals.py
@@ -4,14 +4,27 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from coldfront.core.user.models import UserProfile
+from django.utils.module_loading import import_string
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        ldap_search = import_string('coldfront.plugins.ldap_user_search.utils.LDAPSearch')
+        search_class_obj = ldap_search()
+        attributes = search_class_obj.search_a_user(instance.username, ['title', 'department'])
+        UserProfile.objects.create(
+            user=instance,
+            title=attributes['title'][0],
+            department=attributes['department'][0]
+        )
 
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
+    ldap_search = import_string('coldfront.plugins.ldap_user_search.utils.LDAPSearch')
+    search_class_obj = ldap_search()
+    attributes = search_class_obj.search_a_user(instance.username, ['title', 'department'])
+    instance.userprofile.title = attributes['title'][0]
+    instance.userprofile.department = attributes['department'][0]
     instance.userprofile.save()
