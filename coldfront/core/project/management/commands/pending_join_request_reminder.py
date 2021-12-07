e517c3bfb1f4b48edce06fccda5e27b950265f0e
jofeinstein <jofeinstein@gmail.com>
2021-12-07 17:01:06 -0500

    changed emails sent. removed time delta

--- coldfront/core/project/management/commands/pending_join_request_reminder.py
+++ coldfront/core/project/management/commands/pending_join_request_reminder.py
@@ -23,36 +23,97 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        proj_join_requests_qeuryset = ProjectUserJoinRequest.objects.filter(project_user__status__name='Pending - Add')
+        proj_join_requests_qeuryset = \
+            ProjectUserJoinRequest.objects.filter(
+                project_user__status__name='Pending - Add')
+
+        projects_with_pending_join_requests = set([request.project_user.project for request in proj_join_requests_qeuryset])
+        users_with_pending_join_requests = set([request.project_user.user for request in proj_join_requests_qeuryset])
 
         emails_sent = 0
-        for request in proj_join_requests_qeuryset:
-            creation_time = request.created
-            now = utc_now_offset_aware()
-            diff = now - creation_time
+        for project in projects_with_pending_join_requests:
+            proj_join_requests_qeuryset = \
+                ProjectUserJoinRequest.objects.filter(
+                    project_user__project=project,
+                    project_user__status__name='Pending - Add')
 
-            if diff > TIME_DELTA and settings.EMAIL_ENABLED:
-                subject = 'Pending Project Join Requests'
-                template_names = ['email/pending_project_join_requests.txt',
-                                  'email/pending_project_join_request_user.txt']
+            if settings.EMAIL_ENABLED:
+                request_string_list = [f'{request.project_user.user.first_name} ' \
+                                       f'{request.project_user.user.last_name} | ' \
+                                       f'{request.project_user.user.email} | ' \
+                                       f'{request.created.strftime("%m/%d/%Y, %H:%M")}'
+                                       for request in proj_join_requests_qeuryset]
+
                 context = {
-                    'project_name': request.project_user.project.name,
-                    'user_first_name': request.project_user.user.first_name,
-                    'user_last_name': request.project_user.user.last_name,
-                    'user_email': request.project_user.user.email,
+                    'project_name': project.name,
+                    'request_list': '\n'.join(request_string_list),
+                    'num_requests': proj_join_requests_qeuryset.count(),
+                    'verb': 'are' if proj_join_requests_qeuryset.count() > 1 else 'is',
+                    'pk': project.pk,
                     'signature': settings.EMAIL_SIGNATURE
                 }
-                sender = settings.EMAIL_SENDER
-                manager_pi_set = request.project_user.project.projectuser_set.filter(
+                manager_pi_set = project.projectuser_set.filter(
                     role__name__in=['Manager', 'Principal Investigator'],
                     status__name='Active')
-                receiver_list = [[proj_user.user.email for proj_user in manager_pi_set],
-                                 [request.project_user.user.email]]
+                recipients = [proj_user.user.email for proj_user in manager_pi_set]
                 try:
-                    for template, recipients in zip(template_names, receiver_list):
-                        send_email_template(
-                            subject, template, context, sender, recipients)
-                        emails_sent += len(recipients)
+                    # send_email_template(
+                    #     'Pending Project Join Requests',
+                    #     'email/pending_project_join_requests.txt',
+                    #     context,
+                    #     settings.EMAIL_SENDER,
+                    #     recipients)
+                    # emails_sent += len(recipients)
+
+                    from django.core.mail import send_mail
+                    from django.template.loader import render_to_string
+
+
+                    msg_plain = render_to_string('email/pending_project_join_requests.txt', context)
+                    msg_html = render_to_string('email/pending_project_join_requests.html', context)
+
+                    send_mail(
+                        'Pending Project Join Requests',
+                        msg_plain,
+                        settings.EMAIL_SENDER,
+                        recipients,
+                        html_message=msg_html,
+                    )
+                    print(recipients)
+                    emails_sent += len(recipients)
+                except Exception as e:
+                    message = 'Failed to send reminder email. Details:'
+                    self.stderr.write(self.style.ERROR(message))
+                    self.stderr.write(self.style.ERROR(str(e)))
+                    self.logger.error(message)
+                    self.logger.exception(e)
+
+        for user in users_with_pending_join_requests:
+            proj_join_requests_qeuryset = \
+                ProjectUserJoinRequest.objects.filter(
+                    project_user__user=user,
+                    project_user__status__name='Pending - Add')
+
+            if settings.EMAIL_ENABLED:
+                request_string_list = [f'{request.project_user.project.name} | ' \
+                                       f'{request.created.strftime("%m/%d/%Y, %H:%M")}'
+                                       for request in proj_join_requests_qeuryset]
+
+                context = {
+                    'user_name': f'{user.first_name} {user.last_name}',
+                    'request_list': '\n'.join(request_string_list),
+                    'num_requests': proj_join_requests_qeuryset.count(),
+                    'signature': settings.EMAIL_SIGNATURE
+                }
+
+                try:
+                    send_email_template(
+                        'Pending Project Join Requests',
+                        'email/pending_project_join_request_user.txt',
+                        context,
+                        settings.EMAIL_SENDER,
+                        [user.email])
+                    emails_sent += 1
                 except Exception as e:
                     message = 'Failed to send reminder email. Details:'
                     self.stderr.write(self.style.ERROR(message))
