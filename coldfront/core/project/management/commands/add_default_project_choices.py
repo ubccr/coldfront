41b2bc6a7937bee6d9ca6ce9b9adff4df4a1d7d9
Matthew Kusz <mkusz@iu.edu>
2021-12-09 14:17:05 -0500

    Update project command

--- coldfront/core/project/management/commands/add_default_project_choices.py
+++ coldfront/core/project/management/commands/add_default_project_choices.py
@@ -11,21 +11,22 @@ class Command(BaseCommand):
     help = 'Add default project related choices'
 
     def handle(self, *args, **options):
-        #ProjectStatusChoice.objects.all().delete()
-        for choice in ['New', 'Active', 'Archived', ]:
+        ProjectStatusChoice.objects.all().delete()
+        for choice in ['New', 'Active', 'Archived', 'Denied', 'Expired', 'Review Pending', 'Waiting For Admin Approval', ]:
             ProjectStatusChoice.objects.get_or_create(name=choice)
 
-        #ProjectReviewStatusChoice.objects.all().delete()
-        for choice in ['Completed', 'Pending', ]:
+        ProjectReviewStatusChoice.objects.all().delete()
+        for choice in ['Approved', 'Pending', 'Denied', ]:
             ProjectReviewStatusChoice.objects.get_or_create(name=choice)
 
-        #ProjectUserRoleChoice.objects.all().delete()
-        for choice in ['User', 'Manager', ]:
+        ProjectUserRoleChoice.objects.all().delete()
+        for choice in ['User', 'Manager', 'Group', ]:
             ProjectUserRoleChoice.objects.get_or_create(name=choice)
 
-        #ProjectUserStatusChoice.objects.all().delete()
+        ProjectUserStatusChoice.objects.all().delete()
         for choice in ['Active', 'Pending - Add', 'Pending - Remove', 'Denied', 'Removed', ]:
             ProjectUserStatusChoice.objects.get_or_create(name=choice)
 
+        ProjectTypeChoice.objects.all().delete()
         for choice in ['Research', 'Class', ]:
             ProjectTypeChoice.objects.get_or_create(name=choice)
