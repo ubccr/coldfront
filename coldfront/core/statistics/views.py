c4548e7656658014c31a7e6f918ea927365383ce
jofeinstein <jofeinstein@gmail.com>
2022-01-19 15:49:58 -0500

    fixed issue in test_func of SlurmJobDetailView

--- coldfront/core/statistics/views.py
+++ coldfront/core/statistics/views.py
@@ -165,7 +165,7 @@ class SlurmJobDetailView(LoginRequiredMixin,
 
         job_obj = self.get_object()
 
-        if job_obj.userid.projectuser_set.filter(
+        if job_obj.accountid.projectuser_set.filter(
                 user=self.request.user,
                 status__name__in=['Active', 'Pending - Remove'],
                 role__name__in=['Principal Investigator', 'Manager']).exists():
