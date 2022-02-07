f2cca0fe8c20a55006dbd624e75f49f2dce62527
jofeinstein <jofeinstein@gmail.com>
2022-02-07 11:35:47 -0500

    updated tests for user_list. need to remake tests for new account list

--- coldfront/core/utils/tests/test_export_data.py
+++ coldfront/core/utils/tests/test_export_data.py
@@ -15,6 +15,8 @@ import os
 import sys
 
 from coldfront.config import settings
+from coldfront.core.allocation.models import AllocationAttributeType, \
+    AllocationUserAttribute
 from coldfront.core.statistics.models import Job
 from coldfront.core.user.models import UserProfile
 from coldfront.core.utils.common import utc_now_offset_aware
@@ -89,13 +91,14 @@ class TestUserList(TestBase2):
         out.seek(0)
         output = json.loads(''.join(out.readlines()))
 
-        job_list = Job.objects.annotate(str_submitdate=Func(
+        job_list = Job.objects.annotate(submit_date=Func(
             F('submitdate'),
             Value('MM-dd-yyyy hh:mm:ss'),
             function='to_char',
-            output_field=CharField()
-        )).order_by('userid', '-submitdate').distinct('userid').values(
-            'userid__username', 'jobslurmid', 'str_submitdate')
+            output_field=CharField()),
+            username=F('userid__username')).\
+            order_by('userid', '-submitdate').distinct('userid').\
+            values('username', 'jobslurmid', 'submit_date')
 
         for index, item in enumerate(output):
             self.assertDictEqual(item, job_list[index])
@@ -112,19 +115,20 @@ class TestUserList(TestBase2):
 
         out, err = StringIO(''), StringIO('')
         call_command('export_data', 'user_list', '--format=json',
-                     f'--date={start_date}', stdout=out, stderr=err)
+                     f'--start_date={start_date}', stdout=out, stderr=err)
         sys.stdout = sys.__stdout__
 
         out.seek(0)
         output = json.loads(''.join(out.readlines()))
 
-        job_list = Job.objects.annotate(str_submitdate=Func(
+        job_list = Job.objects.annotate(submit_date=Func(
             F('submitdate'),
             Value('MM-dd-yyyy hh:mm:ss'),
             function='to_char',
-            output_field=CharField()
-        )).values(
-            'userid__username', 'jobslurmid', 'str_submitdate').\
+            output_field=CharField()),
+            username=F('userid__username')). \
+            order_by('userid', '-submitdate').distinct('userid'). \
+            values('username', 'jobslurmid', 'submit_date').\
             get(jobslurmid='12345')
 
         for index, item in enumerate(output):
@@ -150,17 +154,18 @@ class TestUserList(TestBase2):
         out.seek(0)
         reader = csv.reader(out.readlines())
 
-        job_list = Job.objects.annotate(str_submitdate=Func(
+        job_list = Job.objects.annotate(submit_date=Func(
             F('submitdate'),
             Value('MM-dd-yyyy hh:mm:ss'),
             function='to_char',
-            output_field=CharField()
-        )).order_by('userid', '-submitdate').distinct('userid').\
-            values_list('userid__username', 'jobslurmid', 'str_submitdate')
+            output_field=CharField()),
+            username=F('userid__username')).\
+            order_by('userid', '-submitdate').distinct('userid').\
+            values_list('username', 'jobslurmid', 'submit_date')
 
         for index, item in enumerate(reader):
             if index == 0:
-                lst = ['user__username', 'last_job_id', 'last_job_submitdate']
+                lst = ['username', 'jobslurmid', 'submit_date']
             else:
                 lst = list(job_list[index-1])
 
@@ -178,24 +183,25 @@ class TestUserList(TestBase2):
 
         out, err = StringIO(''), StringIO('')
         call_command('export_data', 'user_list', '--format=csv',
-                     f'--date={start_date}', stdout=out, stderr=err)
+                     f'--start_date={start_date}', stdout=out, stderr=err)
         sys.stdout = sys.__stdout__
 
         out.seek(0)
         reader = csv.reader(out.readlines())
 
-        job_list = Job.objects.annotate(str_submitdate=Func(
+        job_list = Job.objects.annotate(submit_date=Func(
             F('submitdate'),
             Value('MM-dd-yyyy hh:mm:ss'),
             function='to_char',
-            output_field=CharField()
-        )).order_by('userid', '-submitdate').distinct('userid').\
-            values_list('userid__username', 'jobslurmid', 'str_submitdate').\
+            output_field=CharField()),
+            username=F('userid__username')). \
+            order_by('userid', '-submitdate').distinct('userid'). \
+            values_list('username', 'jobslurmid', 'submit_date').\
             get(jobslurmid='12345')
 
         for index, item in enumerate(reader):
             if index == 0:
-                lst = ['user__username', 'last_job_id', 'last_job_submitdate']
+                lst = ['username', 'jobslurmid', 'submit_date']
             else:
                 lst = list(job_list)
 
@@ -205,157 +211,166 @@ class TestUserList(TestBase2):
         self.assertEqual(err.read(), '')
 
 
-class TestNewUserAccount(TestBase2):
-    """Test class to test export data subcommand new_user_account runs
-    correctly."""
-
-    def setUp(self):
-        """Setup test data"""
-        super().setUp()
-
-    def convert_time_to_utc(self, time):
-        """Convert naive LA time to UTC time"""
-        local_tz = pytz.timezone('America/Los_Angeles')
-        tz = pytz.timezone(settings.TIME_ZONE)
-        naive_dt = datetime.datetime.combine(time, datetime.datetime.min.time())
-        new_time = local_tz.localize(naive_dt).astimezone(tz).isoformat()
-
-        return new_time
-
-    def test_test_new_user_account_json_no_date(self):
-        """Testing new_user_account subcommand with NO date arg passed,
-        exporting as JSON"""
-
-        out, err = StringIO(''), StringIO('')
-        call_command('export_data', 'new_user_account', '--format=json',
-                     stdout=out, stderr=err)
-        sys.stdout = sys.__stdout__
-
-        out.seek(0)
-        output = json.loads(''.join(out.readlines()))
-
-        user_list = User.objects.annotate(str_date_joined=Func(
-            F('date_joined'),
-            Value('MM-dd-yyyy hh:mm:ss'),
-            function='to_char',
-            output_field=CharField()
-        )).order_by('username', '-date_joined'). \
-            distinct('username').values('username', 'str_date_joined')
-
-        for index, item in enumerate(output):
-            self.assertDictEqual(item, user_list[index])
-
-        err.seek(0)
-        self.assertEqual(err.read(), '')
-
-    def test_test_new_user_account_json_with_date(self):
-        """Testing new_user_account subcommand with ONE date arg passed,
-        exporting as JSON"""
-
-        start_date = datetime.datetime.strftime(
-            self.current_time - datetime.timedelta(days=4), '%m-%d-%Y')
-
-        new_date = self.convert_time_to_utc(self.current_time -
-                                            datetime.timedelta(days=10))
-        self.user2.date_joined = new_date
-        self.user2.save()
-        self.assertEqual(self.user2.date_joined, new_date)
-
-        out, err = StringIO(''), StringIO('')
-        call_command('export_data', 'new_user_account', '--format=json',
-                     f'--date={start_date}', stdout=out, stderr=err)
-        sys.stdout = sys.__stdout__
-
-        out.seek(0)
-        output = json.loads(''.join(out.readlines()))
-
-        user_list = User.objects.annotate(str_date_joined=Func(
-            F('date_joined'),
-            Value('MM-dd-yyyy hh:mm:ss'),
-            function='to_char',
-            output_field=CharField()
-        )).order_by('username', '-date_joined'). \
-            distinct('username').values('username', 'str_date_joined').get(
-            username=self.user1.username
-        )
-
-        for index, item in enumerate(output):
-            self.assertDictEqual(item, user_list)
-
-        err.seek(0)
-        self.assertEqual(err.read(), '')
-
-    def test_test_new_user_account_csv_no_date(self):
-        """Testing new_user_account subcommand with NO date arg passed,
-        exporting as CSV"""
-
-        out, err = StringIO(''), StringIO('')
-        call_command('export_data', 'new_user_account', '--format=csv',
-                     stdout=out, stderr=err)
-        sys.stdout = sys.__stdout__
-
-        out.seek(0)
-        reader = csv.reader(out.readlines())
-
-        user_list = User.objects.annotate(str_date_joined=Func(
-            F('date_joined'),
-            Value('MM-dd-yyyy hh:mm:ss'),
-            function='to_char',
-            output_field=CharField()
-        )).order_by('username', '-date_joined'). \
-            distinct('username').values_list('username', 'str_date_joined')
-
-        for index, item in enumerate(reader):
-            if index == 0:
-                lst = ['username', 'str_date_joined']
-            else:
-                lst = list(user_list[index-1])
-
-            self.assertEqual(item, lst)
-
-            err.seek(0)
-            self.assertEqual(err.read(), '')
-
-    def test_test_new_user_account_csv_with_date(self):
-        """Testing new_user_account subcommand with ONE date arg passed,
-        exporting as CSV"""
-        start_date = datetime.datetime.strftime(
-            self.current_time - datetime.timedelta(days=4), '%m-%d-%Y')
-
-        new_date = self.convert_time_to_utc(self.current_time -
-                                            datetime.timedelta(days=10))
-        self.user2.date_joined = new_date
-        self.user2.save()
-        self.assertEqual(self.user2.date_joined, new_date)
-
-        out, err = StringIO(''), StringIO('')
-        call_command('export_data', 'new_user_account', '--format=csv',
-                     f'--date={start_date}', stdout=out, stderr=err)
-        sys.stdout = sys.__stdout__
-
-        out.seek(0)
-        reader = csv.reader(out.readlines())
-
-        user_list = User.objects.annotate(str_date_joined=Func(
-            F('date_joined'),
-            Value('MM-dd-yyyy hh:mm:ss'),
-            function='to_char',
-            output_field=CharField()
-        )).order_by('username', '-date_joined'). \
-            distinct('username').values_list('username', 'str_date_joined').get(
-            username=self.user1.username
-        )
-
-        for index, item in enumerate(reader):
-            if index == 0:
-                lst = ['username', 'str_date_joined']
-            else:
-                lst = list(user_list)
-
-            self.assertEqual(item, lst)
-
-            err.seek(0)
-            self.assertEqual(err.read(), '')
+# class TestNewUserAccount(TestBase2):
+#     """Test class to test export data subcommand new_user_account runs
+#     correctly."""
+#
+#     def setUp(self):
+#         """Setup test data"""
+#         super().setUp()
+#
+#     def convert_time_to_utc(self, time):
+#         """Convert naive LA time to UTC time"""
+#         local_tz = pytz.timezone('America/Los_Angeles')
+#         tz = pytz.timezone(settings.TIME_ZONE)
+#         naive_dt = datetime.datetime.combine(time, datetime.datetime.min.time())
+#         new_time = local_tz.localize(naive_dt).astimezone(tz).isoformat()
+#
+#         return new_time
+#
+#     def test_test_new_user_account_json_no_date(self):
+#         """Testing new_user_account subcommand with NO date arg passed,
+#         exporting as JSON"""
+#
+#         out, err = StringIO(''), StringIO('')
+#         call_command('export_data', 'new_user_account', '--format=json',
+#                      stdout=out, stderr=err)
+#         sys.stdout = sys.__stdout__
+#
+#         out.seek(0)
+#         output = json.loads(''.join(out.readlines()))
+#
+#         cluster_account_status = AllocationAttributeType.objects.get(
+#             name='Cluster Account Status')
+#
+#         user_list = AllocationUserAttribute.objects.filter(
+#             allocation_attribute_type=cluster_account_status,
+#             value='Active')
+#
+#         user_list = User.objects.annotate(
+#             date_created=Func(
+#                 F('created'),
+#                 Value('MM-dd-yyyy hh:mm:ss'),
+#                 function='to_char',
+#                 output_field=CharField()),
+#             username=F('allocation_user__user__username')).\
+#             order_by('username', '-created'). \
+#             distinct('username').values('username', 'date_created')
+#
+#         for index, item in enumerate(output):
+#             self.assertDictEqual(item, user_list[index])
+#
+#         err.seek(0)
+#         self.assertEqual(err.read(), '')
+#
+#     def test_test_new_user_account_json_with_date(self):
+#         """Testing new_user_account subcommand with ONE date arg passed,
+#         exporting as JSON"""
+#
+#         start_date = datetime.datetime.strftime(
+#             self.current_time - datetime.timedelta(days=4), '%m-%d-%Y')
+#
+#         new_date = self.convert_time_to_utc(self.current_time -
+#                                             datetime.timedelta(days=10))
+#         self.user2.date_joined = new_date
+#         self.user2.save()
+#         self.assertEqual(self.user2.date_joined, new_date)
+#
+#         out, err = StringIO(''), StringIO('')
+#         call_command('export_data', 'new_user_account', '--format=json',
+#                      f'--date={start_date}', stdout=out, stderr=err)
+#         sys.stdout = sys.__stdout__
+#
+#         out.seek(0)
+#         output = json.loads(''.join(out.readlines()))
+#
+#         user_list = User.objects.annotate(str_date_joined=Func(
+#             F('date_joined'),
+#             Value('MM-dd-yyyy hh:mm:ss'),
+#             function='to_char',
+#             output_field=CharField()
+#         )).order_by('username', '-date_joined'). \
+#             distinct('username').values('username', 'str_date_joined').get(
+#             username=self.user1.username
+#         )
+#
+#         for index, item in enumerate(output):
+#             self.assertDictEqual(item, user_list)
+#
+#         err.seek(0)
+#         self.assertEqual(err.read(), '')
+#
+#     def test_test_new_user_account_csv_no_date(self):
+#         """Testing new_user_account subcommand with NO date arg passed,
+#         exporting as CSV"""
+#
+#         out, err = StringIO(''), StringIO('')
+#         call_command('export_data', 'new_user_account', '--format=csv',
+#                      stdout=out, stderr=err)
+#         sys.stdout = sys.__stdout__
+#
+#         out.seek(0)
+#         reader = csv.reader(out.readlines())
+#
+#         user_list = User.objects.annotate(str_date_joined=Func(
+#             F('date_joined'),
+#             Value('MM-dd-yyyy hh:mm:ss'),
+#             function='to_char',
+#             output_field=CharField()
+#         )).order_by('username', '-date_joined'). \
+#             distinct('username').values_list('username', 'str_date_joined')
+#
+#         for index, item in enumerate(reader):
+#             if index == 0:
+#                 lst = ['username', 'str_date_joined']
+#             else:
+#                 lst = list(user_list[index-1])
+#
+#             self.assertEqual(item, lst)
+#
+#             err.seek(0)
+#             self.assertEqual(err.read(), '')
+#
+#     def test_test_new_user_account_csv_with_date(self):
+#         """Testing new_user_account subcommand with ONE date arg passed,
+#         exporting as CSV"""
+#         start_date = datetime.datetime.strftime(
+#             self.current_time - datetime.timedelta(days=4), '%m-%d-%Y')
+#
+#         new_date = self.convert_time_to_utc(self.current_time -
+#                                             datetime.timedelta(days=10))
+#         self.user2.date_joined = new_date
+#         self.user2.save()
+#         self.assertEqual(self.user2.date_joined, new_date)
+#
+#         out, err = StringIO(''), StringIO('')
+#         call_command('export_data', 'new_user_account', '--format=csv',
+#                      f'--date={start_date}', stdout=out, stderr=err)
+#         sys.stdout = sys.__stdout__
+#
+#         out.seek(0)
+#         reader = csv.reader(out.readlines())
+#
+#         user_list = User.objects.annotate(str_date_joined=Func(
+#             F('date_joined'),
+#             Value('MM-dd-yyyy hh:mm:ss'),
+#             function='to_char',
+#             output_field=CharField()
+#         )).order_by('username', '-date_joined'). \
+#             distinct('username').values_list('username', 'str_date_joined').get(
+#             username=self.user1.username
+#         )
+#
+#         for index, item in enumerate(reader):
+#             if index == 0:
+#                 lst = ['username', 'str_date_joined']
+#             else:
+#                 lst = list(user_list)
+#
+#             self.assertEqual(item, lst)
+#
+#             err.seek(0)
+#             self.assertEqual(err.read(), '')
 
 
 class TestJobAvgQueueTime(TestBase2):
@@ -400,7 +415,7 @@ class TestJobAvgQueueTime(TestBase2):
     def test_job_avg_queue_time_errors(self):
         # invalid date error
         start_date = datetime.datetime.strftime(
-            self.current_time - datetime.timedelta(days=6), '%d-%m-%Y')
+            self.current_time - datetime.timedelta(days=6), '%Y-%d-%m')
         end_date = datetime.datetime.strftime(
             self.current_time - datetime.timedelta(days=4), '%m-%d-%Y')
 
