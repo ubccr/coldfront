72354d112cdde83b67470003b898e4135d2616b6
jofeinstein <jofeinstein@gmail.com>
2022-02-22 23:06:39 -0500

    added user inline to groups admin

--- coldfront/core/utils/admin.py
+++ coldfront/core/utils/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import GroupAdmin
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+
+class UserInLine(admin.TabularInline):
+    model = User.groups.through
+    readonly_fields = ('user',)
+    extra = 0
+    can_delete = False
+
+
+class GroupsAdmin(GroupAdmin):
+    list_display = ["name", "pk"]
+    inlines = [UserInLine]
+
+
+admin.site.unregister(Group)
+admin.site.register(Group, GroupsAdmin)
