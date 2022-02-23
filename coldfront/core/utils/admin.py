d55162e5e55c025ad2507facd323aa5d2295c432
jofeinstein <jofeinstein@gmail.com>
2022-02-22 23:16:56 -0500

    added EmailAddress inline to User admin page

--- coldfront/core/utils/admin.py
+++ coldfront/core/utils/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import GroupAdmin
+from django.contrib.auth.admin import GroupAdmin, UserAdmin
 from django.contrib.auth.models import Group, User
+
+from coldfront.core.user.models import EmailAddress
 
 
 class UserInLine(admin.TabularInline):
@@ -15,5 +17,18 @@ class GroupsAdmin(GroupAdmin):
     inlines = [UserInLine]
 
 
+class EmailAddressInLine(admin.TabularInline):
+    model = EmailAddress
+    extra = 0
+
+
+class UsersAdmin(UserAdmin):
+    list_display = ["username", "email", "first_name", "last_name", "is_staff"]
+    inlines = [EmailAddressInLine]
+
+
 admin.site.unregister(Group)
 admin.site.register(Group, GroupsAdmin)
+
+admin.site.unregister(User)
+admin.site.register(User, UsersAdmin)
