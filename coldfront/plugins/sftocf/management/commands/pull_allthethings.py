3f6bc7a4bcaa5035beae9db95d6adbba096e210b
geistling <34081638+geistling@users.noreply.github.com>
2022-03-15 19:09:36 -0700

    add working allthethings quota query

--- coldfront/plugins/sftocf/management/commands/pull_allthethings.py
+++ coldfront/plugins/sftocf/management/commands/pull_allthethings.py
@@ -1,17 +1,20 @@
-# Look at storagereport - pulls from AllTheThings
-#
-# poshbot.RCTools:StorageReport
-#
-# https://gitlab-int.rc.fas.harvard.edu/common/poshbot.rctools/-/blob/master/PoshBot.RCTools/Public/allthethings.ps1
-
+import os
+import re
 import json
+import logging
 import requests
 
-from coldfront.core.utils.common import import_from_settings
-import logging
+from django.core.management.base import BaseCommand, CommandError
 
+from coldfront.core.utils.common import import_from_settings
+from coldfront.core.allocation.models import Allocation, AllocationAttribute
 
 logger = logging.getLogger(__name__)
+
+with open("coldfront/plugins/sftocf/servers.json", "r") as myfile:
+    svp = json.loads(myfile.read())
+
+vol_search = "|".join([i for l in [v.keys() for s, v in svp.items()]for i in l])
 
 class Command(BaseCommand):
     '''
@@ -20,14 +23,35 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
 
-        neo4jp = import_from_settings('neo4jp')
+        neo4jp = os.environ['neo4jp']
+        print(neo4jp)
 
         url = "https://allthethings.rc.fas.harvard.edu:7473/db/data/transaction/commit"
 
-        data = '{"statements": [{"statement": "MATCH p=(g:Group)-[:Owns]-(e:Volume) RETURN datetime(e.DotsLVSUpdateDate) as update_date,  datetime(e.DotsLVDisplayUpdateDate) as display_date, null as end_date, g.ADSamAccountName as lab, e.LogicalVolume as fs_path, e.Hostname as server, \"Volume\" as storage_type, \"\" as backup, (e.SizeGB) as gb_allocation, (e.UsedGB) as gb_usage, e.filequota as files_quota,e.files as files,\"\" as status,\"\" as hw_model_id, \"\" as service_tier, \"\" as security_level, \"\" as note"}]}'
-        headers = { 'accept': 'application/json,charset=UTF-8', 'authorization': neo4jp, 'content-type': 'application/json', }
+        query = {"statements": [{"statement": f"MATCH (g:Group)--(n)\
+            WHERE (n:IsilonPath OR n:Quota) AND \
+            ((n.filesystem =~ '.*{vol_search}.*') OR (n.Isilon =~ '.*{vol_search}.*'))\
+            RETURN datetime(n.DotsLFSUpdateDate) as update_date,\
+            g.ADSamAccountName as lab,\
+            n.filesystem as lfs_volume, \
+            n.limitGB as lfs_limitgb,\
+            n.quotaGB as lfs_quotagb,\
+            n.Isilon as isi_volume,\
+            n.hasQuota as isi_hasquota,\
+            n.softGB as isi_softgb,\
+            n.hardGB as isi_hardgb"
+            }]
+        }
+        headers = { 'accept': 'application/json',
+                    'authorization': neo4jp,
+                    'content-type': 'application/json', }
 
-        resp = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
+        resp = requests.post(url, headers=headers, data=json.dumps(query), verify=False)
 
-        with open('allthethings_output.json', 'w') as f:
-            f.write(resp.text)
+        resp_json = json.loads(resp.text)
+        cols = resp_json['results'][0]['columns']
+        data = resp_json['results'][0]['data']
+        resp_json_formatted = [dict(zip(cols,rowdict['row'])) for rowdict in data]
+
+        with open('coldfront/plugins/sftocf/data/allthethings_output.json', 'w') as f:
+            f.write(json.dumps(resp_json_formatted, indent=2))
