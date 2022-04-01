b3d7982591855b0ef732bf1d928bf15964d8bd27
Matthew Kusz <mkusz@iu.edu>
2022-04-01 13:38:39 -0400

    Fix makemigration serialization error

--- coldfront/core/utils/validators.py
+++ coldfront/core/utils/validators.py
@@ -1,6 +1,8 @@
 from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
 
 
+@deconstructible
 class IsAlpha:
     message = 'The entry must not contain numbers or special characters'
     code = 'invalid'
@@ -12,3 +14,9 @@ class IsAlpha:
     def __call__(self, value):
         if not value.isalpha():
             raise ValidationError(self.message, self.code)
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, IsAlpha)
+            and (self.message == other.message)
+        )
