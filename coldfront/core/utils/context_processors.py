e35e0684450ab10492e5ae4b4e3b03efa1b4b70f
Matthew Li <matthew.elbert.li@gmail.com>
2022-04-06 20:06:47 -0700

    Display end of current allowance year in popup via a new context processor

--- coldfront/core/utils/context_processors.py
+++ coldfront/core/utils/context_processors.py
@@ -1,4 +1,20 @@
+from coldfront.core.allocation.models import AllocationPeriod
+from coldfront.core.utils.common import display_time_zone_current_date
+
 from django.conf import settings
+
+
+def current_allowance_year_allocation_period(request):
+    date = display_time_zone_current_date()
+    allocation_periods = AllocationPeriod.objects.filter(
+        name__startswith='Allowance Year',
+        start_date__lte=date,
+        end_date__gte=date).order_by('start_date')
+    context = {}
+    if allocation_periods.exists():
+        context['CURRENT_ALLOWANCE_YEAR_ALLOCATION_PERIOD'] = \
+            allocation_periods.first()
+    return context
 
 
 def display_time_zone(request):
