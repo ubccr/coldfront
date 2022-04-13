22645dfc2db13338d2bdd65fa35f6a057ed52329
jofeinstein <jofeinstein@gmail.com>
2022-04-13 12:32:32 -0400

    modified SecureDirAddUsersView to both add and remove users

--- coldfront/core/allocation/views_/secure_dir_views.py
+++ coldfront/core/allocation/views_/secure_dir_views.py
@@ -27,10 +27,10 @@ from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
 
-from coldfront.core.allocation.forms import AllocationSecureDirJoinForm
+from coldfront.core.allocation.forms import AllocationSecureDirManageUserForm
 from coldfront.core.allocation.models import Allocation, \
     SecureDirAddUserRequest, SecureDirAddUserRequestStatusChoice, \
-    SecureDirRemoveUserRequest
+    SecureDirRemoveUserRequest, SecureDirRemoveUserRequestStatusChoice
 from coldfront.core.resource.models import Resource
 from coldfront.core.utils.mail import send_email, send_email_template
 
@@ -39,6 +39,7 @@ class SecureDirAddUsersView(LoginRequiredMixin,
                             UserPassesTestMixin,
                             TemplateView):
     template_name = 'secure_dir/secure_dir_add_users.html'
+    action = 'Add'
 
     logger = logging.getLogger(__name__)
 
@@ -61,7 +62,7 @@ class SecureDirAddUsersView(LoginRequiredMixin,
             messages.error(
                 request, 'You can only add users to an active allocation.')
             return HttpResponseRedirect(
-                reverse('allocation-detail', kwargs={'pk': project_obj.pk}))
+                reverse('allocation-detail', kwargs={'pk': alloc_obj.pk}))
         else:
             return super().dispatch(request, *args, **kwargs)
 
@@ -114,36 +115,78 @@ class SecureDirAddUsersView(LoginRequiredMixin,
 
         return user_data_list
 
+    def get_users_to_remove(self, alloc_obj):
+        users_to_remove = set(alloc_user.user for alloc_user in
+                              alloc_obj.allocationuser_set.filter(
+                                  status__name='Active'))
+
+        users_to_remove -= set(request.user for request in
+                               SecureDirRemoveUserRequest.objects.filter(
+                                   allocation=alloc_obj,
+                                   status__name__in=['Pending - Remove',
+                                                     'Processing - Remove']))
+
+        # PIs cannot request to remove themselves from their
+        # own secure directories
+        users_to_remove -= set(proj_user.user for proj_user in
+                               alloc_obj.project.projectuser_set.filter(
+                                   role__name='Principal Investigator',
+                                   status__name='Active'))
+
+        user_data_list = []
+        for user in users_to_remove:
+            user_data = {
+                'username': user.username,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'email': user.email
+            }
+            user_data_list.append(user_data)
+
+        return user_data_list
+
     def get(self, request, *args, **kwargs):
         pk = self.kwargs.get('pk')
         alloc_obj = get_object_or_404(Allocation, pk=pk)
 
-        users_to_add = self.get_users_to_add(alloc_obj)
+        if self.action == 'Add':
+            user_list = self.get_users_to_add(alloc_obj)
+        else:
+            user_list = self.get_users_to_remove(alloc_obj)
         context = {}
 
-        if users_to_add:
+        if user_list:
             formset = formset_factory(
-                AllocationSecureDirJoinForm, max_num=len(users_to_add))
-            formset = formset(initial=users_to_add, prefix='userform')
+                AllocationSecureDirManageUserForm, max_num=len(user_list))
+            formset = formset(initial=user_list, prefix='userform')
             context['formset'] = formset
 
         context['allocation'] = alloc_obj
 
-        context['can_add_users'] = False
+        context['can_manage_users'] = False
         if self.request.user.is_superuser:
-            context['can_add_users'] = True
+            context['can_manage_users'] = True
 
         if alloc_obj.project.projectuser_set.filter(
                 user=self.request.user,
                 role__name='Principal Investigator',
                 status__name='Active').exists():
-            context['can_add_users'] = True
+            context['can_manage_users'] = True
 
-        resource_type = 'scratch2' if \
+        directory_name = 'scratch2' if \
             alloc_obj.resources.filter(name__icontains='Scratch').exists() \
             else 'group'
-        context['title'] = f'secure {resource_type} directory for ' \
+        context['title'] = f'secure {directory_name} directory for ' \
                            f'{alloc_obj.project.name}'
+
+        context['action'] = self.action
+        context['url'] = 'secure-dir-add-users' \
+            if self.action == 'Add' else 'secure-dir-remove-users'
+
+        context['button'] = 'btn-success' if self.action == 'Add' \
+            else 'btn-danger'
+
+        context['preposition'] = 'to' if self.action == 'Add' else 'from'
 
         return render(request, self.template_name, context)
 
@@ -151,41 +194,48 @@ class SecureDirAddUsersView(LoginRequiredMixin,
         pk = self.kwargs.get('pk')
         alloc_obj = get_object_or_404(Allocation, pk=pk)
 
-        allowed_to_approve_users = False
+        allowed_to_manage_users = False
         if alloc_obj.project.projectuser_set.filter(
                 user=self.request.user,
                 role__name='Principal Investigator',
                 status__name='Active').exists():
-            allowed_to_approve_users = True
+            allowed_to_manage_users = True
 
         if self.request.user.is_superuser:
-            allowed_to_approve_users = True
+            allowed_to_manage_users = True
 
-        if not allowed_to_approve_users:
+        if not allowed_to_manage_users:
             message = 'You do not have permission to view the this page.'
             messages.error(request, message)
 
             return HttpResponseRedirect(
                 reverse('allocation-detail', kwargs={'pk': pk}))
 
-        users_to_add = self.get_users_to_add(alloc_obj)
+        if self.action == 'Add':
+            user_list = self.get_users_to_add(alloc_obj)
+        else:
+            user_list = self.get_users_to_remove(alloc_obj)
 
         formset = formset_factory(
-            AllocationSecureDirJoinForm, max_num=len(users_to_add))
+            AllocationSecureDirManageUserForm, max_num=len(user_list))
         formset = formset(
-            request.POST, initial=users_to_add, prefix='userform')
+            request.POST, initial=user_list, prefix='userform')
 
         reviewed_users_count = 0
 
-        # TODO: how does this decision variable get populated?
         decision = request.POST.get('decision', None)
-        if decision != 'add':
+        if decision not in ['Add', 'Remove']:
             return HttpResponse('', status=400)
 
         if formset.is_valid():
-            pending_status = \
-                SecureDirAddUserRequestStatusChoice.objects.get(
-                    name='Pending - Add')
+            if decision == 'Add':
+                pending_status = \
+                    SecureDirAddUserRequestStatusChoice.objects.get(
+                        name='Pending - Add')
+            else:
+                pending_status = \
+                    SecureDirRemoveUserRequestStatusChoice.objects.get(
+                        name='Pending - Remove')
 
             for form in formset:
                 user_form_data = form.cleaned_data
@@ -195,20 +245,35 @@ class SecureDirAddUsersView(LoginRequiredMixin,
                         username=user_form_data.get('username'))
 
                     # Create a new SecureDirAddUserRequest for the user.
-                    secure_dir_request = SecureDirAddUserRequest.objects.create(
-                        user=user_obj,
-                        allocation=alloc_obj,
-                        status=pending_status
-                    )
+                    if decision == 'Add':
+                        secure_dir_add_request = \
+                            SecureDirAddUserRequest.objects.create(
+                                user=user_obj,
+                                allocation=alloc_obj,
+                                status=pending_status
+                            )
+                    else:
+                        secure_dir_remove_request = \
+                            SecureDirRemoveUserRequest.objects.create(
+                                user=user_obj,
+                                allocation=alloc_obj,
+                                status=pending_status
+                            )
 
                     # TODO: do we email admins? Probably
 
             # TODO: alter message or is it already informative enough?
+            directory_name = 'scratch2' if \
+                alloc_obj.resources.filter(name__icontains='Scratch').exists() \
+                else 'group'
+
             message = (
-                f'Successfully requested secure directory access for '
+                f'Successfully requested to {decision.lower()} '
                 f'{reviewed_users_count} user'
-                f'{"s" if reviewed_users_count > 1 else ""}. BRC staff '
-                f'have been notified.')
+                f'{"s" if reviewed_users_count > 1 else ""} '
+                f'{"to" if decision == "Add" else "from"} the secure '
+                f'{directory_name} directory for {alloc_obj.project.name}. '
+                f'BRC staff have been notified.')
             messages.success(request, message)
 
         else:
