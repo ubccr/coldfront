8cc41dcd0a87daa9fb564df5bc910db65ab5f592
Matthew Li <matthew.elbert.li@gmail.com>
2022-04-24 13:55:43 -0700

    Register new signals; only schedule new run if period hasn't started yet

--- coldfront/core/allocation/signals_/renewal_signals.py
+++ coldfront/core/allocation/signals_/renewal_signals.py
@@ -1,6 +1,7 @@
 from coldfront.core.allocation.models import AllocationPeriod
 from coldfront.core.utils.common import delete_scheduled_tasks
 from coldfront.core.utils.common import display_time_zone_date_to_utc_datetime
+from coldfront.core.utils.common import utc_now_offset_aware
 
 from django.db.models.signals import post_delete
 from django.db.models.signals import post_save
@@ -19,19 +20,21 @@ logger = logging.getLogger(__name__)
 def reschedule_allocation_renewal_scheduled_task(sender, instance, created,
                                                  **kwargs):
     """When an AllocationPeriod is saved, delete any scheduled tasks for
-    starting it and schedule a new one."""
+    starting it and schedule a new one if its start date is in the
+    future."""
     func = 'django.core.management.call_command'
     args = ('start_allocation_period', instance.pk)
 
     delete_scheduled_tasks(func, *args)
 
     next_run = display_time_zone_date_to_utc_datetime(instance.start_date)
-    kwargs = {
-        'next_run': next_run,
-        'repeats': -1,
-        'schedule_type': Schedule.ONCE,
-    }
-    schedule(func, *args, **kwargs)
+    if next_run > utc_now_offset_aware():
+        kwargs = {
+            'next_run': next_run,
+            'repeats': -1,
+            'schedule_type': Schedule.ONCE,
+        }
+        schedule(func, *args, **kwargs)
 
     message = (
         f'Scheduled a task for starting AllocationPeriod {instance.pk} to run '
