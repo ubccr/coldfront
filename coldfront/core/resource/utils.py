d3fdb303e530854a0728868bb5c850bd374a4adb
jofeinstein <jofeinstein@gmail.com>
2022-04-26 11:57:26 -0400

    made filtering more stringent

--- coldfront/core/resource/utils.py
+++ coldfront/core/resource/utils.py
@@ -4,6 +4,6 @@ from coldfront.core.resource.models import Resource
 def get_compute_resource_names():
     """Returns sorted list of Resource names without ' Compute' in the name"""
     names = [x.replace(' Compute', '') for x in
-             Resource.objects.filter(name__icontains='Compute')
+             Resource.objects.filter(name__endswith=' Compute')
                  .values_list('name', flat=True).order_by('name')]
     return names
