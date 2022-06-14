9d3af96af63e8d3025009f17cc173a86be2b2d02
Matthew Li <matthew.elbert.li@gmail.com>
2022-06-14 14:11:14 -0700

    Adjust command for creating EmailAddresses to create either instances from allauth.account.models or coldfront.core.user.models

--- coldfront/core/user/management/commands/create_email_addresses.py
+++ coldfront/core/user/management/commands/create_email_addresses.py
@@ -1,4 +1,3 @@
-from coldfront.core.user.models import EmailAddress
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 import logging
@@ -17,21 +16,49 @@ class Command(BaseCommand):
     logger = logging.getLogger(__name__)
 
     def add_arguments(self, parser):
-        pass
+        # TODO: Remove this once all emails are transitioned to
+        # TODO: allauth.account.models.EmailAddress.
+        parser.add_argument(
+            'module',
+            choices=['allauth.account.models', 'coldfront.core.user.models'],
+            help=(
+                'There are temporarily two EmailAddress models, until all can '
+                'be transitioned under allauth.account.models.'),
+            type=str)
 
     def handle(self, *args, **options):
-        """For each User that has no EmailAddress, create one with
-        is_primary and is_verified set to True."""
-        users = User.objects.prefetch_related(
-            'emailaddress_set').filter(emailaddress=None)
-        for user in users:
+        """For each User that has no EmailAddress, create a verified,
+        primary instance using the User's email field."""
+        if options['module'] == 'allauth.account.models':
+            from allauth.account.models import EmailAddress
+            verified_field, primary_field = 'verified', 'primary'
+        else:
+            from coldfront.core.user.models import EmailAddress
+            verified_field, primary_field = 'is_verified', 'is_primary'
+
+        user_pks_with_emails = EmailAddress.objects.values_list(
+            'user', flat=True)
+        users_without_emails = User.objects.exclude(
+            pk__in=user_pks_with_emails)
+
+        for user in users_without_emails:
             email = user.email.lower()
-            email_address = EmailAddress.objects.create(
-                user=user,
-                email=email,
-                is_verified=True,
-                is_primary=True)
-            message = (
-                f'Created verified, primary EmailAddress {email_address.pk} '
-                f'for User {user.pk} and email {email}.')
-            self.logger.info(message)
+            kwargs = {
+                'user': user,
+                'email': email,
+                verified_field: True,
+                primary_field: True,
+            }
+            try:
+                email_address = EmailAddress.objects.create(**kwargs)
+            except Exception as e:
+                message = (
+                    f'Failed to create a verified, primary EmailAddress for '
+                    f'User {user.pk} and email {email}. Details:\n{e}')
+                self.stderr.write(self.style.ERROR(message))
+            else:
+                message = (
+                    f'Created verified, primary EmailAddress '
+                    f'{email_address.pk} for User {user.pk} and email '
+                    f'{email}.')
+                self.logger.info(message)
