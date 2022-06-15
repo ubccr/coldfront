8df203cae269a48b1ebbd9fa1476886803a85aaf
claire-peters <34081638+claire-peters@users.noreply.github.com>
2022-06-15 14:07:14 -0400

    Cp testsuiteupdate (#92)
    * add fixtures and allocation tests
    
    * add allocation data test
    
    * add QC test to find
    
    * Update tests.py
    
    * Update tests.py
    
    Co-authored-by: Claire Peters <clairepeters@g.harvard.edu>

--- coldfront/core/allocation/tests.py
+++ coldfront/core/allocation/tests.py
@@ -1,8 +1,23 @@
+import unittest
+
+from django.db.models import Count
 from django.test import TestCase, Client
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from coldfront.core.allocation.models import Allocation, AllocationAttribute, AllocationAttributeUsage
+
+
+class AllocationQC(unittest.TestCase):
+    def check_resource_quotas(self):
+        zero_quotas = AllocationAttribute.objects.filter(allocation_attribute_type__in=[1,5], value=0)
+        self.assertEqual(zero_quotas.count(), 0)
+                                                         
+    def check_resource_counts(self):
+        over_one = Allocation.objects.annotate(resource_count=Count('resources')).filter(resource_count__gt=1)
+        print(over_one)
+        self.assertEqual(over_one.count(), 0)
+
 
 class AllocationViewTest(TestCase):
     fixtures = [
@@ -22,10 +37,11 @@ class AllocationViewTest(TestCase):
 
     def test_allocation_list_template(self):
         """Confirm that allocation-list renders correctly
-        """ 
+        """
         response = self.c.get("/allocation/")
         # response = self.client.get(reverse_lazy('allocation-list'))
         self.assertEqual(response.status_code, 200)
+
 
 class AllocationDetailViewTest(TestCase):
 
@@ -45,10 +61,12 @@ class AllocationDetailViewTest(TestCase):
         self.c.force_login(user, backend="django.contrib.auth.backends.ModelBackend")
 
     def test_allocation_detail_template_value_render(self):
-        """Confirm that quota_tb and usage_tb are correctly rendered in the generated 
+        """Confirm that quota_tb and usage_tb are correctly rendered in the generated
         AllocationDetailView
-        """ 
+        """
         response = self.c.get('/allocation/1/')
         self.assertEqual(response.status_code, 200)
-        # check that allocation_quota_tb has value 
+        # check that allocation_quota_tb has value
+        self.assertEqual(response.context['allocation_quota_bytes'], 109951162777600)
         # check that allocation_usage_tb has value
+        self.assertEqual(response.context['allocation_usage_bytes'], 10995116277760)
