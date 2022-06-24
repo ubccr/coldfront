f4af777639b06ca949b1d5857c5661c93f587c17
Matthew Li <matthew.elbert.li@gmail.com>
2022-06-24 13:23:14 -0700

    Add comments to allowance constants

--- coldfront/core/resource/utils_/allowance_utils/constants.py
+++ coldfront/core/resource/utils_/allowance_utils/constants.py
@@ -1,13 +1,19 @@
 class Allowances(object):
+    """Names of Resource objects corresponding to common computing
+    allowances."""
     CO = 'Condo Allocation'
     RECHARGE = 'Recharge Allocation'
 
 
 class BRCAllowances(Allowances):
+    """Names of Resource objects corresponding to BRC-exclusive
+    computing allowances."""
     FCA = 'Faculty Computing Allowance'
     ICA = 'Instructional Computing Allowance'
     PCA = 'Partner Computing Allowance'
 
 
 class LRCAllowances(Allowances):
+    """Names of Resource objects corresponding to LRC-exclusive
+    computing allowances."""
     PCA = 'PI Computing Allowance'
