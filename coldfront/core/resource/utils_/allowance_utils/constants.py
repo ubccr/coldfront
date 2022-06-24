31df5c1a46a9f575f5b1560d09cece2d939be93c
Matthew Li <matthew.elbert.li@gmail.com>
2022-06-24 12:56:45 -0700

    Add command for introducing Resources/ResourceAttributes for modeling Savio/Lawrencium allowance types

---
+++ coldfront/core/resource/utils_/allowance_utils/constants.py
@@ -0,0 +1,13 @@
+class Allowances(object):
+    CO = 'Condo Allocation'
+    RECHARGE = 'Recharge Allocation'
+
+
+class BRCAllowances(Allowances):
+    FCA = 'Faculty Computing Allowance'
+    ICA = 'Instructional Computing Allowance'
+    PCA = 'Partner Computing Allowance'
+
+
+class LRCAllowances(Allowances):
+    PCA = 'PI Computing Allowance'
