035e61c72828ba01dfe598f0aa141fd828e72655
jofeinstein <jofeinstein@gmail.com>
2022-07-06 00:49:19 -0400

    added url for project removal requests

--- coldfront/api/project/urls.py
+++ coldfront/api/project/urls.py
@@ -1,7 +1,10 @@
-from coldfront.api.project.views import ProjectViewSet
+from coldfront.api.project.views import ProjectViewSet, \
+    ProjectUserRemovalRequestViewSet
 from rest_framework.routers import DefaultRouter
-
 
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet, basename='projects')
+router.register(r'project_user_removal_request',
+                ProjectUserRemovalRequestViewSet,
+                basename='project_user_removal_request')
 urlpatterns = router.urls
