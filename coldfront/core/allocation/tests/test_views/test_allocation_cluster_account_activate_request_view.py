ab7d83566c28a01799789f87137c01f28faa138a
jofeinstein <jofeinstein@gmail.com>
2022-07-18 09:56:31 -0400

    updated tests to reflect new workflow

--- coldfront/core/allocation/tests/test_views/test_allocation_cluster_account_activate_request_view.py
+++ coldfront/core/allocation/tests/test_views/test_allocation_cluster_account_activate_request_view.py
@@ -1,4 +1,8 @@
+from coldfront.core.allocation.models import AllocationUser, \
+    ClusterAccessRequest, ClusterAccessRequestStatusChoice, \
+    AllocationUserAttribute, Allocation
 from coldfront.core.user.tests.utils import grant_user_cluster_access_under_test_project
+from coldfront.core.utils.common import utc_now_offset_aware
 from coldfront.core.utils.tests.test_base import TestBase
 from django.urls import reverse
 
@@ -15,23 +19,28 @@ class TestAllocationClusterAccountActivateRequestView(TestBase):
         self.client.login(username=self.user.username, password=self.password)
         self.user.is_superuser = True
         self.user.save()
-        self.attribute = grant_user_cluster_access_under_test_project(
+        attribute = grant_user_cluster_access_under_test_project(
             self.user)
-        self.attribute.value = 'Processing'
-        self.attribute.save()
+        attribute.delete()
+
+        # Create ClusterAccessRequest
+        self.request_obj = ClusterAccessRequest.objects.create(
+            allocation_user=AllocationUser.objects.get(user=self.user),
+            status=ClusterAccessRequestStatusChoice.objects.get(name='Processing'),
+            request_time=utc_now_offset_aware())
 
     @staticmethod
-    def view_url(attribute_pk):
-        """Return the URL to the view for the AllocationUserAttribute
+    def view_url(pk):
+        """Return the URL to the view for the ClusterAccessRequest
         with the given primary key."""
         return reverse(
             'allocation-cluster-account-activate-request',
-            kwargs={'pk': attribute_pk})
+            kwargs={'pk': pk})
 
     def test_request_user_logged(self):
         """Test that a message is written to the log noting which user
         made the request to update the status."""
-        url = self.view_url(self.attribute.pk)
+        url = self.view_url(self.request_obj.pk)
         data = {
             'username': self.user.username,
             'cluster_uid': '12345',
@@ -44,22 +53,29 @@ class TestAllocationClusterAccountActivateRequestView(TestBase):
         # Assert that an info message was logged.
         self.assertEqual(len(cm.output), 1)
         expected_log_message = (
-            f'Superuser {self.user.pk} changed the value of "Cluster Account '
-            f'Status" AllocationUserAttribute {self.attribute.pk} from '
-            f'"Processing" to "Active".')
+            f'Superuser {self.user.pk} changed the value of "Cluster '
+            f'Account Status" AllocationUserAttribute '
+            f'{self.request_obj.pk} from "Processing" to '
+            f'"Active".')
         self.assertIn(expected_log_message, cm.output[0])
 
     def test_updates_value(self):
         """Test that updating the status results in the correct value
         being set."""
-        url = self.view_url(self.attribute.pk)
+        url = self.view_url(self.request_obj.pk)
         data = {
             'username': self.user.username,
             'cluster_uid': '12345',
         }
         self.client.post(url, data)
 
-        self.attribute.refresh_from_db()
-        self.assertEqual(self.attribute.value, 'Active')
+        self.request_obj.refresh_from_db()
+        self.assertEqual(self.request_obj.status.name, 'Active')
+        cluster_access = AllocationUserAttribute.objects.filter(
+            allocation_attribute_type__name='Cluster Account Status',
+            allocation=Allocation.objects.get(project__name='test_project'),
+            allocation_user=AllocationUser.objects.get(user=self.user),
+            value='Active')
+        self.assertTrue(cluster_access.exists())
 
     # TODO
