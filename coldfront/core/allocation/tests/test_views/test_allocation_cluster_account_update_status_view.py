2c793e853a9b9f6a5fbea6d91aa7aba33c5b19c4
jofeinstein <jofeinstein@gmail.com>
2022-07-18 13:29:54 -0400

    updated test to use ClusterAccessRequest.

--- coldfront/core/allocation/tests/test_views/test_allocation_cluster_account_update_status_view.py
+++ coldfront/core/allocation/tests/test_views/test_allocation_cluster_account_update_status_view.py
@@ -1,4 +1,7 @@
+from coldfront.core.allocation.models import ClusterAccessRequest, \
+    AllocationUser, ClusterAccessRequestStatusChoice
 from coldfront.core.user.tests.utils import grant_user_cluster_access_under_test_project
+from coldfront.core.utils.common import utc_now_offset_aware
 from coldfront.core.utils.tests.test_base import TestBase
 from django.urls import reverse
 
@@ -14,23 +17,28 @@ class TestAllocationClusterAccountUpdateStatusView(TestBase):
         self.client.login(username=self.user.username, password=self.password)
         self.user.is_superuser = True
         self.user.save()
-        self.attribute = grant_user_cluster_access_under_test_project(
+        attribute = grant_user_cluster_access_under_test_project(
             self.user)
-        self.attribute.value = 'Pending - Add'
-        self.attribute.save()
+        attribute.delete()
+
+        # Create ClusterAccessRequest
+        self.request_obj = ClusterAccessRequest.objects.create(
+            allocation_user=AllocationUser.objects.get(user=self.user),
+            status=ClusterAccessRequestStatusChoice.objects.get(name='Pending - Add'),
+            request_time=utc_now_offset_aware())
 
     @staticmethod
-    def view_url(attribute_pk):
+    def view_url(pk):
         """Return the URL to the view for the AllocationUserAttribute
         with the given primary key."""
         return reverse(
             'allocation-cluster-account-update-status',
-            kwargs={'pk': attribute_pk})
+            kwargs={'pk': pk})
 
     def test_logs_request_user(self):
         """Test that a message is written to the log noting which user
         made the request to update the status."""
-        url = self.view_url(self.attribute.pk)
+        url = self.view_url(self.request_obj.pk)
         data = {
             'status': 'Processing',
         }
@@ -43,7 +51,7 @@ class TestAllocationClusterAccountUpdateStatusView(TestBase):
         self.assertEqual(len(cm.output), 1)
         expected_log_message = (
             f'Superuser {self.user.pk} changed the value of "Cluster Account '
-            f'Status" AllocationUserAttribute {self.attribute.pk} from '
+            f'Status" AllocationUserAttribute {self.request_obj.pk} from '
             f'"Pending - Add" to "{data["status"]}".')
         self.assertIn(expected_log_message, cm.output[0])
 
@@ -51,13 +59,13 @@ class TestAllocationClusterAccountUpdateStatusView(TestBase):
         """Test that updating the status results in the correct value
         being set."""
         for status in ('Pending - Add', 'Processing'):
-            url = self.view_url(self.attribute.pk)
+            url = self.view_url(self.request_obj.pk)
             data = {
                 'status': status,
             }
             self.client.post(url, data)
 
-            self.attribute.refresh_from_db()
-            self.assertEqual(self.attribute.value, status)
+            self.request_obj.refresh_from_db()
+            self.assertEqual(self.request_obj.status.name, status)
 
     # TODO
