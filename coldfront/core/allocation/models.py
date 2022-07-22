ebfd78a0c13b183ce6c58a076704c0c50bacc472
jofeinstein <jofeinstein@gmail.com>
2022-07-22 00:19:06 -0400

    removed username and cluster_uid fields from ClusterAccessRequest

--- coldfront/core/allocation/models.py
+++ coldfront/core/allocation/models.py
@@ -686,8 +686,8 @@ class ClusterAccessRequest(TimeStampedModel):
     allocation_user = models.ForeignKey(AllocationUser, on_delete=models.CASCADE)
     status = models.ForeignKey(ClusterAccessRequestStatusChoice, on_delete=models.CASCADE)
 
-    cluster_uid = models.CharField(max_length=10, null=True, blank=True)
-    username = models.CharField(max_length=100, null=True, blank=True)
+    # cluster_uid = models.CharField(max_length=10, null=True, blank=True)
+    # username = models.CharField(max_length=100, null=True, blank=True)
 
     request_time = models.DateTimeField(
         null=True, blank=True, default=timezone.now)
