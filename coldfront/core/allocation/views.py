68d135a541e99f0940410a77d365ca95141a7891
Matthew Kusz <mkusz@iu.edu>
2022-07-25 13:27:27 -0400

    Remove slurm account name identifiers for carbonate dl and gpu allocations

--- coldfront/core/allocation/views.py
+++ coldfront/core/allocation/views.py
@@ -1069,10 +1069,6 @@ class AllocationCreateView(LoginRequiredMixin, UserPassesTestMixin, FormView):
         if resource_obj.resourceattribute_set.filter(resource_attribute_type__name='slurm_cluster').exists():
             if project_obj.slurm_account_name:
                 value = project_obj.slurm_account_name
-                if resource_obj.name == 'Carbonate DL':
-                    value = 'DL_' + project_obj.slurm_account_name
-                elif resource_obj.name == 'Carbonate GPU':
-                    value = 'GPU_' + project_obj.slurm_account_name
 
                 slurm_account_name_attribute_type = AllocationAttributeType.objects.get(
                     name='slurm_account_name'
