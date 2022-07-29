d73be93921538bb71ba3dd2aeecf38f69ee5ece8
Matthew Li <matthew.elbert.li@gmail.com>
2022-07-29 15:42:33 -0700

    Add common and BRC-specific new project user runner tests

--- coldfront/core/project/tests/test_utils/test_new_project_user_utils/test_runners.py
+++ coldfront/core/project/tests/test_utils/test_new_project_user_utils/test_runners.py
@@ -1,14 +1,29 @@
 from decimal import Decimal
+from unittest.mock import patch
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core import mail
 
 from coldfront.api.statistics.utils import create_project_allocation
+from coldfront.core.allocation.models import Allocation
 from coldfront.core.allocation.models import ClusterAccessRequest
+from coldfront.core.project.utils import send_new_cluster_access_request_notification_email
 from coldfront.core.project.utils_.new_project_user_utils import BRCNewProjectUserRunner
 from coldfront.core.project.utils_.new_project_user_utils import LRCNewProjectUserRunner
 from coldfront.core.project.utils_.new_project_user_utils import NewProjectUserRunner
 from coldfront.core.project.utils_.new_project_user_utils import NewProjectUserRunnerFactory
 from coldfront.core.project.utils_.new_project_user_utils import NewProjectUserSource
+from coldfront.core.resource.models import Resource
+from coldfront.core.user.models import UserProfile
+from coldfront.core.utils.email.email_strategy import EnqueueEmailStrategy
 from coldfront.core.utils.tests.test_base import enable_deployment
 from coldfront.core.utils.tests.test_base import TestBase
+
+
+def raise_exception(*args, **kwargs):
+    """Raise an exception."""
+    raise Exception('Test exception.')
 
 
 class TestNewProjectUserRunner(TestBase):
@@ -82,6 +97,96 @@ class TestCommonRunnerMixin(object):
             allocation_user__user=self.user)
         self.assertFalse(cluster_access_requests.exists())
 
+    def test_email_strategy_default(self):
+        """Test that, if no EmailStrategy is provided to the runner, it
+        defaults to using SendEmailStrategy (i.e., it sends emails
+        immediately)."""
+        self.assertEqual(len(mail.outbox), 0)
+
+        runner = self._runner_factory.get_runner(
+            self.project_user, NewProjectUserSource.ADDED)
+        runner.run()
+
+        self.assertEqual(len(mail.outbox), 2)
+
+        # Additionally, test that the correct email is sent for the source.
+        added_email_found = False
+        for email in mail.outbox:
+            if f'Added to Project {self.project.name}' in email.subject:
+                added_email_found = True
+                break
+        self.assertTrue(added_email_found)
+
+    def test_email_strategy_enqueue(self):
+        """Test that if the EnqueueEmailStrategy is provided to the
+        runner, it does not send emails, but enqueues them for later
+        sending."""
+        self.assertEqual(len(mail.outbox), 0)
+
+        email_strategy = EnqueueEmailStrategy()
+        runner = self._runner_factory.get_runner(
+            self.project_user, NewProjectUserSource.JOINED,
+            email_strategy=email_strategy)
+        runner.run()
+
+        self.assertEqual(len(mail.outbox), 0)
+
+        self.assertEqual(len(email_strategy.get_queue()), 2)
+        email_strategy.send_queued_emails()
+
+        self.assertEqual(len(mail.outbox), 2)
+
+        # Additionally, test that the correct email is sent for the source.
+        join_approval_email_found = False
+        for email in mail.outbox:
+            if f'Join {self.project.name} Approved' in email.subject:
+                join_approval_email_found = True
+                break
+        self.assertTrue(join_approval_email_found)
+
+    def test_failure(self):
+        """Test that, when an exception is raised inside the
+        transaction, changes made so far are rolled back. Test that only
+        particular emails are sent for particular EmailStrategy
+        instances."""
+        self.assertEqual(len(mail.outbox), 0)
+        self._assert_pre_state()
+
+        args = (self.project_user, NewProjectUserSource.ADDED)
+        email_strategy = EnqueueEmailStrategy()
+        _class = self._runner_factory.get_runner(*args).__class__
+        with patch.object(_class, '_run_extra_steps', raise_exception):
+            runner = self._runner_factory.get_runner(
+                *args, email_strategy=email_strategy)
+            with self.assertRaises(Exception) as cm:
+                runner.run()
+            self.assertEqual(str(cm.exception), 'Test exception.')
+
+        self._assert_pre_state()
+
+        # When using EnqueueEmailStrategy, an email about a new cluster access
+        # request should be queued, but not sent.
+        self.assertEqual(len(mail.outbox), 0)
+        queue = email_strategy.get_queue()
+        self.assertEqual(len(queue), 1)
+        email_method, _, _ = queue.popleft()
+        self.assertEqual(
+            email_method, send_new_cluster_access_request_notification_email)
+
+        with patch.object(_class, '_run_extra_steps', raise_exception):
+            runner = self._runner_factory.get_runner(*args)
+            with self.assertRaises(Exception) as cm:
+                runner.run()
+            self.assertEqual(str(cm.exception), 'Test exception.')
+
+        self._assert_pre_state()
+
+        # When using SendEmailStrategy, an email about a new cluster access
+        # request should be sent, even though the enclosing transaction failed.
+        self.assertEqual(len(mail.outbox), 1)
+        email = mail.outbox[0]
+        self.assertIn('New Cluster Access Request', email.subject)
+
     def test_success(self):
         """Test that the runner performs expected processing."""
         self._assert_pre_state()
@@ -111,6 +216,97 @@ class TestBRCNewProjectUserRunner(TestCommonRunnerMixin, TestRunnerBase):
         assuming that the runner has either not run or not run
         successfully."""
         super()._assert_pre_state()
+
+    def test_add_vector_user_to_designated_savio_project_failure(self):
+        """Test that, if adding a Vector user to the designated Savio
+        project on Savio fails, the already-made changes are not rolled
+        back."""
+        # Create a PI.
+        pi = User.objects.create(username='pi0', email='pi0@nonexistent.com')
+        user_profile = UserProfile.objects.get(user=pi)
+        user_profile.is_pi = True
+        user_profile.save()
+
+        savio_project_name = settings.SAVIO_PROJECT_FOR_VECTOR_USERS
+        savio_project = self.create_active_project_with_pi(
+            savio_project_name, pi)
+        savio_project_allocation = create_project_allocation(
+            savio_project, Decimal('0.00')).allocation
+
+        vector_project = self.create_active_project_with_pi(
+            'vector_project', self.user)
+        vector_project_allocation = Allocation.objects.create(
+            project=vector_project, status=savio_project_allocation.status)
+        vector_project_allocation.resources.add(
+            Resource.objects.get(name='Vector Compute'))
+        project_user = vector_project.projectuser_set.get(user=self.user)
+
+        self.assertEqual(len(mail.outbox), 0)
+
+        method_to_patch = (
+            'coldfront.core.project.utils_.new_project_user_utils.'
+            'add_vector_user_to_designated_savio_project')
+        with patch(method_to_patch) as patched_method:
+            patched_method.side_effect = raise_exception
+            runner = self._runner_factory.get_runner(
+                project_user, NewProjectUserSource.ADDED)
+            runner.run()
+
+        self.assertIn(
+            'Failed to automatically add', runner.get_warning_messages()[0])
+
+        # There should be one ClusterAccessRequest, for the Vector project.
+        cluster_access_requests = ClusterAccessRequest.objects.filter(
+            allocation_user__user=self.user)
+        self.assertEqual(cluster_access_requests.count(), 1)
+        vector_request = cluster_access_requests.get(
+            allocation_user__allocation=vector_project_allocation)
+        self.assertEqual(vector_request.status.name, 'Pending - Add')
+
+        self.assertEqual(len(mail.outbox), 2)
+
+    def test_add_vector_user_to_designated_savio_project_success(self):
+        """Test that, for a Vector project, the user is also added to
+        the designated project on Savio."""
+        # Create a PI.
+        pi = User.objects.create(username='pi0', email='pi0@nonexistent.com')
+        user_profile = UserProfile.objects.get(user=pi)
+        user_profile.is_pi = True
+        user_profile.save()
+
+        savio_project_name = settings.SAVIO_PROJECT_FOR_VECTOR_USERS
+        savio_project = self.create_active_project_with_pi(
+            savio_project_name, pi)
+        savio_project_allocation = create_project_allocation(
+            savio_project, Decimal('0.00')).allocation
+
+        vector_project = self.create_active_project_with_pi(
+            'vector_project', self.user)
+        vector_project_allocation = Allocation.objects.create(
+            project=vector_project, status=savio_project_allocation.status)
+        vector_project_allocation.resources.add(
+            Resource.objects.get(name='Vector Compute'))
+        project_user = vector_project.projectuser_set.get(user=self.user)
+
+        self.assertEqual(len(mail.outbox), 0)
+
+        runner = self._runner_factory.get_runner(
+            project_user, NewProjectUserSource.ADDED)
+        runner.run()
+
+        # There should be two ClusterAccessRequests: one for the Vector
+        # project, and one for the Savio project.
+        cluster_access_requests = ClusterAccessRequest.objects.filter(
+            allocation_user__user=self.user)
+        self.assertEqual(cluster_access_requests.count(), 2)
+        savio_request = cluster_access_requests.get(
+            allocation_user__allocation=savio_project_allocation)
+        self.assertEqual(savio_request.status.name, 'Pending - Add')
+        vector_request = cluster_access_requests.get(
+            allocation_user__allocation=vector_project_allocation)
+        self.assertEqual(vector_request.status.name, 'Pending - Add')
+
+        self.assertEqual(len(mail.outbox), 4)
 
 
 @enable_deployment('LRC')
