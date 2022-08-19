a55a8d9f5479d9ee0680c22539830d5f0aeb2b4c
Matthew Li <matthew.elbert.li@gmail.com>
2022-08-19 15:42:15 -0700

    Set billing activities in UserProfile and AllocationUserAttribute during LRC new project user runner; add tests

--- coldfront/core/project/tests/test_utils/test_new_project_user_utils/test_runners.py
+++ coldfront/core/project/tests/test_utils/test_new_project_user_utils/test_runners.py
@@ -4,16 +4,22 @@ from unittest.mock import patch
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.core import mail
+from django.db import transaction
 
 from coldfront.api.statistics.utils import create_project_allocation
 from coldfront.core.allocation.models import Allocation
+from coldfront.core.allocation.models import AllocationAttribute
 from coldfront.core.allocation.models import AllocationAttributeType
+from coldfront.core.allocation.models import AllocationStatusChoice
 from coldfront.core.allocation.models import AllocationUser
 from coldfront.core.allocation.models import AllocationUserAttribute
 from coldfront.core.allocation.models import AllocationUserStatusChoice
 from coldfront.core.allocation.models import ClusterAccessRequest
+from coldfront.core.allocation.utils import get_project_compute_resource_name
 from coldfront.core.allocation.utils_.cluster_access_utils import ClusterAccessRequestRunnerValidationError
 from coldfront.core.allocation.utils_.cluster_access_utils import send_new_cluster_access_request_notification_email
+from coldfront.core.billing.models import BillingActivity
+from coldfront.core.billing.models import BillingProject
 from coldfront.core.project.models import ProjectUser
 from coldfront.core.project.models import ProjectUserRoleChoice
 from coldfront.core.project.models import ProjectUserStatusChoice
@@ -485,6 +491,210 @@ class TestLRCNewProjectUserRunner(TestCommonRunnerMixin, TestRunnerBase):
             role=ProjectUserRoleChoice.objects.get(
                 name='Principal Investigator'),
             status=ProjectUserStatusChoice.objects.get(name='Active'))
+
+        # Create a BillingProject and a BillingActivity.
+        self.billing_project = BillingProject.objects.create(
+            identifier='123456')
+        self.billing_activity = BillingActivity.objects.create(
+            billing_project=self.billing_project, identifier='789')
+        self.allocation_billing_attribute = AllocationAttribute.objects.create(
+            allocation_attribute_type=AllocationAttributeType.objects.get(
+                name='Billing Activity'),
+            allocation=self.allocation,
+            value=str(self.billing_activity.pk))
+
+    @enable_deployment('LRC')
+    def test_set_billing_activities_failure(self):
+        """Test that, if the Allocation does not have an existent
+        billing activity, the runner raises an exception and rolls back
+        changes made so far."""
+        user_profile = self.user.userprofile
+
+        def assert_runner_failure():
+            """Run the runner. Assert that billing changes are not made
+            and that other changes are rolled back."""
+            email_strategy = EnqueueEmailStrategy()
+            runner = self._runner_factory.get_runner(
+                self.project_user, NewProjectUserSource.ADDED,
+                email_strategy=email_strategy)
+            with self.assertRaises(Exception):
+                runner.run()
+
+            user_profile.refresh_from_db()
+            self.assertIsNone(user_profile.billing_activity)
+            # If there is no AllocationUser, there cannot be an
+            # AllocationUserAttribute.
+            self.assertFalse(
+                AllocationUser.objects.filter(
+                    allocation=self.allocation, user=self.user).exists())
+
+        # The Attribute value is an invalid BillingActivity primary key.
+        self.billing_activity.delete()
+
+        assert_runner_failure()
+
+        # The Attribute value is empty.
+        self.allocation_billing_attribute.value = ''
+        self.allocation_billing_attribute.save()
+
+        assert_runner_failure()
+
+        # The Attribute does not exist.
+        self.allocation_billing_attribute.delete()
+
+        assert_runner_failure()
+
+    @enable_deployment('LRC')
+    def test_set_billing_activities_for_empty_attribute_success(self):
+        """Test that, if an AllocationUserAttribute exists, but has an
+        empty value, it is overwritten. Additionally, test that the
+        activity is set in the UserProfile if it is null."""
+        user_profile = self.user.userprofile
+        self.assertIsNone(user_profile.billing_activity)
+
+        allocation_user = AllocationUser.objects.create(
+            allocation=self.allocation,
+            user=self.user,
+            status=AllocationUserStatusChoice.objects.get(name='Active'))
+        allocation_user_attribute = AllocationUserAttribute.objects.create(
+            allocation_attribute_type=AllocationAttributeType.objects.get(
+                name='Billing Activity'),
+            allocation=self.allocation,
+            allocation_user=allocation_user,
+            value='    ')
+
+        email_strategy = EnqueueEmailStrategy()
+        runner = self._runner_factory.get_runner(
+            self.project_user, NewProjectUserSource.ADDED,
+            email_strategy=email_strategy)
+        runner.run()
+
+        self._assert_post_state()
+
+        user_profile.refresh_from_db()
+        self.assertEqual(user_profile.billing_activity, self.billing_activity)
+
+        allocation_user_attribute.refresh_from_db()
+        self.assertEqual(
+            allocation_user_attribute.value, str(self.billing_activity.pk))
+
+    @enable_deployment('LRC')
+    def test_set_billing_activities_for_nonexistent_attribute_success(self):
+        """Test that, if no AllocationUserAttribute exists, one is
+        created. Additionally, test that the activity is set in the
+        UserProfile if it is null."""
+        user_profile = self.user.userprofile
+        self.assertIsNone(user_profile.billing_activity)
+
+        # If there is no AllocationUser, there cannot be an
+        # AllocationUserAttribute.
+        self.assertFalse(
+            AllocationUser.objects.filter(
+                allocation=self.allocation, user=self.user).exists())
+
+        self._assert_pre_state()
+
+        email_strategy = EnqueueEmailStrategy()
+        runner = self._runner_factory.get_runner(
+            self.project_user, NewProjectUserSource.ADDED,
+            email_strategy=email_strategy)
+        runner.run()
+
+        self._assert_post_state()
+
+        user_profile.refresh_from_db()
+        self.assertEqual(user_profile.billing_activity, self.billing_activity)
+
+        allocation_user_attribute = AllocationUserAttribute.objects.filter(
+            allocation_attribute_type=AllocationAttributeType.objects.get(
+                name='Billing Activity'),
+            allocation=self.allocation,
+            allocation_user__user=self.user).first()
+        self.assertIsNotNone(allocation_user_attribute)
+        self.assertEqual(
+            allocation_user_attribute.value, str(self.billing_activity.pk))
+
+    @enable_deployment('LRC')
+    def test_set_billing_activities_not_overwrites_existing(self):
+        """Test that the runner does not overwrite billing activities if
+        they are already set."""
+        new_billing_activity = BillingActivity.objects.create(
+            billing_project=self.billing_project, identifier='000')
+
+        user_profile = self.user.userprofile
+        user_profile.billing_activity = new_billing_activity
+        user_profile.save()
+
+        allocation_user = AllocationUser.objects.create(
+            allocation=self.allocation,
+            user=self.user,
+            status=AllocationUserStatusChoice.objects.get(name='Active'))
+        allocation_user_attribute = AllocationUserAttribute.objects.create(
+            allocation_attribute_type=AllocationAttributeType.objects.get(
+                name='Billing Activity'),
+            allocation=self.allocation,
+            allocation_user=allocation_user,
+            value=str(new_billing_activity.pk))
+
+        email_strategy = EnqueueEmailStrategy()
+        runner = self._runner_factory.get_runner(
+            self.project_user, NewProjectUserSource.ADDED,
+            email_strategy=email_strategy)
+        runner.run()
+
+        self._assert_post_state()
+
+        user_profile.refresh_from_db()
+        self.assertEqual(user_profile.billing_activity, new_billing_activity)
+        self.assertNotEqual(
+            user_profile.billing_activity, self.billing_activity)
+
+        allocation_user_attribute.refresh_from_db()
+        self.assertEqual(
+            int(allocation_user_attribute.value), new_billing_activity.pk)
+        self.assertNotEqual(
+            int(allocation_user_attribute.value), self.billing_activity.pk)
+
+    @enable_deployment('LRC')
+    def test_set_billing_activities_skipped_if_should_not_be_set(self):
+        """Test that no attempt is made to set billing activities for
+        particular Projects."""
+        example_project_names = ('alsacc', 'etna', 'nano', 'vulcan')
+        args = (self.project_user, NewProjectUserSource.ADDED)
+        kwargs = {'email_strategy': EnqueueEmailStrategy()}
+
+        manual_exception_message = 'Rolling back inner changes manually.'
+        for project_name in example_project_names:
+            self.project.name = project_name
+            self.project.save()
+
+            allocation = Allocation.objects.create(
+                project=self.project,
+                status=AllocationStatusChoice.objects.get(name='Active'))
+            allocation.resources.add(
+                Resource.objects.get(
+                    name=get_project_compute_resource_name(self.project)))
+
+            self._assert_pre_state()
+            try:
+                _class = self._runner_factory.get_runner(
+                    *args, **kwargs).__class__
+                with transaction.atomic():
+                    with patch.object(
+                            _class, '_set_billing_activities',
+                            raise_exception):
+                        runner = self._runner_factory.get_runner(
+                            *args, **kwargs)
+                        # This call should not raise an exception because the
+                        # patched method should never be invoked to begin with.
+                        runner.run()
+                    # Raise an exception to roll changes back so that the next
+                    # project name may be tested.
+                    raise Exception(manual_exception_message)
+            except Exception as e:
+                # The exception raised should be the manually-raised one.
+                self.assertEqual(str(e), manual_exception_message)
+            self._assert_pre_state()
 
     @enable_deployment('LRC')
     def test_set_host_user_failure(self):
