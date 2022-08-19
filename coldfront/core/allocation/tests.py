432ad729577de1df3080b9a43653afafe2cd3433
geistling <34081638+geistling@users.noreply.github.com>
2022-08-19 00:07:40 -0700

    update dept detail

--- coldfront/core/allocation/tests.py
+++ coldfront/core/allocation/tests.py
@@ -5,14 +5,25 @@ from django.test import TestCase, Client
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth import get_user_model
 
-from coldfront.core.allocation.models import Allocation, AllocationAttribute, AllocationAttributeUsage
+from coldfront.core.allocation.models import (Allocation,
+                                AllocationAttribute,
+                                AllocationChangeRequest)
+
+FIXTURES = [
+            "coldfront/core/test_helpers/test_data/test_fixtures/resources.json",
+            "coldfront/core/test_helpers/test_data/test_fixtures/poisson_fixtures.json",
+            "coldfront/core/test_helpers/test_data/test_fixtures/admin_fixtures.json",
+            "coldfront/core/test_helpers/test_data/test_fixtures/all_res_choices.json",
+            "coldfront/core/test_helpers/test_data/test_fixtures/field_of_science.json",
+            "coldfront/core/test_helpers/test_data/test_fixtures/project_choices.json",
+            ]
 
 
 class AllocationQC(unittest.TestCase):
     def check_resource_quotas(self):
         zero_quotas = AllocationAttribute.objects.filter(allocation_attribute_type__in=[1,5], value=0)
         self.assertEqual(zero_quotas.count(), 0)
-                                                         
+
     def check_resource_counts(self):
         over_one = Allocation.objects.annotate(resource_count=Count('resources')).filter(resource_count__gt=1)
         print(over_one)
@@ -20,13 +31,7 @@ class AllocationQC(unittest.TestCase):
 
 
 class AllocationViewTest(TestCase):
-    fixtures = [
-                "coldfront/core/test_helpers/test_data/test_fixtures/poisson_fixtures.json",
-                "coldfront/core/test_helpers/test_data/test_fixtures/admin_fixtures.json",
-                "coldfront/core/test_helpers/test_data/test_fixtures/all_res_choices.json",
-                "coldfront/core/test_helpers/test_data/test_fixtures/field_of_science.json",
-                "coldfront/core/test_helpers/test_data/test_fixtures/project_choices.json",
-                ]
+    fixtures = FIXTURES
 
     def setUp(self):
         user = get_user_model().objects.get(username="gvanrossum")
@@ -42,20 +47,67 @@ class AllocationViewTest(TestCase):
         # response = self.client.get(reverse_lazy('allocation-list'))
         self.assertEqual(response.status_code, 200)
 
+class AllocationChangeDetailViewTest(TestCase):
+
+    fixtures = FIXTURES
+
+    def setUp(self):
+        """create an AllocationChangeRequest to test
+        """
+        self.test_user1 = get_user_model().objects.get(username='gvanrossum')
+        self.test_user2 = get_user_model().objects.get(username='sdpoisson')
+        self.client = Client()
+        self.client.force_login(self.test_user1, backend="django.contrib.auth.backends.ModelBackend")
+        AllocationChangeRequest.objects.create(pk=2, allocation_id=1, status_id=1,
+                        justification="Test.").save()
+
+    def test_allocationchangedetailview_access(self):
+        response = self.client.get(reverse('allocation-change-detail', kwargs={'pk':2}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_allocationchangedetailview_post_deny(self):
+        param = {'choice': 'deny'}
+        response = self.client.post(reverse('allocation-change-detail', kwargs={'pk':2}),
+        param, follow=True)
+        self.assertEqual(response.status_code, 200)
+        alloc_change_req = AllocationChangeRequest.objects.get(pk=2)
+        self.assertEqual(alloc_change_req.status_id, 3)
+
+    def test_allocationchangedetailview_post_approve(self):
+        AllocationChangeRequest.objects.create(pk=2, allocation_id=1, status_id=1,
+                        justification="Test.").save()
+        param = {'choice': 'approve'}
+        response = self.client.post(reverse('allocation-change-detail', kwargs={'pk':2}),
+        param, follow=True)
+        self.assertEqual(response.status_code, 200)
+        alloc_change_req = AllocationChangeRequest.objects.get(pk=2)
+        self.assertEqual(alloc_change_req.status_id, 2)
+
+
+class AllocationChangeViewTest(TestCase):
+
+    fixtures = FIXTURES
+
+    def setUp(self):
+        self.test_user1 = get_user_model().objects.get(username='gvanrossum')
+        self.test_user2 = get_user_model().objects.get(username='sdpoisson')
+        self.client = Client()
+        self.client.force_login(self.test_user1, backend="django.contrib.auth.backends.ModelBackend")
+
+    def test_allocationchangeview_access(self):
+        """
+        """
+        kwargs={'pk':1, }
+        response = self.client.get('/allocation/1/change-request', kwargs=kwargs)
+        self.assertEqual(response.status_code, 200)
+
+
 
 class AllocationDetailViewTest(TestCase):
 
-    fixtures = [
-                "coldfront/core/test_helpers/test_data/test_fixtures/poisson_fixtures.json",
-                "coldfront/core/test_helpers/test_data/test_fixtures/admin_fixtures.json",
-                "coldfront/core/test_helpers/test_data/test_fixtures/all_res_choices.json",
-                "coldfront/core/test_helpers/test_data/test_fixtures/field_of_science.json",
-                "coldfront/core/test_helpers/test_data/test_fixtures/project_choices.json",
-                ]
+    fixtures = FIXTURES
 
     def setUp(self):
-        """create an AllocationDetailView to test
-        """
         user = get_user_model().objects.get(username="gvanrossum")
         self.c = Client()
         self.c.force_login(user, backend="django.contrib.auth.backends.ModelBackend")
