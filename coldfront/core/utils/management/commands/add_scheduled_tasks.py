e5dbd86c83bbc8cb7e911260a0703ca586ce4997
Matthew Kusz <mkusz@iu.edu>
2022-08-24 13:44:47 -0400

    Fix incorrect function path for project tasks

--- coldfront/core/utils/management/commands/add_scheduled_tasks.py
+++ coldfront/core/utils/management/commands/add_scheduled_tasks.py
@@ -23,11 +23,11 @@ class Command(BaseCommand):
                  schedule_type=Schedule.DAILY,
                  next_run=date)
 
-        schedule('coldfront.core.projects.tasks.update_statuses',
+        schedule('coldfront.core.project.tasks.update_statuses',
                  schedule_type=Schedule.DAILY,
                  next_run=date)
 
-        schedule('coldfront.core.projects.tasks.send_expiry_emails',
+        schedule('coldfront.core.project.tasks.send_expiry_emails',
                  schedule_type=Schedule.DAILY,
                  next_run=date)
 
