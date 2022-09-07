4a330ae4faa83214215e66d4de0a6f8f9d3872a4
geistling <34081638+geistling@users.noreply.github.com>
2022-09-06 17:46:30 -0700

    compare allocation user usage and group usage statistics

--- coldfront/core/test_helpers/management/commands/check_common_views.py
+++ coldfront/core/test_helpers/management/commands/check_common_views.py
@@ -15,3 +15,23 @@ class Command(BaseCommand):
         client = utils.login_return_client(username, password)
         utils.confirm_loads(client, "/project/?show_all_projects=on")
         utils.confirm_loads(client, "/allocation/?show_all_allocations=on")
+
+        obj_ids = utils.collect_all_ids_in_listpage(client, "/allocation/?show_all_allocations=on")
+        for obj_id in obj_ids:
+            url = f"/allocation/{obj_id}/"
+            user_pct_dict = {}
+            response = client.get(url)
+            allocation_usage = response.context_data['allocation_usage_bytes']
+            for user in response.context_data['allocation_users']._result_cache:
+                if user.usage_bytes != 0:
+                    user_usage_pct = user.usage_bytes/allocation_usage*100
+                else:
+                    user_usage_pct = 0
+                user_pct_dict[user] = user_usage_pct
+            pct_sum = sum(user_pct_dict.values())
+            if not user_pct_dict:
+                print(f"no user_pct_dict for {obj_id}.")
+            elif pct_sum > 101:
+                print(f"ALLOCATION {obj_id}: pct_sum > 100. Total: {pct_sum}.\n{user_pct_dict}")
+            elif sum(user_pct_dict.values()) < 99:
+                print(f"ALLOCATION {obj_id}: pct_sum < 99. Total: {pct_sum}.\n{user_pct_dict}")
