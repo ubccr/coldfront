414695db02096f70344d87aa281b1867e7470c7f
Andrew Stoltman <aestoltm@buffalo.edu>
2022-09-27 01:34:57 -0400

    Added warning and choice to run initial_setup script.

--- coldfront/core/utils/management/commands/initial_setup.py
+++ coldfront/core/utils/management/commands/initial_setup.py
@@ -10,11 +10,20 @@ base_dir = settings.BASE_DIR
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        call_command('migrate')
-        call_command('import_field_of_science_data')
-        call_command('add_default_grant_options')
-        call_command('add_default_project_choices')
-        call_command('add_resource_defaults')
-        call_command('add_allocation_defaults')
-        call_command('add_default_publication_sources')
-        call_command('add_scheduled_tasks')
+        while 1:
+            print("""WARNING: Running this command initializes the ColdFront database. This should only be run one time. Running this will delete anything currently in the ColdFront database.""")
+            user_response = input("Do you want to proceed?(Yes/No):")
+            if user_response == "No":
+                break
+            if user_response == "Yes":
+                call_command('migrate')
+                call_command('import_field_of_science_data')
+                call_command('add_default_grant_options')
+                call_command('add_default_project_choices')
+                call_command('add_resource_defaults')
+                call_command('add_allocation_defaults')
+                call_command('add_default_publication_sources')
+                call_command('add_scheduled_tasks')
+                break
+            
+        
