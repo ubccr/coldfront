822f620d76487dc3f0a8e230f7de1ca127e56124
geistling <34081638+geistling@users.noreply.github.com>
2022-10-13 10:07:20 -0700

    add size and allocation_users properties to Allocation model

--- coldfront/core/allocation/models.py
+++ coldfront/core/allocation/models.py
@@ -102,7 +102,17 @@ class Allocation(TimeStampedModel):
         return (self.end_date - datetime.date.today()).days
 
     @property
+    def size(self):
+        return self.allocationattribute_set.get(allocation_attribute_type_id=1).value
 
+
+    @property
+    def allocation_users(self):
+        # allocationuser_filter = (Q(status__name='Active') #&
+        #                         #~Q(usage_bytes__isnull=True))
+        return self.allocationuser_set.filter(status__name='Active')
+
+    @property
     def get_information(self, public_only=True):
         html_string = ''
         if public_only:
