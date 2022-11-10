75dc066a9f49eb663a1908dab1528b4b7197ec56
Andrew Stoltman <aestoltm@buffalo.edu>
2022-11-10 07:29:38 -0500

    Put call_command into its own function

--- coldfront/core/utils/management/commands/initial_setup.py
+++ coldfront/core/utils/management/commands/initial_setup.py
@@ -15,29 +15,24 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
             if options['force-overwrite']:
-                call_command('migrate')
-                call_command('import_field_of_science_data')
-                call_command('add_default_grant_options')
-                call_command('add_default_project_choices')
-                call_command('add_resource_defaults')
-                call_command('add_allocation_defaults')
-                call_command('add_default_publication_sources')
-                call_command('add_scheduled_tasks')
+                run_setup()
 
             else:
                 print("""WARNING: Running this command initializes the ColdFront database. This should only be run one time. Running this will delete anything currently in the ColdFront database.""")
                 user_response = input("Do you want to proceed?(yes):")
             
                 if user_response == "yes":
-                    call_command('migrate')
-                    call_command('import_field_of_science_data')
-                    call_command('add_default_grant_options')
-                    call_command('add_default_project_choices')
-                    call_command('add_resource_defaults')
-                    call_command('add_allocation_defaults')
-                    call_command('add_default_publication_sources')
-                    call_command('add_scheduled_tasks')
+                    run_setup()
                 else:
                     print("Please enter 'yes' if you wish to run intital setup.")
-                
-        
+
+def run_setup():
+    call_command('migrate')
+    call_command('import_field_of_science_data')
+    call_command('add_default_grant_options')
+    call_command('add_default_project_choices')
+    call_command('add_resource_defaults')
+    call_command('add_allocation_defaults')
+    call_command('add_default_publication_sources')
+    call_command('add_scheduled_tasks')              
+
