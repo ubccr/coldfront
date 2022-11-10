bb24235f1346d039e20be41fd85559cad56967ff
Andrew Stoltman <aestoltm@buffalo.edu>
2022-11-10 07:25:33 -0500

    Added argument to force setup_script and adjusted logic of script.

--- coldfront/core/utils/management/commands/initial_setup.py
+++ coldfront/core/utils/management/commands/initial_setup.py
@@ -8,14 +8,13 @@ base_dir = settings.BASE_DIR
 
 
 class Command(BaseCommand):
+    help = 'Run setup script to initialize Coldfront with testing data'
+
+    def add_arguments(self, parser):
+         parser.add_argument("-fo", "--force-overwrite", help="Force intial_setup script to run with no warning.")
 
     def handle(self, *args, **options):
-        while 1:
-            print("""WARNING: Running this command initializes the ColdFront database. This should only be run one time. Running this will delete anything currently in the ColdFront database.""")
-            user_response = input("Do you want to proceed?(Yes/No):")
-            if user_response == "No":
-                break
-            if user_response == "Yes":
+            if options['force-overwrite']:
                 call_command('migrate')
                 call_command('import_field_of_science_data')
                 call_command('add_default_grant_options')
@@ -24,6 +23,21 @@ class Command(BaseCommand):
                 call_command('add_allocation_defaults')
                 call_command('add_default_publication_sources')
                 call_command('add_scheduled_tasks')
-                break
+
+            else:
+                print("""WARNING: Running this command initializes the ColdFront database. This should only be run one time. Running this will delete anything currently in the ColdFront database.""")
+                user_response = input("Do you want to proceed?(yes):")
             
+                if user_response == "yes":
+                    call_command('migrate')
+                    call_command('import_field_of_science_data')
+                    call_command('add_default_grant_options')
+                    call_command('add_default_project_choices')
+                    call_command('add_resource_defaults')
+                    call_command('add_allocation_defaults')
+                    call_command('add_default_publication_sources')
+                    call_command('add_scheduled_tasks')
+                else:
+                    print("Please enter 'yes' if you wish to run intital setup.")
+                
         
