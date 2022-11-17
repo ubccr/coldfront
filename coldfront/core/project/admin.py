1e5a80b55bc8a878294ca34f01d589cae7058499
Matthew Kusz <mkusz@iu.edu>
2022-11-17 10:21:40 -0500

    Fix project title not wrapping on project admin action list

--- coldfront/core/project/admin.py
+++ coldfront/core/project/admin.py
@@ -172,9 +172,12 @@ class ProjectReviewStatusChoiceAdmin(admin.ModelAdmin):
 
 @admin.register(ProjectAdminAction)
 class ProjectAdminActionAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'user', 'project_pk', 'project', 'action', 'created', )
+    list_display = ('pk', 'user', 'project_pk', 'project_title', 'action', 'created', )
     readonly_fields = ('user', 'project_pk', 'project', 'action', 'created', )
     list_filter = ('action', )
 
     def project_pk(self, obj):
         return obj.project.pk
+
+    def project_title(self, obj):
+        return obj.project.title
