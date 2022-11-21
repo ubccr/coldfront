9f290bfd931da1dc8538652d8ca7863eac885b51
Claire Peters <clairepeters@g.harvard.edu>
2022-11-21 14:05:25 -0800

    update startup routine

--- container_startup.sh
+++ container_startup.sh
@@ -11,5 +11,7 @@
 # RUN python3 ./manage.py load_test_data
 service redis-server start
 python ./manage.py qcluster &
+python ./manage.py add_scheduled_tasks
+# initial_setup does not appear to work as requested.
 python ./manage.py initial_setup &
 python ./manage.py runserver 0.0.0.0:80 --insecure
