2051b7f4e5e57ef4748a7d1cfb262d00faca1552
Claire Peters <clairepeters@g.harvard.edu>
2022-11-21 13:30:32 -0800

    qcluster startup fix

--- container_startup.sh
+++ container_startup.sh
@@ -10,6 +10,6 @@
 # RUN python3 ./manage.py initial_setup
 # RUN python3 ./manage.py load_test_data
 service redis-server start
-# python ./manage.py qcluster &
+python ./manage.py qcluster &
 python ./manage.py initial_setup &
 python ./manage.py runserver 0.0.0.0:80 --insecure
