2921753d7cc83338c095334e69e92a284c6a0562
Claire Peters <clairepeters@g.harvard.edu>
2023-03-16 11:10:23 -0700

    update formatting

--- coldfront/core/department/forms.py
+++ coldfront/core/department/forms.py
@@ -2,8 +2,8 @@
 from django import forms
 
 class DepartmentSearchForm(forms.Form):
-    """ Search form for the Project list page.
-    """
+    ''' Search form for the Project list page.
+    '''
 
     name = forms.CharField(label='Department Name', max_length=100, required=False)
     rank = forms.CharField(label='Rank', max_length=100, required=False)
