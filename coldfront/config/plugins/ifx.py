170651d7bb1d8bb2d000eb5f4b4e0b0b8e40604c
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2023-03-24 12:10:44 -0400

    Made Starfish plugin and portal display optional Converted billing calculator to new v4 code with billing_data_dict ifxbilling, fiine.client, and ifxec are pointing to github

--- coldfront/config/plugins/ifx.py
+++ coldfront/config/plugins/ifx.py
@@ -3,6 +3,7 @@ Config for ifxbilling
 Installs ifxuser, ifxbilling, and author.  Sets AUTH_USER_MODEL
 '''
 import os
+from decimal import Decimal
 from coldfront.config.base import INSTALLED_APPS, MIDDLEWARE
 
 INSTALLED_APPS = ['ifxuser'] + INSTALLED_APPS + ['author', 'ifxbilling', 'rest_framework.authtoken',]
@@ -20,8 +21,13 @@ class GROUPS():
     ADMIN_GROUP_NAME = 'rc_admin'
     PREFERRED_BILLING_RECORD_APPROVAL_ACCOUNT_GROUP_NAME = 'Preferred Billing Record Approval Account'
 
+class RATES():
+    INTERNAL_RATE_NAME = 'Harvard Internal Rate'
+
 # Ignore billing models in the django-author pre-save so that values are set directly
 AUTHOR_IGNORE_MODELS = [
     'ifxbilling.BillingRecord',
     'ifxbilling.Transaction',
 ]
+STANDARD_QUANTIZE = Decimal('0.0000')
+TWO_DIGIT_QUANTIZE = Decimal('0.00')
