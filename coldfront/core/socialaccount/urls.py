a80573ae7ae90744a0fefed6c8c3266fbba78fc7
Matthew Li <matthew.elbert.li@gmail.com>
2023-05-12 14:15:33 -0700

    Block SSO views when flag disabled; update test for third-party accounts section accordingly

--- coldfront/core/socialaccount/urls.py
+++ coldfront/core/socialaccount/urls.py
@@ -8,23 +8,33 @@ from flags.urls import flagged_paths
 urlpatterns = []
 
 
-names_to_include = {
-    'socialaccount_login_cancelled',
-}
-for pattern in all_patterns:
-    if pattern.name in names_to_include:
-        urlpatterns.append(pattern)
-
-
-# Only include view for connecting additional social accounts if users are
-# allowed to have multiple emails.
-names_to_include_if_multiple_emails_allowed = {
-    'socialaccount_connections',
-}
-with flagged_paths('MULTIPLE_EMAIL_ADDRESSES_ALLOWED') as f_path:
+# TODO: Come up with a more elegant solution for dealing with views protected by
+#  multiple flags.
+with flagged_paths('SSO_ENABLED') as sso_f_path:
+    names_to_include = {
+        'socialaccount_login_cancelled',
+    }
     for pattern in all_patterns:
-        if pattern.name in names_to_include_if_multiple_emails_allowed:
-            urlpatterns.append(
-                f_path(
+        if pattern.name in names_to_include:
+            urlpatterns.append(sso_f_path(
+                str(pattern.pattern), pattern.callback,
+                pattern.default_args, pattern.name)
+            )
+
+    # Only include the view for connecting additional social accounts if users
+    # are allowed to have multiple emails.
+    names_to_include_if_multiple_emails_allowed = {
+        'socialaccount_connections',
+    }
+    with flagged_paths('MULTIPLE_EMAIL_ADDRESSES_ALLOWED') as multi_email_f_path:
+        for pattern in all_patterns:
+            if pattern.name in names_to_include_if_multiple_emails_allowed:
+                # The URL is not correctly disabled unless passed through both
+                # context managers.
+                tmp_pattern = multi_email_f_path(
                     str(pattern.pattern), pattern.callback,
-                    pattern.default_args, pattern.name))
+                    pattern.default_args, pattern.name)
+                final_pattern = sso_f_path(
+                    str(tmp_pattern.pattern), tmp_pattern.callback,
+                    tmp_pattern.default_args, tmp_pattern.name)
+                urlpatterns.append(final_pattern)
--- coldfront/core/user/tests/test_views/test_user_profile/test_third_party_accounts.py
+++ coldfront/core/user/tests/test_views/test_user_profile/test_third_party_accounts.py
@@ -27,12 +27,16 @@ class TestThirdPartyAccounts(TestBase):
         """Return the URL to the User Profile."""
         return reverse('user-profile')
 
-    def test_section_disabled_if_multiple_addresses_disallowed(self):
-        """Test that, if the 'MULTIPLE_EMAIL_ADDRESSES_ALLOWED' feature
-        flag is disabled, the section is hidden."""
-        flag_name = 'MULTIPLE_EMAIL_ADDRESSES_ALLOWED'
-        enable_flag(flag_name)
+    def test_section_disabled_if_flags_disabled(self):
+        """Test that, if either the 'MULTIPLE_EMAIL_ADDRESSES_ALLOWED'
+        feature flag or the 'SSO_ENABLED' feature flag is disabled, the
+        section is hidden."""
+        email_flag_name = 'MULTIPLE_EMAIL_ADDRESSES_ALLOWED'
+        sso_flag_name = 'SSO_ENABLED'
+        enable_flag(email_flag_name)
+        enable_flag(sso_flag_name)
 
+        # Both enabled
         response = self.client.get(self.user_profile_url())
         self.assertContains(response, 'Third-Party Accounts')
         section_url = reverse('socialaccount_connections')
@@ -40,13 +44,53 @@ class TestThirdPartyAccounts(TestBase):
         response = self.client.get(section_url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
+        disabled_dict = {'condition': 'boolean', 'value': False}
+        enabled_dict = {'condition': 'boolean', 'value': True}
+
+        # One enabled, one disabled
         flags_copy = deepcopy(settings.FLAGS)
-        flags_copy[flag_name] = {'condition': 'boolean', 'value': False}
+        flags_copy[email_flag_name] = disabled_dict
+        flags_copy[sso_flag_name] = enabled_dict
         with override_settings(FLAGS=flags_copy):
-            disable_flag(flag_name)
-            self.assertFalse(flag_enabled(flag_name))
+            disable_flag(email_flag_name)
+            enable_flag(sso_flag_name)
+            self.assertFalse(flag_enabled(email_flag_name))
+            self.assertTrue(flag_enabled(sso_flag_name))
+            # The section should be hidden.
             response = self.client.get(self.user_profile_url())
             self.assertNotContains(response, 'Third-Party Accounts')
             self.assertNotContains(response, section_url)
+            # The underlying view should be inaccessible.
+            response = self.client.get(section_url)
+            self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+        flags_copy[email_flag_name] = enabled_dict
+        flags_copy[sso_flag_name] = disabled_dict
+        with override_settings(FLAGS=flags_copy):
+            enable_flag(email_flag_name)
+            disable_flag(sso_flag_name)
+            self.assertTrue(flag_enabled(email_flag_name))
+            self.assertFalse(flag_enabled(sso_flag_name))
+            # The section should be hidden.
+            response = self.client.get(self.user_profile_url())
+            self.assertNotContains(response, 'Third-Party Accounts')
+            self.assertNotContains(response, section_url)
+            # The underlying view should not be accessible.
+            response = self.client.get(section_url)
+            self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+        # Both disabled
+        flags_copy[email_flag_name] = disabled_dict
+        flags_copy[sso_flag_name] = disabled_dict
+        with override_settings(FLAGS=flags_copy):
+            disable_flag(email_flag_name)
+            disable_flag(sso_flag_name)
+            self.assertFalse(flag_enabled(email_flag_name))
+            self.assertFalse(flag_enabled(sso_flag_name))
+            # The section should be hidden.
+            response = self.client.get(self.user_profile_url())
+            self.assertNotContains(response, 'Third-Party Accounts')
+            self.assertNotContains(response, section_url)
+            # The underlying view should not be accessible.
             response = self.client.get(section_url)
             self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
