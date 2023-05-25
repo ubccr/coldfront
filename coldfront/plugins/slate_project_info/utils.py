ebf7d16678cf1b48ceeb3a1dde0c8c3a3be01e80
Matthew Kusz <Matt52@roadrunner.com>
2023-05-25 10:41:45 -0400

    Display slate project name instead of group

--- coldfront/plugins/slate_project_info/utils.py
+++ coldfront/plugins/slate_project_info/utils.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import os
 
 import ldap.filter
 from coldfront.core.utils.common import import_from_settings
@@ -8,7 +9,64 @@ from ldap3 import Connection, Server
 logger = logging.getLogger(__name__)
 
 
-def get_slate_project_info_from_ldap(username):
+def get_slate_project_gid_to_name_mapping():
+    """
+    This works with multiple slate projects. Returns a dictionary with the key as the slate
+    project's gid and value as its name.
+    """
+    slate_project_gid_to_name_mapping = {}
+    with open(os.path.join('slate_projects', 'slate_projects.txt'), 'r') as file_with_gids:
+        for line in file_with_gids:
+            split_line = line.split(' ')
+            split_line = [ element for element in split_line if element ]
+            gid = int(split_line[3])
+            name = split_line[8][:-1]
+            slate_project_gid_to_name_mapping[gid] = name
+
+    return slate_project_gid_to_name_mapping
+
+
+def get_slate_project_info(slate_groups):
+    """
+    This works with multiple slate projects. Returns a list of dictionaries that contain a slate
+    project's name, user's access, and owner.
+
+    :param slate_groups: List of slate project group info
+    """
+    if not slate_groups:
+        return []
+
+    slate_project_gid_to_name_mapping = get_slate_project_gid_to_name_mapping()
+
+    slate_project_list = []
+    for slate_group in slate_groups:
+        gid_number = slate_group['gidNumber'][0]
+        slate_project_name = slate_project_gid_to_name_mapping.get(gid_number)
+        slate_project_name_read_only = slate_project_gid_to_name_mapping.get(gid_number + 1)
+        if slate_project_name or slate_project_name_read_only:
+            name = slate_project_name
+            access = 'read/write'
+            if name is None:
+                name = slate_project_name_read_only
+                access = 'read only'
+            owner = slate_group.get('description')[0].split(',')[1].strip().split(' ')[0]
+
+            slate_project_list.append(
+                {
+                    'name': name,
+                    'access': access,
+                    'owner': owner
+                }
+            )
+
+    return slate_project_list
+
+
+def get_slate_project_group_info_from_ldap(username):
+    """
+    Grabs all the slate project groups the username is in. Returns a list of dictionaries that
+    contain a slate project group's name, description, and gid number.
+    """
     server = Server(
         import_from_settings('LDAP_SLATE_PROJECT_SERVER_URI'), use_ssl=True, connect_timeout=1
     )
@@ -20,7 +78,7 @@ def get_slate_project_info_from_ldap(username):
     searchParameters = {
         'search_base': import_from_settings('LDAP_SLATE_PROJECT_USER_SEARCH_BASE'),
         'search_filter': ldap.filter.filter_format("(memberUid=%s)", [username]),
-        'attributes': ['cn', 'description']
+        'attributes': ['cn', 'description', 'gidNumber']
     }
     conn.search(**searchParameters)
     results = []
--- coldfront/plugins/slate_project_info/views.py
+++ coldfront/plugins/slate_project_info/views.py
@@ -1,31 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from coldfront.plugins.slate_project_info.utils import get_slate_project_info_from_ldap
+from coldfront.plugins.slate_project_info import utils
 
 
 @login_required
 def get_slate_project_info(request):
-    slate_projects = get_slate_project_info_from_ldap(request.user.username)
-    slate_project_list = []
-    for slate_project in slate_projects:
-        name = slate_project.get('cn')[0]
-        access = 'read/write'
-        if name.endswith('-ro'):
-            name = name[:-len('-ro')]
-            access = 'read only'
-        owner = slate_project.get('description')[0].split(',')[1].strip().split(' ')[0]
-
-        slate_project_list.append(
-            {
-                'name': name,
-                'access': access,
-                'owner': owner
-            }
-        )
+    slate_project_groups = utils.get_slate_project_group_info_from_ldap(request.POST.get('viewed_username'))
+    slate_projects = utils.get_slate_project_info(slate_project_groups)
 
     context = {
-        'slate_projects': slate_project_list
+        'slate_projects': slate_projects
     }
 
     return render(request, "slate_project_info/slate_project_info.html", context)
