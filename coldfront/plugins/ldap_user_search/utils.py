b3632b6234d7a50cc8b8be8352bb6849591bea70
Cecilia Lau <cecilialau6776@gmail.com>
2023-06-05 17:25:05 -0400

    Add TLS support for ldap user search

--- coldfront/plugins/ldap_user_search/utils.py
+++ coldfront/plugins/ldap_user_search/utils.py
@@ -4,9 +4,10 @@ import logging
 import ldap.filter
 from coldfront.core.user.utils import UserSearch
 from coldfront.core.utils.common import import_from_settings
-from ldap3 import Connection, Server
+from ldap3 import Connection, Server, Tls
 
 logger = logging.getLogger(__name__)
+
 
 class LDAPUserSearch(UserSearch):
     search_source = 'LDAP'
@@ -19,19 +20,37 @@ class LDAPUserSearch(UserSearch):
         self.LDAP_BIND_PASSWORD = import_from_settings('LDAP_USER_SEARCH_BIND_PASSWORD', None)
         self.LDAP_CONNECT_TIMEOUT = import_from_settings('LDAP_USER_SEARCH_CONNECT_TIMEOUT', 2.5)
         self.LDAP_USE_SSL = import_from_settings('LDAP_USER_SEARCH_USE_SSL', True)
+        self.LDAP_USE_TLS = import_from_settings("LDAP_USER_SEARCH_USE_TLS", False)
+        self.LDAP_PRIV_KEY_FILE = import_from_settings('LDAP_USER_SEARCH_PRIV_KEY_FILE', '')
+        self.LDAP_CERT_FILE = import_from_settings('LDAP_USER_SEARCH_CERT_FILE', '')
+        self.LDAP_CACERT_FILE = import_from_settings('LDAP_USER_SEARCH_CACERT_FILE', '')
 
-        self.server = Server(self.LDAP_SERVER_URI, use_ssl=self.LDAP_USE_SSL, connect_timeout=self.LDAP_CONNECT_TIMEOUT)
+        tls = None
+        if self.LDAP_USE_TLS:
+            tls = Tls(
+                local_private_key_file=self.LDAP_PRIV_KEY_FILE,
+                local_certificate_file=self.LDAP_CERT_FILE,
+                ca_certs_file=self.LDAP_CACERT_FILE,
+            )
+
+        self.server = Server(self.LDAP_SERVER_URI, use_ssl=self.LDAP_USE_SSL, connect_timeout=self.LDAP_CONNECT_TIMEOUT, tls=tls)
         self.conn = Connection(self.server, self.LDAP_BIND_DN, self.LDAP_BIND_PASSWORD, auto_bind=True)
 
     def parse_ldap_entry(self, entry):
         entry_dict = json.loads(entry.entry_to_json()).get('attributes')
 
         user_dict = {
-            'last_name': entry_dict.get('sn')[0] if entry_dict.get('sn') else '',
-            'first_name': entry_dict.get('givenName')[0] if entry_dict.get('givenName') else '',
-            'username': entry_dict.get('uid')[0] if entry_dict.get('uid') else '',
-            'email': entry_dict.get('mail')[0] if entry_dict.get('mail') else '',
-            'source': self.search_source,
+            'last_name':
+            entry_dict.get('sn')[0] if entry_dict.get('sn') else '',
+            'first_name':
+            entry_dict.get('givenName')[0]
+            if entry_dict.get('givenName') else '',
+            'username':
+            entry_dict.get('uid')[0] if entry_dict.get('uid') else '',
+            'email':
+            entry_dict.get('mail')[0] if entry_dict.get('mail') else '',
+            'source':
+            self.search_source,
         }
 
         return user_dict
@@ -39,22 +58,28 @@ class LDAPUserSearch(UserSearch):
     def search_a_user(self, user_search_string=None, search_by='all_fields'):
         size_limit = 50
         if user_search_string and search_by == 'all_fields':
-            filter = ldap.filter.filter_format("(|(givenName=*%s*)(sn=*%s*)(uid=*%s*)(mail=*%s*))", [user_search_string] * 4)
+            filter = ldap.filter.filter_format(
+                "(|(givenName=*%s*)(sn=*%s*)(uid=*%s*)(mail=*%s*))",
+                [user_search_string] * 4)
         elif user_search_string and search_by == 'username_only':
-            filter = ldap.filter.filter_format("(uid=%s)", [user_search_string])
+            filter = ldap.filter.filter_format("(uid=%s)",
+                                               [user_search_string])
             size_limit = 1
         else:
             filter = '(objectclass=person)'
 
-        searchParameters = {'search_base': self.LDAP_USER_SEARCH_BASE,
-                            'search_filter': filter,
-                            'attributes': ['uid', 'sn', 'givenName', 'mail'],
-                            'size_limit': size_limit}
+        searchParameters = {
+            'search_base': self.LDAP_USER_SEARCH_BASE,
+            'search_filter': filter,
+            'attributes': ['uid', 'sn', 'givenName', 'mail'],
+            'size_limit': size_limit
+        }
         self.conn.search(**searchParameters)
         users = []
         for idx, entry in enumerate(self.conn.entries, 1):
             user_dict = self.parse_ldap_entry(entry)
             users.append(user_dict)
 
-        logger.info("LDAP user search for %s found %s results", user_search_string, len(users))
+        logger.info("LDAP user search for %s found %s results",
+                    user_search_string, len(users))
         return users
