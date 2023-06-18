7f619abff36ea9b3f03dcea9d66cb6a8284ff908
root <root@DESKTOP-GN0FQ4V>
2023-06-18 08:08:37 -0400

    fix 494

--- coldfront/core/research_output/urls.py
+++ coldfront/core/research_output/urls.py
@@ -5,4 +5,6 @@ import coldfront.core.research_output.views as research_output_views
 urlpatterns = [
     path('add-research-output/<int:project_pk>/', research_output_views.ResearchOutputCreateView.as_view(), name='add-research-output'),
     path('project/<int:project_pk>/delete-research-outputs', research_output_views.ResearchOutputDeleteResearchOutputsView.as_view(), name='research-output-delete-research-outputs'),
+    path('research-output-report/', research_output_views.ResearchOutputReportView.as_view(), name='research-output-report'),
+
 ]
