08f63563c5e2b3494ee21585f47b56d15d1f4c5e
Jim Harter <harterj@wustl.edu>
2023-07-13 17:02:51 -0500

    adding attempt to add request

--- coldfront/plugins/scale_management/utils.py
+++ coldfront/plugins/scale_management/utils.py
@@ -1,4 +1,6 @@
-# from bs4 import BeautifulSoup
+import logging
+import requests
+from requests.auth import HTTPBasicAuth
 
 from coldfront.core.utils.common import import_from_settings
 
@@ -6,6 +8,8 @@ from coldfront.core.utils.common import import_from_settings
 def get_scale_management_context():
     context = {}
     system_monitor = ScaleManagement()
+    system_monitor.getExample()
+    context['data'] = system_monitor.getResponse()
 
     return context
 
@@ -19,3 +23,19 @@ class ScaleManagement:
     def __init__(self):
         self.response = None
         self.data = {}
+        self.getExample()
+
+    def getExample(self):
+        logging.debug('getting data')
+
+        try:
+            response = requests.get('https://storage1-gui1.ris.wustl.edu/scalemgmt/v2/filesystems/rdcw-fs1/quotas?fields=blockQuota,blockUsage,objectName', auth = HTTPBasicAuth('user', 'pass'))
+        except Exception as e:
+            response = None
+
+        if response and response.status_code == 200:
+            self.response = response    
+
+    def getResponse(self):
+        return self.response                      
+
