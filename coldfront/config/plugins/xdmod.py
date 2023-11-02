f5ca015cf69b2d0bb8375aa10500e0456eeb6e4f
Claire Peters <clairepeters@g.harvard.edu>
2023-11-02 15:12:22 -0700

    add xdmod and slurm env variables to settings and setup scripts

--- coldfront/config/plugins/xdmod.py
+++ coldfront/config/plugins/xdmod.py
@@ -8,4 +8,6 @@ INSTALLED_APPS += [
     'coldfront.plugins.xdmod',
 ]
 
+XDMOD_USER = ENV.str('XDMOD_USER', default='')
+XDMOD_PASS = ENV.str('XDMOD_PASS', default='')
 XDMOD_API_URL = ENV.str('XDMOD_API_URL')
