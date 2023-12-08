ee113c3d4d417f49b9affd6acf7f9edecd50f610
Matthew Kusz <Matt52@roadrunner.com>
2023-12-08 11:48:50 -0500

    Remove unused functions

--- coldfront/plugins/slate_project/utils.py
+++ coldfront/plugins/slate_project/utils.py
@@ -607,57 +607,6 @@ def change_users_slate_project_groups(allocation_user_obj):
     )
 
 
-
-def get_slate_project_gid_number(username, namespace_entry):
-    server = Server(
-        import_from_settings('LDAP_SLATE_PROJECT_SERVER_URI'), use_ssl=True, connect_timeout=1
-    )
-    conn = Connection(server)
-    if not conn.bind():
-        return []
-    
-    searchParameters = {
-        'search_base': import_from_settings('LDAP_SLATE_PROJECT_USER_SEARCH_BASE'),
-        'search_filter': ldap.filter.filter_format(
-            "(&(memberUid=%s)(cn=%s))", [username, 'condo_' + namespace_entry]
-        ),
-        'attributes': ['gidNumber']
-    }
-    conn.search(**searchParameters)
-    results = []
-    if conn.entries:
-        for entry in conn.entries:
-            results.append(json.loads(entry.entry_to_json()).get('attributes'))
-
-    return results
-
-
-def get_allocation_user_role(username, namespace_entry):
-    gid_number = get_slate_project_gid_number(username, namespace_entry)
-    role = 'read/write'
-    if gid_number % 2:
-        role = 'read only'
-
-    return AllocationUserRoleChoice.objects.get(name=role)
-
-
-def get_slate_project_gid_to_name_mapping():
-    """
-    This works with multiple slate projects. Returns a dictionary with the key as the slate
-    project's gid and value as its name.
-    """
-    slate_project_gid_to_name_mapping = {}
-    with open(os.path.join('slate_projects', 'slate_projects.txt'), 'r') as file_with_gids:
-        for line in file_with_gids:
-            split_line = line.split(' ')
-            split_line = [ element for element in split_line if element ]
-            gid = int(split_line[3])
-            name = split_line[8][:-1]
-            slate_project_gid_to_name_mapping[gid] = name
-
-    return slate_project_gid_to_name_mapping
-
-
 def get_slate_project_info(username):
     """
     Returns a list of dictionaries that contain a slate project's name, user's access, and owner.
@@ -689,76 +638,6 @@ def get_slate_project_info(username):
         )
 
     return slate_projects
-
-
-def get_slate_project_info_old(slate_groups):
-    """
-    This works with multiple slate projects. Returns a list of dictionaries that contain a slate
-    project's name, user's access, and owner.
-
-    :param slate_groups: List of slate project group info
-    """
-    if not slate_groups:
-        return []
-
-    slate_project_gid_to_name_mapping = get_slate_project_gid_to_name_mapping()
-
-    slate_projects = []
-    for slate_group in slate_groups:
-        gid_number = slate_group.get('gidNumber')[0]
-        slate_project_name = slate_project_gid_to_name_mapping.get(gid_number)
-        slate_project_name_read_only = slate_project_gid_to_name_mapping.get(gid_number - 1)
-        if slate_project_name or slate_project_name_read_only:
-            name = slate_project_name
-            access = 'read/write'
-            if name is None:
-                name = slate_project_name_read_only
-                access = 'read only'
-            owner = slate_group.get('description')[0].split(',')[1].strip().split(' ')[0]
-
-            slate_projects.append(
-                {
-                    'name': name,
-                    'access': access,
-                    'owner': owner
-                }
-            )
-
-    return slate_projects
-
-
-def get_slate_project_group_info_from_ldap(username):
-    """
-    Grabs all the slate project groups the username is in. Returns a list of dictionaries that
-    contain a slate project group's name, description, and gid number.
-
-    :param username: Username to use in LDAP search for slate project groups
-    """
-    server = Server(
-        import_from_settings('LDAP_SLATE_PROJECT_SERVER_URI'), use_ssl=True, connect_timeout=1
-    )
-    conn = Connection(server)
-    if not conn.bind():
-        logger.error('LDAP Slate Project: Failed to bind to LDAP server: {}'.format(conn.result))
-        return []
-    
-    searchParameters = {
-        'search_base': import_from_settings('LDAP_SLATE_PROJECT_USER_SEARCH_BASE'),
-        'search_filter': ldap.filter.filter_format("(memberUid=%s)", [username]),
-        'attributes': ['cn', 'description', 'gidNumber']
-    }
-    conn.search(**searchParameters)
-    results = []
-    if conn.entries:
-        for entry in conn.entries:
-            results.append(json.loads(entry.entry_to_json()).get('attributes'))
-
-        logger.info(f'Slate project info found for user {username}')
-    else:
-        logger.info(f'Slate project info not found for user {username}')
-        return []
-
-    return results
 
 
 def get_estimated_storage_cost(allocation_obj):
