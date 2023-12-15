e4e7b9b311deec1a74f77b473c0488f3e8fbacfd
Matthew Kusz <Matt52@roadrunner.com>
2023-12-15 09:08:56 -0500

    Add logging for the new tasks

--- coldfront/plugins/slate_project/tasks.py
+++ coldfront/plugins/slate_project/tasks.py
@@ -1,7 +1,11 @@
+import logging
+
 from coldfront.core.allocation.models import Allocation
 from coldfront.plugins.slate_project.utils import (sync_slate_project_users,
                                                    send_inactive_users_report,
                                                    LDAPModify)
+
+logger = logging.getLogger(__name__)
 
 
 def sync_all_slate_project_allocations():
@@ -9,10 +13,12 @@ def sync_all_slate_project_allocations():
         resources__name='Slate Project', status__name__in=['Active', 'Renewal Requested']
     )
     ldap_conn = LDAPModify()
+    logger.info('Running sync...')
     for slate_project_allocation in slate_project_allocations:
         sync_slate_project_users(slate_project_allocation, ldap_conn)
+    logger.info('Sync complete')
 
 
 def send_inactive_user_email_report():
     send_inactive_users_report()
-    
+    logger.info('Inactive users email report sent')
