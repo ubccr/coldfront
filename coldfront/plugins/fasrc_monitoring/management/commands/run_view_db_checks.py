342fc9dad5a29944d1f4b5ef0cf5810595f704e6
claire-peters <clairepeters@g.harvard.edu>
2023-12-21 13:35:51 -0500

    update run_view_db_checks to use env vars

--- coldfront/plugins/fasrc_monitoring/management/commands/run_view_db_checks.py
+++ coldfront/plugins/fasrc_monitoring/management/commands/run_view_db_checks.py
@@ -1,15 +1,13 @@
 from django.core.management.base import BaseCommand
 
 from coldfront.plugins.fasrc_monitoring.utils import run_view_db_checks
+from coldfront.core.utils.common import import_from_settings
+from coldfront.config.env import ENV
 
 class Command(BaseCommand):
     help = 'automatically check pages and database entries for common issues.'
 
-    def add_arguments(self, parser):
-        parser.add_argument('username', nargs='+', type=str)
-        parser.add_argument('password', nargs='+', type=str)
-
     def handle(self, *args, **options):
-        username = options['username'][0]
-        password = options['password'][0]
+        username = ENV.str('TESTUSER')
+        password = ENV.str('TESTPASS')
         run_view_db_checks(username, password)
