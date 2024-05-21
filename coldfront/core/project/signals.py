3ecbb682fff4e286342d1328877aafa666a8dbc3
geistling <34081638+geistling@users.noreply.github.com>
2024-05-21 16:35:29 -0700

    fix signal name

--- coldfront/core/project/signals.py
+++ coldfront/core/project/signals.py
@@ -1,12 +1,11 @@
 import django.dispatch
 
-
 project_create = django.dispatch.Signal()
     #providing_args=["project_title"]
 project_post_create = django.dispatch.Signal()
     #providing_args=["project_obj"]
 
-project_add_projectuser = django.dispatch.Signal()
+project_make_projectuser = django.dispatch.Signal()
     #providing_args=["user_name", "group_name"]
 
 project_preremove_projectuser = django.dispatch.Signal()
@@ -15,8 +14,3 @@ project_preremove_projectuser = django.dispatch.Signal()
 project_filter_users_to_remove = django.dispatch.Signal()
     #providing_args=["project_user_list"]
     # return tuple of (no_removal, can_remove)
-
-project_activate_user = django.dispatch.Signal()
-    #providing_args=["project_user_pk"]
-project_remove_user = django.dispatch.Signal()
-    #providing_args=["project_user_pk"]
