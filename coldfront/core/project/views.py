841e4fd308bb0d43f8fa75e154687b7180d80eb1
Matthew Kusz <Matt52@roadrunner.com>
2024-06-05 08:34:51 -0400

    Add a project title to the requesting more information email's subject line

--- coldfront/core/project/views.py
+++ coldfront/core/project/views.py
@@ -2579,7 +2579,7 @@ class ProjectRequestEmailView(LoginRequiredMixin, UserPassesTestMixin, FormView)
                 cc = []
 
             send_email(
-                'Request for more information',
+                f'Follow-up on Project {project_obj.title}',
                 form_data.get('email_body'),
                 EMAIL_TICKET_SYSTEM_ADDRESS,
                 receiver_list,
