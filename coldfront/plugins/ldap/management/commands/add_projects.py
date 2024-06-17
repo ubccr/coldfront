30ef1808c8e76533bd2e101714be686a0811420d
claire-peters <clairepeters@g.harvard.edu>
2024-06-17 18:39:19 -0400

    improve logging

--- coldfront/plugins/ldap/management/commands/add_projects.py
+++ coldfront/plugins/ldap/management/commands/add_projects.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
         project_csv = 'local_data/ready_to_add/add_projects.csv'
         projects_list = pd.read_csv(project_csv)
         added_projects, errs = import_projects_projectusers(projects_list.title.to_list())
-        add_later = errs['no_pi'] + errs['not_found'] + errs['no_fos']
+        add_later = errs['no_pi'] + [errs['not_found']] + errs['no_fos']
         projects_to_add = projects_list.loc[projects_list['title'].isin(add_later)].copy()
         try:
             projects_to_add['first_attempt'] = projects_to_add['first_attempt'].fillna(datetime.now())
