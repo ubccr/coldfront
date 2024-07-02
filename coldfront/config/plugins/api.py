5178008a412505dcf1f22aa40475fe9fc967eaae
geistling <34081638+geistling@users.noreply.github.com>
2024-07-02 15:22:53 -0400

    remove basicauthentication, add knox dependencies

--- coldfront/config/plugins/api.py
+++ coldfront/config/plugins/api.py
@@ -11,7 +11,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
