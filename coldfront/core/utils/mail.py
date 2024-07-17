c42fd4f26e370dcc9ca3a3edb7045731c138f041
claire-peters <clairepeters@g.harvard.edu>
2024-07-17 14:25:40 -0400

    add info to email logging error

--- coldfront/core/utils/mail.py
+++ coldfront/core/utils/mail.py
@@ -32,11 +32,11 @@ def send_email(subject, body, sender, receiver_list, cc=[]):
         return
 
     if len(receiver_list) == 0:
-        logger.error('Failed to send email missing receiver_list')
+        logger.error('Failed to send email missing receiver_list. Subject: %s', subject)
         return
 
     if len(sender) == 0:
-        logger.error('Failed to send email missing sender address')
+        logger.error('Failed to send email missing sender address. Subject: %s', subject)
         return
 
     if len(EMAIL_SUBJECT_PREFIX) > 0:
