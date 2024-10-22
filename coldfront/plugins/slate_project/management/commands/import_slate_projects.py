319b4c5c4f2a29e33c46c1445c6c940e15c109db
Matthew Kusz <Matt52@roadrunner.com>
2024-10-22 16:18:03 -0400

    Forgot to add the command changes

--- coldfront/plugins/slate_project/management/commands/import_slate_projects.py
+++ coldfront/plugins/slate_project/management/commands/import_slate_projects.py
@@ -9,6 +9,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("--json", type=str)
         parser.add_argument("--out", type=str)
+        parser.add_argument("--user", type=str)
         parser.add_argument("--limit", type=int)
 
     def handle(self, *args, **kwargs):
@@ -18,7 +19,7 @@ class Command(BaseCommand):
         if not kwargs.get("out"):
             raise CommandError("Out file does not exist")
         
-        if not kwargs.get("out"):
+        if not kwargs.get("user"):
             raise CommandError("Please provide your username")
         
         if kwargs.get("limit") is not None and kwargs.get("limit") <= 0:
@@ -26,5 +27,5 @@ class Command(BaseCommand):
 
         print('Importing Slate Projects...')
         start_time = time.time()
-        import_slate_projects(kwargs.get("limit"), kwargs.get("json"), kwargs.get("out"), kwargs.get("user"))
+        import_slate_projects(kwargs.get("json"), kwargs.get("out"), kwargs.get("user"), kwargs.get("limit"))
         print(f'Time elapsed: {time.time() - start_time}')
