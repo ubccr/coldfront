aaa144fdaa9cec043d84fa2617d4627558d01be6
Matthew Kusz <Matt52@roadrunner.com>
2024-11-05 13:03:24 -0500

    Add a minimum character requirement for the Slate Project search form

--- coldfront/plugins/slate_project/forms.py
+++ coldfront/plugins/slate_project/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class SlateProjectSearchForm(forms.Form):
     SLATE_PROJECT = 'Slate Project'
-    slate_project = forms.CharField(label=SLATE_PROJECT, max_length=30)
+    slate_project = forms.CharField(label=SLATE_PROJECT, min_length=2, max_length=30)
