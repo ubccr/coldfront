181ca09356cd0b760eda0b23e345b2d27eb57654
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2024-11-20 17:47:57 +0000

    Implement native plugin mechanism

--- coldfront/plugins/iquota/urls.py
+++ coldfront/plugins/iquota/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import include, path
 
 from coldfront.plugins.iquota.views import get_isilon_quota
 
+
 urlpatterns = [
-    path('get-isilon-quota/', get_isilon_quota, name='get-isilon-quota'),
+    path(
+        "iquota/", include([path('get-isilon-quota/', get_isilon_quota, name='get-isilon-quota')]),
+    ),
 ]
