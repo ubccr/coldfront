9fd74fe74e7881bdb9f670f1c254ef15e8a9c5a9
Matthew Kusz <Matt52@roadrunner.com>
2025-02-21 09:00:05 -0500

    Simplify command

--- coldfront/plugins/slate_project/management/commands/enable_changeable_slate_projects.py
+++ coldfront/plugins/slate_project/management/commands/enable_changeable_slate_projects.py
@@ -6,7 +6,4 @@ from coldfront.core.allocation.models import Allocation
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        allocation_objs = Allocation.objects.filter(resources__name='Slate Project')
-        for allocation in allocation_objs:
-            allocation.is_changeable = True
-            allocation.save()
+        Allocation.objects.filter(resources__name='Slate Project').update(is_changeable=True)
