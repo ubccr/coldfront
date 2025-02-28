a50b89d29e1903f39193184c711cc87aa3853dcd
Matthew Kusz <Matt52@roadrunner.com>
2025-02-28 08:58:45 -0500

    Add urls

--- coldfront/config/urls.py
+++ coldfront/config/urls.py
@@ -28,7 +28,9 @@ urlpatterns = [
     path('grant/', include('coldfront.core.grant.urls')),
     path('publication/', include('coldfront.core.publication.urls')),
     path('research-output/', include('coldfront.core.research_output.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if 'coldfront_custom_resources' in settings.INSTALLED_APPS: 
+    urlpatterns.append(path('custom_resources/', include('coldfront_custom_resources.urls')))
 
 if 'coldfront.plugins.announcements' in settings.INSTALLED_APPS:
     urlpatterns.append(path('announcements/', include('coldfront.plugins.announcements.urls')))
