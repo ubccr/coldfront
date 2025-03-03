0a29abf5c23621f6dcaee28965e412c7413fd554
Matthew Kusz <Matt52@roadrunner.com>
2025-03-03 13:54:06 -0500

    Add historical change reasons

--- coldfront/core/utils/apps.py
+++ coldfront/core/utils/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UtilsConfig(AppConfig):
     name = 'coldfront.core.utils'
     verbose_name = 'Coldfront Utils'
+
+    def ready(self):
+        import coldfront.core.utils.signals
