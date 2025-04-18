65f200e9b912b3477b70bc2a4bbcff1cfe670e01
geistling <34081638+geistling@users.noreply.github.com>
2025-04-18 16:22:48 -0700

    typo

--- coldfront/core/test_helpers/factories.py
+++ coldfront/core/test_helpers/factories.py
@@ -218,7 +218,7 @@ class ResourceFactory(DjangoModelFactory):
 
 class RAttributeTypeFactory(DjangoModelFactory):
     class Meta:
-        model = PAttributeType
+        model = RAttributeType
         # django_get_or_create = ('name',)
     name = factory.Faker('Int')
 
