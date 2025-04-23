19d2a6a94637398bbf3bb8b2be47616aa1d07aac
geistling <34081638+geistling@users.noreply.github.com>
2025-04-23 16:07:35 -0700

    update import_allocation_filepaths to handle allocations with missing paths

--- coldfront/plugins/sftocf/management/commands/import_allocation_filepaths.py
+++ coldfront/plugins/sftocf/management/commands/import_allocation_filepaths.py
@@ -18,8 +18,8 @@ def make_error_csv(filename, errors):
 
 class Command(BaseCommand):
     """
-    collect LFS and Isilon subdir values from starfish redash query; assign them
-    to respective allocations' Subdirectory allocationattribute.
+    Identify allocations missing path values and assign them the correct path
+    from the starfish redash query.
 
     If a group/user pairing has 0 or >1 allocation, log in an error csv to
     handle manually.
@@ -33,36 +33,51 @@ class Command(BaseCommand):
         data = [result for result in data if result['group_name']]
         starfishserver = StarFishServer()
         searched_resources = starfishserver.get_volumes_in_coldfront()
-        allocations = Allocation.objects.filter(resources__name__in=searched_resources)
+        allocations = [
+            a for a in
+            Allocation.objects.filter(
+                resources__name__in=searched_resources, status__name='Active')
+            if not a.path
+        ]
         for allocation in allocations:
             lab = allocation.project.title
             resource = allocation.get_parent_resource
             volume = resource.name.split('/')[0]
-            matched_subdirs = [entry for entry in data if entry['group_name'].lower() == lab
-            and entry['vol_name'] == volume]
-            if not matched_subdirs:
-                if allocation.status.name in ['Pending Deactivation', 'Inactive']:
-                    pass
-                else:
-                    errors.append({"lab":lab, "allocation":allocation, "issue": "no_results",
-                        "url": f"https://coldfront.rc.fas.harvard.edu/allocation/{allocation.pk}"})
-                continue
-            if len(matched_subdirs) > 1:
+            matched_paths = [
+                entry for entry in data
+                if entry['group_name'].lower() == lab
+                and entry['vol_name'] == volume
+            ]
+            if not matched_paths:
                 errors.append({
-                    "lab":lab, "allocation":allocation,
-                    "issue": "multiple_results", "results": matched_subdirs,
+                    "lab":lab, "allocation":allocation, "issue": "no_results",
                     "url": f"https://coldfront.rc.fas.harvard.edu/allocation/{allocation.pk}"
                 })
                 continue
-            subdir_value = matched_subdirs[0]['path']
-            if subdir_value != allocation.path:
-               allocation.allocationattribute_set.update_or_create(
-                   allocation_attribute_type_id=8, defaults={'value': subdir_value}
-               )
-               confirmation = f"added path to allocation {allocation.pk}: {subdir_value}"
-               logger.info(confirmation)
-               print(confirmation)
+            if len(matched_paths) > 1:
+                other_allocations = Allocation.objects.filter(
+                    resources__name__contains=volume,
+                    project__title=lab
+                ).exclude(pk=allocation.pk)
+                other_allocations_paths = [a.path for a in other_allocations]
+                matched_paths = [
+                    entry for entry in matched_paths if entry['path'] not in other_allocations_paths
+                ]
+                if len(matched_paths) != 1:
+                    errors.append({
+                        "lab":lab, "allocation":allocation,
+                        "issue": "multiple_results", "results": matched_paths,
+                        "url": f"https://coldfront.rc.fas.harvard.edu/allocation/{allocation.pk}"
+                    })
+                    continue
+            subdir_value = matched_paths[0]['path']
+            allocation.allocationattribute_set.update_or_create(
+                allocation_attribute_type_id=8, defaults={'value': subdir_value}
+            )
+            confirmation = f"added path to allocation {allocation.pk}: {subdir_value}"
+            logger.info(confirmation)
+            print(confirmation)
         for error in errors:
-            logger.error(error)
+            logger.error("problem matching path for allocation: %s", error)
             print(error)
         make_error_csv("errors_subdirs_to_add", errors)
