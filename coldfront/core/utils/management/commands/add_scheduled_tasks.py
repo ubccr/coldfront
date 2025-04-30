67b0e6a32ae2b46c69b07e8b5cc4fcf507dde75a
geistling <34081638+geistling@users.noreply.github.com>
2025-04-30 14:22:31 -0700

    add import_allocation_filepaths to schedule, add time setting to add_scheduled_tasks

--- coldfront/core/utils/management/commands/add_scheduled_tasks.py
+++ coldfront/core/utils/management/commands/add_scheduled_tasks.py
@@ -23,25 +23,39 @@ class Command(BaseCommand):
         #          next_run=date)
 
         # if plugins are installed, add their tasks
-        kwargs = {  "repeats":-1, }
+        kwargs = {"repeats": -1,}
         plugins_tasks = {
-            'fasrc': ['import_quotas', 'id_import_allocations', 'pull_resource_data'],
-            'sftocf': ['pull_sf_push_cf', 'update_zones'],
-            'lfs': ['pull_lfs_filesystem_stats'],
-            'ldap': ['update_group_membership_ldap', 'id_add_projects'],
-            'slurm': ['slurm_sync'],
-            'xdmod': ['xdmod_usage'],
+            'fasrc': [
+                ('import_allocation_filepaths', (1,0)),
+                ('id_import_allocations', (1,15)),
+                ('import_quotas', (1,30)),
+                ('pull_resource_data', (0,0)),
+            ],
+            'sftocf': [
+                ('pull_sf_push_cf', (1,45)),
+                ('update_zones', (6,25)),
+            ],
+            # 'lfs': ['pull_lfs_filesystem_stats'],
+            'ldap': [
+                ('update_group_membership_ldap', (12,45)),
+                ('id_add_projects', (1,0)),
+            ],
+            'slurm': [('slurm_sync', (3,0))],
+            'xdmod': [('xdmod_usage', (3,30))],
         }
         scheduled = [task.func for task in Schedule.objects.all()]
 
         for plugin, tasks in plugins_tasks.items():
             if f'coldfront.plugins.{plugin}' in settings.INSTALLED_APPS:
                 for task in tasks:
+                    tname = task[0]
+                    ttime = task[1]
+                    date = date.replace(hour=ttime[0], minute=ttime[1], second=0, microsecond=0)
                     if f'coldfront.plugins.{plugin}.tasks.{task}' not in scheduled:
                         schedule(f'coldfront.plugins.{plugin}.tasks.{task}',
                             next_run=date,
                             schedule_type=Schedule.DAILY,
-                            name=task,
+                            name=tname,
                             **kwargs)
 
         if 'coldfront.core.allocation.tasks.send_request_reminder_emails' not in scheduled:
