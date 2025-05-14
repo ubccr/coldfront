32f8b326ca1f1ad794532d5ab4b29f1bb57d628d
Chris Diaz <24395592+chrisdaaz@users.noreply.github.com>
2025-05-14 15:43:08 -0500

    adds pagination to support large queries

--- coldfront/config/plugins/api.py
+++ coldfront/config/plugins/api.py
@@ -20,4 +20,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
 }
