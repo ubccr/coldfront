69ea1a82313d0c4bf53f373dc83550e4d7a31c29
Dahye Kim <dahye.kim@tsgforce.com>
2025-05-16 18:16:06 -0400

    Add argument to take a directory path for importing schools

--- coldfront/core/school/management/commands/import_school_data.py
+++ coldfront/core/school/management/commands/import_school_data.py
@@ -9,10 +9,18 @@ app_commands_dir = os.path.dirname(__file__)
 
 class Command(BaseCommand):
     help = 'Import school data'
+    def add_arguments(self, parser):
+        default_path = os.path.join(app_commands_dir, 'data', 'school_data.csv')
+        parser.add_argument(
+            '--csv-file-path',
+            type=str,
+            default=default_path,
+            help='Filesystem path to the tab‑delimited school_data.csv'
+        )
 
     def handle(self, *args, **options):
         print('Adding schools ...')
-        file_path = os.path.join(app_commands_dir, 'data', 'school_data.csv')
+        file_path = options['csv_file_path']
         School.objects.all().delete()
         with open(file_path, 'r') as fp:
             for line in fp:
