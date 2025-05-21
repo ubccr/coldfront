d1f429bea01e30c577e283f8eeb5d7fd34274a97
claire-peters <clairepeters@g.harvard.edu>
2025-05-21 13:47:08 -0700

    add logging for projectuser role change

--- coldfront/core/project/views.py
+++ coldfront/core/project/views.py
@@ -934,6 +934,7 @@ class ProjectUserDetail(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
                 old_role = project_user_obj.role.name
                 new_role = form_data.get('role')
                 username = project_user_obj.user.username
+                requester_uname = self.request.user.username
                 project_user_obj.role = ProjectUserRoleChoice.objects.get(name=new_role)
                 project_user_obj.save()
 
@@ -954,6 +955,10 @@ class ProjectUserDetail(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
                     cc=[project_obj.pi.email]
                 )
                 messages.success(request, 'User details updated.')
+                logger.info(
+                    'Project %s User %s role changed from %s to %s by %s',
+                    project_obj.title, username, old_role, new_role, requester_uname
+                )
                 return HttpResponseRedirect(
                     reverse(
                         'project-user-detail',
