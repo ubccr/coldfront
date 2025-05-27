c1912047ecb7285bb5f8b0d979b1a4341eb2e02f
Matthew Kusz <matt52@roadrunner.com>
2025-05-27 14:38:02 -0400

    Remove unused url

--- coldfront/plugins/help/urls.py
+++ coldfront/plugins/help/urls.py
@@ -5,5 +5,4 @@ from coldfront.plugins.help.views import SlateProjectSearchResultsView
 
 urlpatterns = [
     path("", SlateProjectSearchResultsView.as_view(), name="get-help"),
-    path("<str:queue>", SlateProjectSearchResultsView.as_view(), name="get-help"),
 ]
