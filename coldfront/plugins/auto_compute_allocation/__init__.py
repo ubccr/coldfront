597ca638d35c0f0f8112cd7b2f8ac0c6068365f2
David Simpson <mpmds@hplaptop.home>
2025-06-09 14:55:06 +0100

    Adds auto_compute_allocation plugin
    Signed-off-by: David Simpson <David Simpson>

---
+++ coldfront/plugins/auto_compute_allocation/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: (C) ColdFront Authors
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+default_app_config = "coldfront.plugins.auto_compute_allocation.apps.AutoComputeAllocationConfig"
