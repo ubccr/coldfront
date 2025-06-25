95131c4e301ba3dc3c1f5f2dc0eb357e6917c121
claire-peters <clairempeters@gmail.com>
2025-06-25 21:22:52 +0000

    fix function for sending review

--- coldfront/core/project/tasks.py
+++ coldfront/core/project/tasks.py
@@ -1,18 +1,16 @@
-from io import BytesIO
 from datetime import datetime
 
-from django.http import FileResponse, HttpResponse
+from django.contrib.auth import get_user_model
 from django.test import RequestFactory
-from django.conf import settings
 
-# import your report view
-
+from coldfront.config.env import ENV
 from coldfront.core.project.models import Project
 from coldfront.core.project.views import ProjectStorageReportView
 from coldfront.core.utils.common import import_from_settings
 from coldfront.core.utils.mail import send_email_template, email_template_context
 
 EMAIL_SENDER = import_from_settings('EMAIL_SENDER')
+TESTUSER = ENV.str('TESTUSER')
 
 def send_storage_report_emails():
     """Send monthly email with project storage reports"""
@@ -31,30 +29,29 @@ def send_pdf(project, context=None):
     `context` will be passed to the view when rendering.
     """
 
+    system_user = get_user_model().objects.get(username=TESTUSER)
     month = datetime.now().strftime("%B")
     title = project.title
     subject = f'Monthly Coldfront {month} Report for {title}'
     context = email_template_context(extra_context={'project_title': title})
-    # 1) build a fake GET request, set any attributes your view needs (e.g. user)
+    # 1) build a fake GET request, set any necessary attributes
     factory = RequestFactory()
     request = factory.get(f'project/{project.pk}/report')
-    request.user = getattr(context or {}, 'user', None)
+    request.user = system_user
 
-    # 2) instantiate and call your class‐based view
-    #    if you are using a simple function‐based view, just call it
+    # 2) instantiate and call class‐based view
     view = ProjectStorageReportView.as_view()
-    response = view(request, **(context or {}))
+    response = view(request, pk=project.pk)
 
     # 3) make sure the response is rendered and grab the bytes
-    #    PDFTemplateResponseMixin often provides `.rendered_content`
     if hasattr(response, 'rendered_content'):
         pdf_bytes = response.rendered_content
     else:
-        # fallback
         pdf_bytes = response.content
 
     receivers = project.projectuser_set.filter(role__name__in=['PI','General Manager'])
     receiver_list = [receiver.user.email for receiver in receivers]
+    attachment = (f'{title}_{month}_{year}_storagereport.pdf', pdf_bytes, 'application/pdf')
     send_email_template(
-        subject, 'storage_report.txt', context, EMAIL_SENDER, receiver_list, attachments=(pdf_bytes)
+        subject, 'email/storage_report.txt', context, EMAIL_SENDER, receiver_list, attachments=(attachment,)
     )
