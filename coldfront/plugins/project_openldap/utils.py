34edfd38f56888750069736f4efbcb2897ef45a9
David Simpson <>
2025-10-03 10:19:38 +0100

    remove unnecessary connection in project_openldap utils.py
    Signed-off-by: David Simpson <>

--- coldfront/plugins/project_openldap/utils.py
+++ coldfront/plugins/project_openldap/utils.py
@@ -170,8 +170,6 @@ def remove_dn_from_openldap(dn, write=True):
 
     try:
         conn.delete(dn)
-        conn.unbind()
-        conn = openldap_connection(server, PROJECT_OPENLDAP_BIND_USER, PROJECT_OPENLDAP_BIND_PASSWORD)
     except Exception as exc_log:
         logger.info(exc_log)
     finally:
@@ -191,7 +189,6 @@ def update_posixgroup_description_in_openldap(dn, openldap_description, write=Tr
 
     try:
         conn.modify(dn, {"description": [(MODIFY_REPLACE, [openldap_description])]})
-        conn.unbind()
     except Exception as exc_log:
         logger.info(exc_log)
     finally:
@@ -211,7 +208,6 @@ def move_dn_in_openldap(current_dn, relative_dn, destination_ou, write=True):
 
     try:
         conn.modify_dn(current_dn, relative_dn, new_superior=destination_ou)
-        conn.unbind()
     except Exception as exc_log:
         logger.info(exc_log)
     finally:
